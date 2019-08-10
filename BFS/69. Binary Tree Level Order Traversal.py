3c87da7cbc6072a70122a0b9abc5cdeab99b781e
Tian Feng <feng.txy@outlook.com>
2019-08-10 14:10:00 -0700

    Create 69. Binary Tree Level Order Traversal.py

---
+++ BFS/69. Binary Tree Level Order Traversal.py
@@ -0,0 +1,34 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here 
+        if not root:return []
+        res = [[root.val]]
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            queue_len = len(queue)
+            path = []
+            for i in range(queue_len):
+                curr_node = queue.popleft()
+                if curr_node.left:
+                    queue.append(curr_node.left)
+                    path.append(curr_node.left.val)
+                if curr_node.right:
+                    queue.append(curr_node.right)
+                    path.append(curr_node.right.val)
+            if path != []:
+                res.append(path)
+        return res
+                    
