8947579af8150698644ccdc7e3d9226a7e6df690
Tian Feng <feng.txy@outlook.com>
2019-08-10 14:56:44 -0700

    Create 627. Longest Palindrome.py

---
+++ Defeat_Coding_Interview/627. Longest Palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    @param s: a string which consists of lowercase or uppercase letters
+    @return: the length of the longest palindromes that can be built
+    """
+    def longestPalindrome(self, s):
+        # write your code here
+        chars = []
+        res = 0
+        for char in s:
+            if char in chars:
+                chars.remove(char)
+                res+=1
+                
+            else:
+                chars.append(char)
+        return min(res*2+1, len(s))
+            
