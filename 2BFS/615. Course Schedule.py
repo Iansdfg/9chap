87c7b7a0a78ab84021747781532f9bc142c6faf2
Tian Feng <feng.txy@outlook.com>
2019-08-12 22:01:33 -0700

    Update 615. Course Schedule.py

--- 2BFS/615. Course Schedule.py
+++ 2BFS/615. Course Schedule.py
@@ -6,27 +6,33 @@ class Solution:
     """
     def canFinish(self, numCourses, prerequisites):
         # write your code here
-        couses = {i : [] for i in range(numCourses)}
-        indegree =  [0 for i in range(numCourses)]
+        indegree = [0 for _ in range(numCourses)]
+        courses_preres = {x:[] for x in range(numCourses)}
         
-        for i, j in prerequisites:
-            couses[j].append(i)
-            indegree[i] += 1
-        
+        for i,j in prerequisites:
+            indegree[i]+=1
+            courses_preres[j].append(i)
+            
+        print(indegree, courses_preres)
+            
         queue = collections.deque()
-        count = 0
-        
-        for key in couses:
+        for key in courses_preres:
             if indegree[key] == 0:
                 queue.append(key)
-        
+                
+        print(queue)
+                
+        count = 0
         while queue:
             curr_course = queue.popleft()
             count += 1
-            for prerequisite in couses[curr_course]:
-                indegree[prerequisite]-=1
-                if indegree[prerequisite] == 0:
-                    queue.append(prerequisite)
+            for prere in courses_preres[curr_course]:
+                indegree[prere] -= 1
+                if indegree[prere] == 0:
+                    queue.append(prere)
                     
         return count == numCourses
+    
+    
+        
          
