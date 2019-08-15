6508999780b933171d3587d3ca930fe7521d8f5a
Tian Feng <feng.txy@outlook.com>
2019-08-15 12:05:04 -0700

    Create 585. Maximum Number in Mountain Sequence.py

---
+++ 3Binary_Search/585. Maximum Number in Mountain Sequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param nums: a mountain sequence which increase firstly and then decrease
+    @return: then mountain top
+    """
+    def mountainSequence(self, nums):
+        # write your code here
+        if nums == []: return -1
+        
+        start, end = 0, len(nums)-1
+        
+        while start+1<end:
+            m = (start+end)//2
+            
+            if nums[m]<nums[m+1]:
+                start = m 
+            elif nums[m]>nums[m+1]:
+                end = m
+                
+        return max(nums[start],nums[end])
