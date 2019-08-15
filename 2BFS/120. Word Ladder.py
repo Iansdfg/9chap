4ec597c9c68e90414b654b7a004fac856f972eaf
Tian Feng <feng.txy@outlook.com>
2019-08-15 11:45:22 -0700

    Create  120. Word Ladder.py

---
+++ 2BFS/120. Word Ladder.py
@@ -0,0 +1,30 @@
+
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    def ladderLength(self, start, end, dict):
+        # write your code here
+        if start == end: return 0
+        dict.add(end)
+        queue = collections.deque([start])
+        visited = set([start])
+        step = 0
+        while queue:
+            step+=1
+            for _ in range(len(queue)):
+                curr_word = queue.popleft()
+                if curr_word == end:
+                    return step
+                for i in range(len(curr_word)):
+                    for char in 'abcdefghijklmnopqrstuvwxyz':
+                        new_word = curr_word[:i] + char + curr_word[i+1:]
+                        if new_word in dict and new_word not in visited:
+                            queue.append(new_word)
+                            visited.add(new_word)
+        return 0
+                        
+            
