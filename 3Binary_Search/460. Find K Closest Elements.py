6ae890e8d5b00b1c344d9ebd87730db5ad1b6e33
Tian Feng <feng.txy@outlook.com>
2019-08-15 15:09:30 -0700

    Create 460. Find K Closest Elements.py

---
+++ 3Binary_Search/460. Find K Closest Elements.py
@@ -0,0 +1,48 @@
+class Solution:
+    """
+    @param A: an integer array
+    @param target: An integer
+    @param k: An integer
+    @return: an integer array
+    """
+    def kClosestNumbers(self, A, target, k):
+        # write your code here
+        right = self.find_first_k(A, target)
+        print(right)
+        left = right-1
+        nearKth = []
+ 
+        for _ in range(k):
+            if self.is_left_clother(A, target, left, right):
+                nearKth.append(A[left])
+                left-=1
+            else:
+                nearKth.append(A[right])
+                right+=1
+            
+        return nearKth
+            
+            
+    def find_first_k(self, A, target):
+        start, end  = 0, len(A)-1
+        while start+1<end:
+            m = (start + end) // 2
+            
+            if A[m] < target:
+                start = m
+            elif A[m] > target:
+                end = m
+            else:
+                return m
+        
+        if abs(A[start]-target) < abs(A[end]-target):
+            return start 
+        else:
+            return end
+        
+    def is_left_clother(self, A, target, left, right):
+        if left < 0:
+            return False
+        if right >= len(A):
+            return True
+        return target - A[left] <= A[right] - target
