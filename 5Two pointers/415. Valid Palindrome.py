1a5c8a93510999468a64c0a1992fc9a46abcd601
Tian Feng <feng.txy@outlook.com>
2019-08-18 15:12:47 -0700

    Create 415. Valid Palindrome.py

---
+++ 5Two pointers/415. Valid Palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    @param s: A string
+    @return: Whether the string is a valid palindrome
+    """
+    def isPalindrome(self, s):
+        # write your code here
+        string  = ''
+        for char in s:
+            if char.isalnum():
+                if char.isdigit():
+                    string += char
+                else:
+                    string += char.lower()
+        return string[:] == string[::-1]
