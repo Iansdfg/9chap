eead9928d04e3939fbdefea8c279f447463e961f
Tian Feng <feng.txy@outlook.com>
2019-08-17 17:54:04 -0700

    Create 75. Find Peak Element.py

---
+++ 3Binary_Search/75. Find Peak Element.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param A: An integers array.
+    @return: return any of peek positions.
+    """
+    def findPeak(self, A):
+        # write your code here
+        start, end = 1, len(A) - 2
+        
+        while start + 1 <  end:
+            mid = (start + end) // 2
+            if A[mid] < A[mid - 1]:
+                end = mid
+            elif A[mid] < A[mid + 1]:
+                start = mid
+            else:
+                end = mid
+                
+        return start if A[start]>A[end] else end
+            
