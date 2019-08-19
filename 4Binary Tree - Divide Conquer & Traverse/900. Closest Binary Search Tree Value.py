df5932ead5805230b7792962606b36352eeeda5a
Tian Feng <feng.txy@outlook.com>
2019-08-19 15:37:09 -0700

    Update 900. Closest Binary Search Tree Value.py

--- 4Binary Tree - Divide Conquer & Traverse/900. Closest Binary Search Tree Value.py
+++ 4Binary Tree - Divide Conquer & Traverse/900. Closest Binary Search Tree Value.py
@@ -28,3 +28,57 @@ class Solution:
         
         return upper.val if abs(upper.val - target) <= abs(lower.val - target) else lower.val
         
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        # write your code here
+        if root is None:
+            return None 
+
+        lower_bound = self.get_lower_bound(root, target)
+
+        upper_bound = self.get_upper_bound(root, target)
+
+        
+        if not lower_bound:
+            return upper_bound.val
+        if not upper_bound:
+            return lower_bound.val
+            
+        if target - lower_bound.val < upper_bound.val - target:
+            return lower_bound.val
+        return upper_bound.val
+        
+    def get_lower_bound(self, root, target):
+        # get the largest node that < target
+        if root is None:
+            return None
+        
+        if target < root.val:
+            return self.get_lower_bound(root.left, target)
+            
+        lower = self.get_lower_bound(root.right, target)
+        return root if lower is None else lower
+        
+    def get_upper_bound(self, root, target):
+        # get the smallest node that >= target
+        if root is None:
+            return None
+        
+        if target >= root.val:
+            return self.get_upper_bound(root.right, target)
+            
+        upper = self.get_upper_bound(root.left, target)
+        return root if upper is None else upper
