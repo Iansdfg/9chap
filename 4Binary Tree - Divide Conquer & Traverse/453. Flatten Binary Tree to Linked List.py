2b617f36031b17370cd0d1c45f2048caa4812602
Tian Feng <feng.txy@outlook.com>
2019-08-21 22:55:49 -0700

    Create 453. Flatten Binary Tree to Linked List.py

---
+++ 4Binary Tree - Divide Conquer & Traverse/453. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,45 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        # write your code here
+        self.connect(root)
+        return root
+        
+        
+    def connect(self, root):
+        if not root:
+            return None
+        
+        left_last = self.connect(root.left)
+        right_last = self.connect(root.right)
+        
+        if left_last:
+            left_last.right = root.right
+            root.right = root.left
+            root.left = None 
+            
+        if right_last:
+            return right_last
+        
+        if left_last:
+            return left_last
+            
+        return root
+            
+        
+        
+            
+            
+        
+        
