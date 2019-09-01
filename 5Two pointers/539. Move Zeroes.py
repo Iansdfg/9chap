f988ba244a63740d6a85c47dc60edd6c5221edfe
Tian Feng <feng.txy@outlook.com>
2019-09-01 16:49:23 +0800

    Update 539. Move Zeroes.py

--- 5Two pointers/539. Move Zeroes.py
+++ 5Two pointers/539. Move Zeroes.py
@@ -11,18 +11,3 @@ class Solution:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
             right += 1
-        # zero_pose = 0
-        # non_zero_pose = 0
-        # while zero_pose<len(nums)-1:
-        #     if nums[zero_pose]!=0:
-        #         zero_pose+=1
-        #     else:
-        #         zero_pose = zero_pose
-        #         while nums[non_zero_pose]==0:
-        #             non_zero_pose+=1
-        #             if non_zero_pose > len(nums)-1:
-        #                 return 
-        #         nums[zero_pose], nums[non_zero_pose] = nums[non_zero_pose],nums[zero_pose]
-        #     zero_pose+=1
-        
-                
