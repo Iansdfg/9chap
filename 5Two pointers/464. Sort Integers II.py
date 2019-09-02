13a55d55d113f5db5dfd5448f53c9c8e79e9bb22
Tian Feng <feng.txy@outlook.com>
2019-09-02 21:01:43 +0800

    Create 464. Sort Integers II.py

---
+++ 5Two pointers/464. Sort Integers II.py
@@ -0,0 +1,36 @@
+class Solution:
+    """
+    @param A: an integer array
+    @return: nothing
+    """
+    def sortIntegers2(self, A):
+        # write your code here
+        return self.quick_sort(A, 0, len(A)-1)
+        
+        
+    def quick_sort(self, A, start, end):
+        
+        if start >= end:
+            return
+        
+        left, right = start, end
+        
+        pivot = A[left]
+        
+        while left<=right:
+            while left<=right and pivot>A[left]:
+                left+=1
+            
+            while left<=right and pivot<A[right]:
+                right-=1
+                
+            if left<=right:
+                A[right], A[left] = A[left],A[right]
+                
+                right-=1
+                left+=1
+        
+        self.quick_sort(A, start, right)
+        self.quick_sort(A, left, end)
+        
+        
