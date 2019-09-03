7bc2079ce07ab9ac216bd3a53a95b86f82e57ea1
Tian Feng <feng.txy@outlook.com>
2019-09-03 10:51:08 +0800

    Update 148. Sort Colors.py

--- 5Two pointers/148. Sort Colors.py
+++ 5Two pointers/148. Sort Colors.py
@@ -1,3 +1,28 @@
+###three-pointer
+class Solution:
+    """
+    @param nums: A list of integer which is 0, 1 or 2 
+    @return: nothing
+    """
+    def sortColors(self, nums):
+        # write your code here
+        if not nums:return nums
+        
+        left, right = 0, len(nums)-1
+        
+        i = 0
+        while i<=right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                i+=1
+                left+=1
+            elif nums[i] == 1:
+                i+=1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right-=1
+            
+
 ###two-pointer
 class Solution:
     """
