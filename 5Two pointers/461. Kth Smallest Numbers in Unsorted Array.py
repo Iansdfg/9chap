323e2f7d13ee63709b69895c687d6a8a72d0c610
Tian Feng <feng.txy@outlook.com>
2019-09-06 14:39:14 +0800

    Update 461. Kth Smallest Numbers in Unsorted Array.py

--- 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
+++ 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
@@ -1,4 +1,4 @@
-class Solution:
+vclass Solution:
     """
     @param k: An integer
     @param nums: An integer array
@@ -6,33 +6,33 @@ class Solution:
     """
     def kthSmallest(self, k, nums):
         # write your code here
-        if not nums or k < 1 or k > len(nums):
+        if not nums or k<1 or k>len(nums):
             return None
-        return self.partition(nums, 0, len(nums) - 1, k-1)
+        return self.quick_select(k-1, nums, 0, len(nums)-1)
         
-    def partition(self, nums, start, end, k):
-        """
-        During the process, it's guaranteed start <= k <= end
-        """
-        if start == end:
+        
+    def quick_select(self, k, nums, start, end):
+        if start>=end:
             return nums[k]
             
         left, right = start, end
-        pivot = nums[(start + end) // 2]
-        while left <= right:
-            while left <= right and nums[left] < pivot:
-                left += 1
-            while left <= right and nums[right] > pivot:
-                right -= 1
-            if left <= right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left, right = left + 1, right - 1
-                
-  
-        # left is not bigger than right
+        pivot = nums[(start + end)//2]
+        while left<=right:
+            while left <= right and nums[left]<pivot:
+                left+=1
+            while left <= right and nums[right]>pivot:
+                right-=1
+            if left<=right:
+                nums[right], nums[left] = nums[left], nums[right] 
+                left+=1
+                right-=1
+            
         if k <= right:
-            return self.partition(nums, start, right, k)
+            return self.quick_select(k, nums, start, right)
         elif k >= left:
-            return self.partition(nums, left, end, k)
+            return self.quick_select(k, nums, left, end)
         
         return nums[k]
+                
+                
+        
