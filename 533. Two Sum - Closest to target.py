763cb8685b3e74fafb69591cde32b7c9bd6dff13
Tian Feng <feng.txy@outlook.com>
2019-09-09 16:33:46 +0800

    Create 533. Two Sum - Closest to target.py

---
+++ 533. Two Sum - Closest to target.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    @param nums: an integer array
+    @param target: An integer
+    @return: the difference between the sum and the target
+    """
+    def twoSumClosest(self, nums, target):
+        # write your code here
+        nums.sort()
+        res = float('inf')
+        left, right = 0, len(nums)-1
+        while left<right:
+            two_sum = nums[left]+nums[right]
+            if two_sum == target:
+                return 0
+            elif two_sum > target:
+                res = min(res, abs(two_sum-target))
+                right-=1
+            elif two_sum < target:
+                res = min(res, abs(two_sum-target))
+                left+=1
+        return res
+                
+                
