50ddfe060138d0db8b63321290babf218ad4c578
Tian Feng <feng.txy@outlook.com>
2019-09-10 16:19:41 +0800

    Update 610. Two Sum - Difference equals to target.py

--- 5Two pointers/610. Two Sum - Difference equals to target.py
+++ 5Two pointers/610. Two Sum - Difference equals to target.py
@@ -18,3 +18,25 @@ class Solution:
                 return sorted([dic[ele[1]], ele[0]])
             
             
+class Solution:
+    """
+    @param nums: an array of Integer
+    @param target: an integer
+    @return: [index1 + 1, index2 + 1] (index1 < index2)
+    """
+    def twoSum7(self, nums, target):
+        # write your code here
+        target = abs(target)
+        numbers = [(pos+1, val) for pos, val in enumerate(nums)]
+        numbers = sorted(numbers, key = lambda x:x[1])
+        dic = {}
+        length = len(numbers)
+        right = 0
+        for left in range(length):
+            if right == left:
+                right+=1
+            while right<length and numbers[right][1]-numbers[left][1]<target:
+                right+=1
+            if right<length and numbers[right][1]-numbers[left][1]==target:
+                return sorted([numbers[left][0], numbers[right][0]])
+            
