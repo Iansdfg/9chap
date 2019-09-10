fc835f4f2c0906e8b8f850311183afbe0df9eb9a
Tian Feng <feng.txy@outlook.com>
2019-09-10 15:12:30 +0800

    Update 56. Two Sum.py

--- 5Two pointers/56. Two Sum.py
+++ 5Two pointers/56. Two Sum.py
@@ -15,3 +15,29 @@ class Solution:
                 return [dic[number], pos]
             
         return [-1, -1]
+
+    
+    class Solution:
+    """
+    @param numbers: An array of Integer
+    @param target: target = numbers[index1] + numbers[index2]
+    @return: [index1, index2] (index1 < index2)
+    """
+    def twoSum(self, numbers, target):
+        # write your code here
+        dic = {}
+        for pos, number in enumerate(numbers):
+            if number not in dic:
+                dic[number] = [pos]
+            else:
+                dic[number].append(pos)
+            
+            val = target - number
+        
+            if val in dic and val != number:
+                index = dic[val][0]
+                return [index, pos]
+                
+            if val in dic and val == number and len(dic[number]) > 1:
+                index_list = dic[number]
+                return [min(index_list), max(index_list)]
