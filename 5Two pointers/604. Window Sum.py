19493a750978bb43315cc98416113fa0640c5c47
Tian Feng <feng.txy@outlook.com>
2019-09-19 17:25:52 +0800

    Update 604. Window Sum.py

--- 5Two pointers/604. Window Sum.py
+++ 5Two pointers/604. Window Sum.py
@@ -12,8 +12,6 @@ class Solution:
         sums = [0] * (n - k + 1)
         for i in range(k):
             sums[0] += nums[i];
-
         for i in range(1, n - k + 1):
             sums[i] = sums[i - 1] - nums[i - 1] + nums[i + k - 1]
-
         return sums
