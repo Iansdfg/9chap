40b0d8fbcd7dc9bfbf7a9f55cb19d8ce6cdeffbf
Tian Feng <feng.txy@outlook.com>
2019-09-27 11:01:02 -0700

    Create 18. Subsets II.py

---
+++ 6DFS/18. Subsets II.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsetsWithDup(self, nums):
+        # write your code here
+        if not nums:
+            return [[]]
+        nums.sort()
+        ans = []
+        self.dfs(nums, 0, [], ans)
+        return ans
+        
+        
+    def dfs(self, nums, index, path, ans):
+        ans.append(path[:])
+        for i in range(index, len(nums)):
+            if i>0 and nums[i] == nums[i-1] and i>index:
+                continue
+            path.append(nums[i])
+            self.dfs(nums, i+1, path, ans)
+            path.pop()
