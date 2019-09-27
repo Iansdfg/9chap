73b4857a650af1e6a719632b90dcc5cc140c704e
Tian Feng <feng.txy@outlook.com>
2019-09-27 11:59:24 -0700

    Create  16. Permutations II.py

---
+++ 6DFS/16. Permutations II.py
@@ -0,0 +1,31 @@
+class Solution:
+    """
+    @param: :  A list of integers
+    @return: A list of unique permutations
+    """
+
+    def permuteUnique(self, nums):
+        # write your code here
+        if not nums:
+            return [[]]
+        nums.sort()
+        visited = [0]*len(nums)
+        ans = []
+        self.dfs( nums, visited, [], ans)
+        return ans
+        
+    def dfs(self, nums, visited, path, ans):
+        if len(path) == len(nums):
+            ans.append(path[:])
+            return 
+        for i in range(len(nums)):
+            if visited[i]:
+                continue
+            if i>0 and nums[i] == nums[i-1] and not visited[i-1]:
+                continue
+            path.append(nums[i])
+            visited[i]=1
+            self.dfs(nums, visited, path, ans)
+            visited[i]=0
+            path.pop()
+        
