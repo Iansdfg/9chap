9008e07c5b6814e41d0935c26b0750793e73f1d0
Tian Feng <feng.txy@outlook.com>
2019-09-27 16:40:26 -0700

    Create  135. Combination Sum.py

---
+++ 6DFS/135. Combination Sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        # write your code here
+        if not candidates:
+            return[[]]
+        candidates.sort()
+        results = []
+        self.dfs(candidates, target, [], results)
+        return results
+        
+    def dfs(self, candidates, target, path, results):
+        if sum(path) > target:
+            return
+        if sum(path) == target:
+            results.append(path[:])
+            return
+        for i in range(len(candidates)):
+            if i > 0 and candidates[i]==candidates[i-1]:
+                continue
+            path.append(candidates[i])
+            self.dfs(candidates[i:], target, path, results)
+            path.pop()
+        
