c1dca0813b366677ad07fda541b14aa705b364be
Tian Feng <feng.txy@outlook.com>
2019-09-29 12:14:31 -0700

    Update 18. Subsets II.py

--- 6DFS/18. Subsets II.py
+++ 6DFS/18. Subsets II.py
@@ -16,7 +16,7 @@ class Solution:
     def dfs(self, nums, index, path, ans):
         ans.append(path[:])
         for i in range(index, len(nums)):
-            if i>0 and nums[i] == nums[i-1] and i>index:
+            if i>0 and nums[i] == nums[i-1] and i!=index:
                 continue
             path.append(nums[i])
             self.dfs(nums, i+1, path, ans)
