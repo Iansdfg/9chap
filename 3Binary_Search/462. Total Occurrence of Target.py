eef4691c827394ca575c3392a9dc6448ec050bb5
Tian Feng <feng.txy@outlook.com>
2019-10-02 10:26:54 -0700

    Create 462. Total Occurrence of Target.py

---
+++ 3Binary_Search/462. Total Occurrence of Target.py
@@ -0,0 +1,54 @@
+class Solution:
+    """
+    @param A: A an integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def totalOccurrence(self, A, target):
+        # write your code here
+        if len(A)==0:
+            return 0
+        fist_pos = self.findFirst(A, target)
+        Last_pos = self.findLast(A, target)
+        print(fist_pos, Last_pos)
+        if fist_pos is None or Last_pos is None:
+            return 0
+        else:
+            return Last_pos-fist_pos+1
+        return fist_pos
+        
+    def findFirst(self, A, target):
+        left, right = 0, len(A)-1
+        while left+1<right:
+            mid = (left+right) // 2
+            if A[mid] > target:
+                right = mid
+            elif  A[mid] < target:
+                left = mid
+            else:
+                right = mid
+
+        if A[left] == target:
+            return left
+        elif A[right] == target:
+            return right
+        else:
+            return None
+            
+    def findLast(self, A, target):
+        left, right = 0, len(A)-1
+        while left+1<right:
+            mid = (left+right)//2
+            if A[mid] > target:
+                right = mid
+            elif  A[mid] < target:
+                left = mid
+            else:
+                left = mid
+                
+        if A[right] == target:
+            return right
+        elif A[left] == target:
+            return left
+        else:
+            return None
