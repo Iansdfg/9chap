0cc5a449cb93f906abca477b1b36775019558ca3
Tian Feng <feng.txy@outlook.com>
2019-10-07 10:19:26 -0700

    Update 90. k Sum II.py

--- 6DFS/90. k Sum II.py
+++ 6DFS/90. k Sum II.py
@@ -27,3 +27,32 @@ class Solution:
             self.dfs(A, k, target-A[i], i+1, path, results)
             path.pop()
         
+
+        
+class Solution:
+    """
+    @param: A: an integer array
+    @param: k: a postive integer <= length(A)
+    @param: targer: an integer
+    @return: A list of lists of integer
+    """
+    def kSumII(self, A, k, target):
+        # write your code here
+        if A==[]:
+            return[[]]
+        results = []
+        self.dfs(A, k, target, 0, [], results)
+        return results
+        
+    def dfs(self, A, k, target, index, path, results):
+        if target == 0 and k == 0:
+            results.append(path[:])
+            return
+        if k==0 or target < 0:
+            return
+        # print(path, results)
+        for i in range(index, len(A)):
+            path.append(A[i])
+            self.dfs(A, k-1, target-A[i], i+1, path, results)
+            path.pop()
+        
