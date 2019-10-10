a7d3e60f27dbd9efa52932d042a06d823b9afee9
Tian Feng <feng.txy@outlook.com>
2019-10-10 13:08:49 -0700

    Create 495. Implement Stack.py

---
+++ 7HashHeap/495. Implement Stack.py
@@ -0,0 +1,31 @@
+class Stack:
+    """
+    @param: x: An integer
+    @return: nothing
+    """
+    stack = []
+    def push(self, x):
+        # write your code here
+        self.stack.append(x)
+
+    """
+    @return: nothing
+    """
+    def pop(self):
+        # write your code here
+        return self.stack.pop()
+        
+
+    """
+    @return: An integer
+    """
+    def top(self):
+        # write your code here
+        return self.stack[-1]
+
+    """
+    @return: True if the stack is empty
+    """
+    def isEmpty(self):
+        # write your code here
+        return len(self.stack) == 0
