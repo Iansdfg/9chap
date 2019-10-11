9f2ff50a2f4ffd64ec4ab240f47a6e7c6f4acec8
Tian Feng <feng.txy@outlook.com>
2019-10-11 08:59:17 -0700

    Update 494. Implement Stack by Two Queues.py

--- 7HashHeap/494. Implement Stack by Two Queues.py
+++ 7HashHeap/494. Implement Stack by Two Queues.py
@@ -1,13 +1,21 @@
 from collections import deque
+
 class Stack:
     """
     @param: x: An integer
     @return: nothing
     """
     def __init__(self):
-        self.queue1 = deque([])
-        self.queue2 = deque([])
+        self.queue1 = deque()
+        self.queue2 = deque()
+    
+    def move(self):
+        while len(self.queue1)>1:
+            self.queue2.append(self.queue1.popleft())
         
+    def switch(self):
+        self.queue1, self.queue2 = self.queue2, self.queue1
+    
     def push(self, x):
         # write your code here
         self.queue1.append(x)
@@ -17,28 +25,24 @@ class Stack:
     """
     def pop(self):
         # write your code here
-        while len(self.queue1)>1:
-            self.queue2.append(self.queue1.popleft())
-        self.queue1, self.queue2 = self.queue2, self.queue1
-        return self.queue2.popleft() if len(self.queue2) != 0 else None
+        self.move()
+        self.switch()
+        self.queue2.popleft()
 
     """
     @return: An integer
     """
     def top(self):
         # write your code here
-        while len(self.queue1)>1:
-            self.queue2.append(self.queue1.popleft())
-
-        top = self.queue1[-1]
-        self.queue2.append(self.queue1.popleft())
-        self.queue1, self.queue2 = self.queue2, self.queue1
-        return top
-
+        self.move()
+        ans = self.queue1.popleft()
+        self.queue2.append(ans)
+        self.switch()
+        return ans
 
     """
     @return: True if the stack is empty
     """
     def isEmpty(self):
         # write your code here
-        return len(self.queue1) == 0 
+        return len(self.queue1) == 0
