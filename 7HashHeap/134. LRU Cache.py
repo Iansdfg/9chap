61703333a318d8b52b26124c28c289cf064f457c
Tian Feng <feng.txy@outlook.com>
2019-10-17 18:42:01 -0700

    Update 134. LRU Cache.py

--- 7HashHeap/134. LRU Cache.py
+++ 7HashHeap/134. LRU Cache.py
@@ -1,58 +1,71 @@
 class LinkedNode:
     
-    def __init__(self, key=None, value=None, next=None):
+    def __init__(self, val = None, key = None, next=None):
+        self.val = val
         self.key = key
-        self.value = value
         self.next = next
-
+        
+    # do intialization if necessary
 class LRUCache:
-
-    # @param capacity, an integer
+    """
+    @param: capacity: An integer
+    """
     def __init__(self, capacity):
-        self.key_to_prev = {}
+        # do intialization if necessary
+        self.key_to_prev = dict()
+        self.capacity = capacity
         self.dummy = LinkedNode()
         self.tail = self.dummy
-        self.capacity = capacity
+        
+        
+    # prev--node--next--...---tail
+    # prev--next--...---tail--node
+    def kick(self, prev):
+        node = prev.next
+        if node == self.tail:
+            return
+        prev.next = node.next
+        if node.next:
+            self.key_to_prev[node.next.key] = prev
+            node.next = None
+        self.append(node)
     
-    def push_back(self, node):
+    def append(self, node):
         self.key_to_prev[node.key] = self.tail
         self.tail.next = node
         self.tail = node
-    
+        
     def pop_front(self):
-        # 删除头部
         head = self.dummy.next
         del self.key_to_prev[head.key]
         self.dummy.next = head.next
         self.key_to_prev[head.next.key] = self.dummy
         
-    # change "prev->node->next...->tail"
-    # to "prev->next->...->tail->node"
-    def kick(self, prev):    #将数据移动至尾部
-        node = prev.next
-        if node == self.tail:
-            return
-        prev.next = node.next
-        if node.next is not None:
-            self.key_to_prev[node.next.key] = prev
-            node.next = None
-        self.push_back(node)
-
-    # @return an integer
-    def get(self, key):		#获取数据
+        
+    """
+    @param: key: An integer
+    @return: An integer
+    """
+    def get(self, key):
+        # write your code here
         if key not in self.key_to_prev:
             return -1
         self.kick(self.key_to_prev[key])
-        return self.key_to_prev[key].next.value
+        return self.key_to_prev[key].next.val
+        
 
-    # @param key, an integer
-    # @param value, an integer
-    # @return nothing
-    def set(self, key, value):     #数据放入缓存
-        if key in self.key_to_prev:	   
+    """
+    @param: key: An integer
+    @param: value: An integer
+    @return: nothing
+    """
+    def set(self, key, value):
+        # write your code here
+        if key in self.key_to_prev:
             self.kick(self.key_to_prev[key])
-            self.key_to_prev[key].next.value = value
+            self.key_to_prev[key].next.val = value
         else:
-            self.push_back(LinkedNode(key, value))  #如果key不存在，则存入新节点
-            if len(self.key_to_prev) > self.capacity:		#如果缓存超出上限
-                self.pop_front()	
+            self.append(LinkedNode(value, key))
+            if self.capacity < len(self.key_to_prev):
+                self.pop_front()
+            
