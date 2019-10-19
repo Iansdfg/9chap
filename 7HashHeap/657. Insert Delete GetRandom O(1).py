8bd352a59d09c9f9eca26497c66642f5ee0a056d
Tian Feng <feng.txy@outlook.com>
2019-10-19 11:27:14 -0700

    Update 657. Insert Delete GetRandom O(1).py

--- 7HashHeap/657. Insert Delete GetRandom O(1).py
+++ 7HashHeap/657. Insert Delete GetRandom O(1).py
@@ -1,22 +1,20 @@
-import random
+from random import randrange
 
 class RandomizedSet:
     
     def __init__(self):
         # do intialization if necessary
-        self.nums = []
-        self.val_to_pos = {}
+        self.pos = dict()
+        self.nums = list()
+
     """
     @param: val: a value to the set
     @return: true if the set did not already contain the specified element or false
     """
     def insert(self, val):
         # write your code here
-        if val in self.val_to_pos:
-            return False
-            
         self.nums.append(val)
-        self.val_to_pos[val] = len(self.nums)-1
+        self.pos[val] = len(self.nums)-1
         return True
 
     """
@@ -25,25 +23,30 @@ class RandomizedSet:
     """
     def remove(self, val):
         # write your code here
-        if val not in self.val_to_pos:
-            return False 
-            
-        index = self.val_to_pos[val]
-        last = self.nums[-1]
+        if val not in self.pos:
+            return False
         
-        self.nums[index] = last
-        self.val_to_pos[last] = index
-
+        val_index = self.pos[val] 
+        last_ele = self.nums[-1]
+        
+        self.nums[val_index] = last_ele
+        del self.pos[last_ele]
+        self.pos[last_ele] = val_index
+        
         self.nums.pop()
-        del self.val_to_pos[val]
         return True
         
+        
+        
+
     """
     @return: Get a random element from the set
     """
     def getRandom(self):
         # write your code here
-        return self.nums[random.randint(0, len(self.nums) - 1)]
+        pos = randrange(len(self.nums))
+        return self.nums[pos]
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
