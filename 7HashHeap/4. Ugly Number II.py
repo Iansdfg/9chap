d498e4bb5d588e213b6654ee60829aa4e0d99375
Tian Feng <feng.txy@outlook.com>
2019-10-19 15:11:56 -0700

    Create  4. Ugly Number II.py

---
+++ 7HashHeap/4. Ugly Number II.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution:
+    """
+    @param n: An integer
+    @return: return a  integer as description.
+    """
+    def nthUglyNumber(self, n):
+        # write your code here
+        heap = [1]
+        visited = set()
+        
+        val = None
+        for i in range(n):
+            val = heapq.heappop(heap)
+            for factor in [2, 3, 5]:
+                if factor * val not in visited:
+                    visited.add(factor * val)
+                    heapq.heappush(heap, factor * val)
+        return val
+                
