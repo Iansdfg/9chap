0e93cdff60b1c26fcce34c4e76b93edd3dd8cffd
Tian Feng <feng.txy@outlook.com>
2019-10-19 16:42:31 -0700

    Create 545. Top k Largest Numbers II.py

---
+++ 7HashHeap/545. Top k Largest Numbers II.py
@@ -0,0 +1,27 @@
+import heapq
+
+class Solution:
+    """
+    @param: k: An integer
+    """
+    def __init__(self, k):
+        # do intialization if necessary
+        self.k = k
+        self.heap = []
+        
+    """
+    @param: num: Number to be added
+    @return: nothing
+    """
+    def add(self, num):
+        # write your code here
+        heapq.heappush(self.heap, num)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+    """
+    @return: Top k element
+    """
+    def topk(self):
+        # write your code here
+        return sorted(self.heap, reverse=True)
