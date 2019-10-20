1cd1291e656e42f71c207251c88f2835aa208ad8
Tian Feng <feng.txy@outlook.com>
2019-10-20 13:07:51 -0700

    Update 104. Merge K Sorted Lists.py

--- 7HashHeap/104. Merge K Sorted Lists.py
+++ 7HashHeap/104. Merge K Sorted Lists.py
@@ -47,5 +47,56 @@ class Solution:
         
         
         
+# merge 
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param lists: a list of ListNode
+    @return: The head of one sorted list.
+    """
+    def mergeKLists(self, lists):
+        # write your code here
+        if not lists: return lists
         
+        return self.devide_and_merge(lists, 0, len(lists)-1)
+        
+        
+    def devide_and_merge(self, lists, start, end):
+        if start == end:
+            return lists[start]
+        
+        mid = (start + end)//2
+        left = self.devide_and_merge(lists, start, mid)
+        right = self.devide_and_merge(lists, mid+1, end)
+        return self.merge_two_list(left, right)
+        
+        
+    def merge_two_list(self, l1, l2):
+        dummy = tail = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+        return dummy.next
+        
+        
+        
+        
+
+
 
