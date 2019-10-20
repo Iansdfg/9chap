0fad5c9e69f015bc1468489fcb029497dce9e7c6
Tian Feng <feng.txy@outlook.com>
2019-10-20 11:26:09 -0700

    Create 104. Merge K Sorted Lists.py

---
+++ 7HashHeap/104. Merge K Sorted Lists.py
@@ -0,0 +1,51 @@
+"Two-Two Merger"
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param lists: a list of ListNode
+    @return: The head of one sorted list.
+    """
+    def mergeKLists(self, lists):
+        # write your code here
+        if not lists: return lists
+        
+        while len(lists) > 1:
+            next_lists = []
+            for i in range(0, len(lists), 2):
+                if i + 1 < len(lists):
+                    new_list = self.merge_two_list(lists[i], lists[i+1])
+                else:
+                    new_list = lists[i]
+                next_lists.append(new_list)
+            lists = next_lists
+        
+        return lists[0]
+        
+        
+    def merge_two_list(self, l1, l2):
+        dummy = tail = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+        return dummy.next
+        
+        
+        
+        
+
