438104312823eadf329ac81919b898287dab0833
Tian Feng <feng.txy@outlook.com>
2019-10-22 12:43:41 -0700

    Create 1311. Lowest Common Ancestor of a Binary Search Tree.py

---
+++ 4Binary Tree - Divide Conquer & Traverse/1311. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,45 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: root of the tree
+    @param p: the node p
+    @param q: the node q
+    @return: find the LCA of p and q
+    """
+    def lowestCommonAncestor(self, root, p, q):
+        # write your code here
+        if not p or not q:
+            return None 
+            
+        lca = self.helper(root, p, q)
+        return lca
+        
+        
+    def helper(self, root, p, q):
+        if not root:
+            return None 
+            
+        p_val, q_val = p.val, q.val
+        
+        if p_val == root.val or q_val == root.val:
+            return root
+        
+        if min(p_val, q_val) < root.val <  max(p_val, q_val):
+            return root
+        elif root.val <= min(p_val, q_val):
+            return self.helper(root.right, p, q)
+        elif root.val >= max(p_val, q_val):
+            return self.helper(root.left, p, q)
+            
+            
+            
+            
+            
+            
