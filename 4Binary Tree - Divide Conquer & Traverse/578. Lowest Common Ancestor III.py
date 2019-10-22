76431228525a4778e58cf26d6c89c61a52606879
Tian Feng <feng.txy@outlook.com>
2019-10-22 12:16:23 -0700

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -1,3 +1,50 @@
+# """
+# Definition of TreeNode:
+# class TreeNode:
+#     def __init__(self, val):
+#         this.val = val
+#         this.left, this.right = None, None
+# """
+
+
+# class Solution:
+#     """
+#     @param: root: The root of the binary tree.
+#     @param: A: A TreeNode
+#     @param: B: A TreeNode
+#     @return: Return the LCA of the two nodes.
+#     """
+#     def lowestCommonAncestor3(self, root, A, B):
+#         # write your code here
+#         if not self.check_exist(root, A) or not self.check_exist(root, B):
+#             return None
+#         return self.helper(root, A, B)
+            
+#     def helper(self, root, A, B):
+#         if not root:
+#             return None 
+#         if root is A or root is B:
+#             return root
+            
+#         left = self.helper(root.left, A, B)
+#         right = self.helper(root.right, A, B)
+        
+#         if left and right:
+#             return root
+#         if not left and right:
+#             return right
+#         if not right and left:
+#             return left
+#         return None
+        
+#     def check_exist(self, root, target):
+#         if not root:
+#             return False
+#         if root is target:
+#             return True
+#         return self.check_exist(root.left, target) or self.check_exist(root.right, target) 
+    
+    
 """
 Definition of TreeNode:
 class TreeNode:
@@ -16,31 +63,52 @@ class Solution:
     """
     def lowestCommonAncestor3(self, root, A, B):
         # write your code here
-        if not self.check_exist(root, A) or not self.check_exist(root, B):
+        a, b, lca = self.helper(root, A, B)
+        if a and b:
+            return lca
+        else:
             return None
-        return self.helper(root, A, B)
-            
+        
+        
+        
     def helper(self, root, A, B):
+        """
+        return if A exist, if B exist, LCA
+        """
         if not root:
-            return None 
-        if root is A or root is B:
-            return root
+            return False, False, None
+        
+        a_left, b_left, lca_left = self.helper(root.left, A, B)
+        a_right, b_right, lca_right = self.helper(root.right, A, B)
+        
+        
+        a_exist = a_left or a_right or A == root
+        b_exist = b_left or b_right or B == root
+        
+        
+        if A == root or B == root:
+            return a_exist, b_exist, root
+        
+        if lca_left and lca_right:
+            return a_exist, b_exist, root
+        elif lca_left and not lca_right:
+            return a_exist, b_exist, lca_left
+        elif lca_right and not lca_left:
+             return a_exist, b_exist, lca_right
+        
+        return a_exist, b_exist, None
+        
             
-        left = self.helper(root.left, A, B)
-        right = self.helper(root.right, A, B)
+
         
-        if left and right:
-            return root
-        if not left and right:
-            return right
-        if not right and left:
-            return left
-        return None
         
-    def check_exist(self, root, target):
-        if not root:
-            return False
-        if root is target:
-            return True
-        return self.check_exist(root.left, target) or self.check_exist(root.right, target) 
+        
+        
+        
+        
+        
+        
+        
+        
+
         
