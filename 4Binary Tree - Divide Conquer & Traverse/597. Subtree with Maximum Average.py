69d038a4c18365f9b6216d4f1abcfdc8306df321
Tian Feng <feng.txy@outlook.com>
2019-10-23 10:02:49 -0700

    Update 597. Subtree with Maximum Average.py

--- 4Binary Tree - Divide Conquer & Traverse/597. Subtree with Maximum Average.py
+++ 4Binary Tree - Divide Conquer & Traverse/597. Subtree with Maximum Average.py
@@ -11,31 +11,28 @@ class Solution:
     @param root: the root of binary tree
     @return: the root of the maximum average of subtree
     """
-    average, node = 0, None
+    avg = 0
+    max_node = None
     
     def findSubtree2(self, root):
         # write your code here
-        self.helper(root)
-        return self.node
-        
-    def helper(self, root):
-        # return # of node, avg 
+        if not root: return None 
+        self.hlper(root)
+        return self.max_node
+    
+    def hlper(self, root):
+        # return summ, size 
         if not root:
-            return 0, 0
+            return 0, 0 
             
-        left_sum, left_size = self.helper(root.left)
-        right_sum, right_size = self.helper(root.right)
-        
+        left_sum, left_size  = self.hlper(root.left)
+        right_sum, right_size = self.hlper(root.right)
         
         summ = left_sum + right_sum + root.val
         size = left_size + right_size + 1
         
-        if not self.node or summ / size > self.average:
-            self.node = root
-            self.average = summ / size
-    
-  
+        if self.max_node == None or summ / size > self.avg:
+            self.avg = summ / size
+            self.max_node = root
+
         return summ, size
-        
-        
-        
