cad98ae0992a9fa7422d4deb672bbc6b581abf91
Tian Feng <feng.txy@outlook.com>
2019-10-23 17:28:49 -0700

    Create 246. Binary Tree Path Sum II.py

---
+++ 4Binary Tree - Divide Conquer & Traverse/246. Binary Tree Path Sum II.py
@@ -0,0 +1,51 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: the root of binary tree
+    @param: target: An integer
+    @return: all valid paths
+    """
+    def binaryTreePathSum2(self, root, target):
+        # write your code here
+        results = []
+        self.dfs(root, target, 0, [], results)
+        return results
+
+        
+    def dfs(self, root, target, level, path, results):
+        if not root:
+            return
+        
+        path.append(root.val)
+        
+        temp = target
+        for i in range(level, -1, -1):
+            temp -= path[i]
+            
+            if temp == 0:
+                results.append(path[i:])
+                
+        self.dfs(root.left, target, level+1, path, results)
+        self.dfs(root.right, target, level+1, path, results)
+        
+        path.pop()
+        
+ 
+            
+            
+            
+            
+            
+            
+            
+            
+
+        
