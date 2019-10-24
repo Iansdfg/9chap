37145832ecb016d922425641a54d211b50e3ec07
Tian Feng <feng.txy@outlook.com>
2019-10-23 19:25:20 -0700

    Update 66. Binary Tree Preorder Traversal.py

--- 4Binary Tree - Divide Conquer & Traverse/66. Binary Tree Preorder Traversal.py
+++ 4Binary Tree - Divide Conquer & Traverse/66. Binary Tree Preorder Traversal.py
@@ -26,3 +26,33 @@ class Solution:
         result.append(root.val)
         self.helper(root.left, result)
         self.helper(root.right, result)
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        if not root: return []
+        stack = []
+        stack.append(root)
+        result = []
+        
+        while stack:
+            curr = stack.pop()
+            result.append(curr.val)
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+            
+        return result
+        
