1d2e195e4c4b220305fdfb69ac10c737c6bad562
Tian Feng <feng.txy@outlook.com>
2019-10-23 22:20:56 -0700

    Update 68. Binary Tree Postorder Traversal.py

--- 4Binary Tree - Divide Conquer & Traverse/68. Binary Tree Postorder Traversal.py
+++ 4Binary Tree - Divide Conquer & Traverse/68. Binary Tree Postorder Traversal.py
@@ -28,3 +28,40 @@ class Solution:
         result.append(root.val)
         
         
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Postorder in ArrayList which contains node values.
+    """
+    def postorderTraversal(self, root):
+        # write your code here
+        stack, res, curr = [], [], root
+        
+        while curr:
+            stack.append(curr)
+            if curr.left:
+                curr = curr.left
+            else:
+                curr = curr.right
+                
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            if stack and stack[-1].left == curr:
+                curr = stack[-1].right
+                while curr:
+                    stack.append(curr)
+                    if curr.left:
+                        curr = curr.left
+                    else:
+                        curr = curr.right
+                        
+        return res
