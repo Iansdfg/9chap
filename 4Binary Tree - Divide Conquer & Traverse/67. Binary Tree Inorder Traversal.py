0d16ba79f11a775754f29dee6bab1f2f370f11c0
Tian Feng <feng.txy@outlook.com>
2019-10-25 09:43:45 -0700

    Update 67. Binary Tree Inorder Traversal.py

--- 4Binary Tree - Divide Conquer & Traverse/67. Binary Tree Inorder Traversal.py
+++ 4Binary Tree - Divide Conquer & Traverse/67. Binary Tree Inorder Traversal.py
@@ -13,51 +13,20 @@ class Solution:
     """
     def inorderTraversal(self, root):
         # write your code here
-        if not root:
-            return []
-            
-        result = []
-        self.helper(root, result)
-        return result
-        
-    def helper(self, root, result):
-        if not root:
-            return
-        
-        self.helper(root.left, result)
-        result.append(root.val)
-        self.helper(root.right, result)
-        
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
-class Solution:
-    """
-    @param root: A Tree
-    @return: Inorder in ArrayList which contains node values.
-    """
-    def inorderTraversal(self, root):
-        # write your code here
         if not root: return []
-        result, stack = [], []
-        curr = root
+        dummy = TreeNode(0)
+        dummy.right = root
+        result, stack = [], [dummy]
         
-        while curr:
-            stack.append(curr)
-            curr = curr.left
-            
         while stack:
             curr = stack.pop()
-            result.append(curr.val)
             if curr.right:
                 curr = curr.right
                 while curr:
                     stack.append(curr)
-                    curr = curr.left 
-         
-        return result
+                    curr = curr.left
+            if stack:
+                result.append(stack[-1].val)
+        return result 
+                    
+            
