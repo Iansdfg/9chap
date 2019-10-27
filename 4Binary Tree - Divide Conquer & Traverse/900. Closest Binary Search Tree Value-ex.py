85601b0e4955d40047a1957b90c98684a0af4d73
Tian Feng <feng.txy@outlook.com>
2019-10-27 16:22:00 -0700

    Rename  900. Closest Binary Search Tree Value.py to  900. Closest Binary Search Tree Value-ex.py

---
+++ 4Binary Tree - Divide Conquer & Traverse/900. Closest Binary Search Tree Value-ex.py
@@ -0,0 +1,37 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    
+    def closestValue(self, root, target):
+        # write your code here
+        lower = root
+        upper = root
+        
+        while root:
+            if target > root.val:
+                lower = root 
+                root = root.right
+                
+            elif target < root.val:
+                upper = root
+                root = root.left
+            else:
+                return root.val
+                
+                
+        if abs(upper.val - target) <= abs(lower.val - target):
+            return upper.val
+        return lower.val
+            
+            
