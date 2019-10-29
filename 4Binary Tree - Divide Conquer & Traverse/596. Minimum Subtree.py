0397ee47ee140348f8722440d3f97f99a3823922
Tian Feng <feng.txy@outlook.com>
2019-10-28 20:54:56 -0700

    Update 596. Minimum Subtree.py

--- 4Binary Tree - Divide Conquer & Traverse/596. Minimum Subtree.py
+++ 4Binary Tree - Divide Conquer & Traverse/596. Minimum Subtree.py
@@ -11,28 +11,28 @@ class Solution:
     @param root: the root of binary tree
     @return: the root of the minimum subtree
     """
-    mini = float('inf')
+    
     mini_node = None 
+    mini = float('inf')
     
     def findSubtree(self, root):
         # write your code here
         self.helper(root)
-        return self.mini_node
+        return self.mini_node 
         
     def helper(self, root):
-        #  return sum
-        if not root:
+        # return summ
+        if not root: 
             return 0
             
         left_sum = self.helper(root.left)
         right_sum = self.helper(root.right)
         
         summ = left_sum + right_sum + root.val
+        
         if summ < self.mini:
             self.mini = summ
             self.mini_node = root
-  
+            
         return summ
-        
-        
         
