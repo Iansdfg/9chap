95fb32d65cf143c87ff4bc6163e83c175b66990f
Tian Feng <feng.txy@outlook.com>
2019-11-02 17:57:50 -0700

    Update 97. Maximum Depth of Binary Tree.py

--- 4Binary Tree - Divide Conquer & Traverse/97. Maximum Depth of Binary Tree.py
+++ 4Binary Tree - Divide Conquer & Traverse/97. Maximum Depth of Binary Tree.py
@@ -16,7 +16,9 @@ class Solution:
         if not root:
             return 0
             
+            
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
+        
         
         return max(left,right)+1
