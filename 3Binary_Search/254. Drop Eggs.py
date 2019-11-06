8eef8b67ed36433fdaf2c355fab7c48e231eb693
Tian Feng <feng.txy@outlook.com>
2019-11-06 15:56:04 -0800

    Update 254. Drop Eggs.py

--- 3Binary_Search/254. Drop Eggs.py
+++ 3Binary_Search/254. Drop Eggs.py
@@ -5,18 +5,19 @@ class Solution:
     """
     def dropEggs(self, n):
         # write your code here
-        right_bound = 1;
-        while (right_bound * (right_bound + 1) / 2 < n):
-            right_bound = right_bound * 2
+        # in short find smallest k that k*(k+1)>=n
+        end = 1 
+        while end * (end + 1) / 2 < n:
+            end *= 2
         
-        start,end  = 1, right_bound
+        start = 0
         while start + 1 < end:
-            mid = (start + end)// 2
-            if mid * (mid + 1) / 2 >= n:
+            mid = (start + end) // 2
+            if mid * (mid + 1) / 2  >= n:
                 end = mid
             else:
                 start = mid
-
+            
         if start * (start + 1) / 2 >= n:
             return start
         else:
