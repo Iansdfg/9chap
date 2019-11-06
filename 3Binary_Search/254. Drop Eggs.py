cb91833a3f81dc65d3a9180a12d4c457b9c9833a
Tian Feng <feng.txy@outlook.com>
2019-11-06 11:53:58 -0800

    Update 254. Drop Eggs.py

--- 3Binary_Search/254. Drop Eggs.py
+++ 3Binary_Search/254. Drop Eggs.py
@@ -5,9 +5,19 @@ class Solution:
     """
     def dropEggs(self, n):
         # write your code here
-        res, sum = 0, 0
-        while sum<n:
-            res += 1
-            sum += res 
-        return res 
+        right_bound = 1;
+        while (right_bound * (right_bound + 1) / 2 < n):
+            right_bound = right_bound * 2
         
+        start,end  = 1, right_bound
+        while start + 1 < end:
+            mid = (start + end)// 2
+            if mid * (mid + 1) / 2 >= n:
+                end = mid
+            else:
+                start = mid
+
+        if start * (start + 1) / 2 >= n:
+            return start
+        else:
+            return end
