c60165bac0de01dfaa8118a9f97b539ec90de0b1
Tian Feng <feng.txy@outlook.com>
2019-11-07 10:46:19 -0800

    Update 414. Divide Two Integers.py

--- 3Binary_Search/414. Divide Two Integers.py
+++ 3Binary_Search/414. Divide Two Integers.py
@@ -6,34 +6,28 @@ class Solution:
     """
     def divide(self, dividend, divisor):
         # write your code here
-        if dividend == 0:
+        if dividend == 0 or divisor == 0:
             return 0
-        
-        if divisor == 0:
-            return 0
-            
         if divisor == 1:
             return dividend
             
-        is_neg = False
-        if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
+        is_neg = False 
+        if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
             is_neg = True
-        
-        dividend, divisor = abs(dividend), abs(divisor)
-        
+            
+        divisor, dividend = abs(divisor), abs(dividend)
+            
         res = 0
         while divisor <= dividend:
-            cnt, summ = 1, divisor
-            while summ + summ < dividend:
+            summ, cnt = divisor, 1
+            while summ + summ <= dividend:
                 summ += summ
-                cnt += cnt
-            res += cnt
+                cnt += cnt 
             dividend -= summ
+            res += cnt
             
         res = min(2147483647, res)
         
         if is_neg:
             return -res
-        return res
-                
-            
+        return res 
