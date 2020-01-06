d3fdec048023d4bcfd46543fd590c9488c8a0acc
Tian Feng <feng.txy@outlook.com>
2020-01-06 14:56:20 -0800

    Update 137. Clone Graph.py

--- 2BFS/137. Clone Graph.py
+++ 2BFS/137. Clone Graph.py
@@ -34,3 +34,62 @@ class Solution:
                 node_dic[dic_key_node].neighbors.append(node_dic[neighbor])
     
         return node_dic[node]
+
+    
+    """
+class UndirectedGraphNode:
+     def __init__(self, x):
+         self.label = x
+         self.neighbors = []
+"""
+from collections import deque
+
+class Solution:
+    """
+    @param node: A undirected graph node
+    @return: A undirected graph node
+    """
+    def get_node(self, node):
+        result = set()
+        queue = deque([node])
+        while queue:
+            node = queue.popleft()
+            for neighbor in node.neighbors:
+                if neighbor not in result:
+                    queue.append(neighbor)
+            result.add(node)
+        return result
+            
+    def cloneGraph(self, node):
+        # write your code here
+        if not node:
+            return node
+        old_nodes = self.get_node(node)
+        
+        old_to_new = dict()
+        for old_node in old_nodes:
+            old_to_new[old_node] = UndirectedGraphNode(old_node.label)
+            
+        for old_node in old_nodes:
+            new_node = old_to_new[old_node]
+            for neighbor in old_node.neighbors:
+                new_neighbor = old_to_new[neighbor]
+                new_node.neighbors.append(new_neighbor)
+                
+        return old_to_new[node]
+            
+    
+    
+    
+    
+    
+    
+    
+    
+            
+            
+            
+            
+            
+            
+            
