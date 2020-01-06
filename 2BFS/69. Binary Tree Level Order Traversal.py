9476b8f4101dc0283d72562bf63dd1e306ee8fd1
Tian Feng <feng.txy@outlook.com>
2020-01-06 13:30:46 -0800

    Update 69. Binary Tree Level Order Traversal.py

--- 2BFS/69. Binary Tree Level Order Traversal.py
+++ 2BFS/69. Binary Tree Level Order Traversal.py
@@ -15,23 +15,21 @@ class Solution:
     """
     def levelOrder(self, root):
         # write your code here
-        if not root:return []
-        
+        if not root:
+            return []
         queue = deque([root])
         result = [[root.val]]
         while queue:
             path = []
             for _ in range(len(queue)):
-                node = queue.popleft()
-                
-                if node.left:
-                    path.append(node.left.val)
-                    queue.append(node.left)
-                    
-                if node.right:
-                    path.append(node.right.val)
-                    queue.append(node.right)
-                    
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                    path.append(curr.left.val)
+                if curr.right:
+                    queue.append(curr.right)
+                    path.append(curr.right.val)
             if path:
                 result.append(path)
         return result
+                    
