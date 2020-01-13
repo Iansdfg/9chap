2f7a80aa15b652b2d54d1f864d000ddc5922f19c
Tian Feng <feng.txy@outlook.com>
2020-01-12 18:12:06 -0800

    Update 137. Clone Graph.py

--- 2BFS/137. Clone Graph.py
+++ 2BFS/137. Clone Graph.py
@@ -4,6 +4,7 @@ class UndirectedGraphNode:
          self.label = x
          self.neighbors = []
 """
+
 from collections import deque
 
 class Solution:
@@ -11,47 +12,36 @@ class Solution:
     @param node: A undirected graph node
     @return: A undirected graph node
     """
-    def get_node(self, node):
-        result = set()
-        queue = deque([node])
-        while queue:
-            node = queue.popleft()
-            for neighbor in node.neighbors:
-                if neighbor not in result:
-                    queue.append(neighbor)
-            result.add(node)
-        return result
-            
     def cloneGraph(self, node):
         # write your code here
         if not node:
             return node
-        old_nodes = self.get_node(node)
-        
-        old_to_new = dict()
-        for old_node in old_nodes:
-            old_to_new[old_node] = UndirectedGraphNode(old_node.label)
             
-        for old_node in old_nodes:
+        nodes = self.get_nodes(node)
+        old_to_new = {}
+    
+        for old_node in nodes:
+            new_node = UndirectedGraphNode(old_node.label)
+            old_to_new[old_node] = new_node
+        # print(old_to_new)
+            
+        for old_node in nodes:
             new_node = old_to_new[old_node]
             for neighbor in old_node.neighbors:
-                new_neighbor = old_to_new[neighbor]
-                new_node.neighbors.append(new_neighbor)
+                new_node.neighbors.append(old_to_new[neighbor])
                 
         return old_to_new[node]
             
-    
-    
-    
-    
-    
-    
-    
-    
-            
-            
-            
-            
-            
-            
-            
+    def get_nodes(self, node):
+        queue = deque([node])
+        nodes = set()
+        
+        while queue:
+            node = queue.popleft()
+            for neighbor in node.neighbors:
+                if neighbor in nodes:
+                    continue
+                queue.append(neighbor)
+            nodes.add(node)
+        return nodes
+        
