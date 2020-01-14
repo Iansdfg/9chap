bd19a35f8c43b1a9aebae80849dc2ef08776f8b6
Tian Feng <feng.txy@outlook.com>
2020-01-13 18:52:52 -0800

    Update 611. Knight Shortest Path.py

--- 2BFS/611. Knight Shortest Path.py
+++ 2BFS/611. Knight Shortest Path.py
@@ -1,43 +1,3 @@
-# """
-# Definition for a point.
-# class Point:
-#     def __init__(self, a=0, b=0):
-#         self.x = a
-#         self.y = b
-# """
-# DIRECTIONS = [
-#     (-2, -1), (-2, 1), (-1, 2), (1, 2),
-#     (2, 1), (2, -1), (1, -2), (-1, -2),
-# ]
-# class Solution:
-#     """
-#     @param grid: a chessboard included 0 (false) and 1 (true)
-#     @param source: a point
-#     @param destination: a point
-#     @return: the shortest path 
-#     """
-#     def shortestPath(self, grid, source, destination):
-#         # write your code here
-#         if not grid or not grid[0]: return -1
-#         queue = collections.deque([(source.x,source.y)])
-#         distance = {(source.x,source.y):0}
-        
-#         while queue:
-#             curr_x, curr_y = queue.popleft()
-#             if (curr_x, curr_y) == (destination.x, destination.y):
-#                     return distance[(curr_x, curr_y)]
-#             for delta_x, delta_y in DIRECTIONS:
-#                 next_x = curr_x + delta_x
-#                 next_y = curr_y + delta_y
-#                 if (next_x, next_y) in distance or not self.is_valid(next_x, next_y,grid):
-#                     continue
-#                 distance[(next_x, next_y)] = distance[(curr_x, curr_y)]+1
-#                 queue.append((next_x, next_y))
-#         return -1  
-    
-#     def is_valid(self, x, y, grid):
-#         m, n = len(grid), len(grid[0])
-#         return 0 <= x < m and 0 <= y < n and not grid[x][y]
 """
 Definition for a point.
 class Point:
@@ -45,6 +5,7 @@ class Point:
         self.x = a
         self.y = b
 """
+from collections import deque
 
 class Solution:
     """
@@ -55,28 +16,37 @@ class Solution:
     """
     def shortestPath(self, grid, source, destination):
         # write your code here
-        
-        if not grid or not grid[0]: return -1
-        queue = collections.deque()
-        queue.append((source.x,source.y))
+        queue = deque([(source.x, source.y)])
         visited = set()
-        visited.add((source.x,source.y))
-        level = 1
+        count = 0
+        
         while queue:
             for _ in range(len(queue)):
-                curr_x, curr_y = queue.popleft()
-                directions = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
-                for delta_x, delta_y in directions:
-                    new_x = curr_x + delta_x
-                    new_y = curr_y + delta_y
-                    if (new_x, new_y) == (destination.x, destination.y):
-                        return level
-                    if self.is_valid(new_x, new_y, grid, visited):
-                        queue.append((new_x,new_y))
-                        visited.add((new_x,new_y))
-            level+=1
+                x, y = queue.popleft()
+                if x == destination.x and y == destination.y:
+                    return count
+                
+                for delta_x, delta_y in [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]:
+                    next_x, next_y = x + delta_x, y + delta_y
+                    if self.is_valid(grid, next_x, next_y, visited):
+                        queue.append((next_x, next_y))
+                        visited.add((next_x, next_y))
+            count += 1 
         return -1
-    
-    def is_valid(self, x, y, grid, visited):
-        m, n = len(grid), len(grid[0])
-        return 0 <= x < m and 0 <= y < n and not grid[x][y] and (x, y) not in visited
+        
+    def is_valid(self,grid, x, y, visited):
+        rows, cols = len(grid), len(grid[0])
+        if (x, y) in visited:
+            return False
+        if x < 0 or x >= rows:
+            return False
+        if y < 0 or y >= cols:
+            return False
+        if grid[x][y] == 1:
+            return False
+        return True
+
+
+
+
+
