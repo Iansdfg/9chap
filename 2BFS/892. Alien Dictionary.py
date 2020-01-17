edb0cbeffdc17891c203ab1962a7de0af7be1bea
Tian Feng <feng.txy@outlook.com>
2020-01-16 23:43:58 -0800

    Update 892. Alien Dictionary.py

--- 2BFS/892. Alien Dictionary.py
+++ 2BFS/892. Alien Dictionary.py
@@ -1,5 +1,4 @@
-from heapq import heappush, heappop, heapify
-
+from heapq import heapify, heappush, heappop
 class Solution:
     """
     @param words: a list of words
@@ -8,49 +7,71 @@ class Solution:
     def alienOrder(self, words):
         # Write your code here
         graph = self.built_graph(words)
-        order = self.topological_sort(graph)
-        return order
+    
+        return self.topological_sort(graph)
+
         
     def built_graph(self, words):
-        char_to_next = {}
+        char_to_nexts = {}
         
-        # initiate graph
-        for word in words:
-            for char in word:
-                if char not in char_to_next:
-                    char_to_next[char] = set()
+        for word_pos in range(len(words)):
+            for char_pos in range(len(words[word_pos])):
+                char = words[word_pos][char_pos]
+                # print(char)
+                if char not in char_to_nexts:
+                    char_to_nexts[char] = set()
                     
         n = len(words)
-        for pos_word in range(n-1):
-            for pos_char in range(min(len(words[pos_word]), len(words[pos_word + 1]))):
-                # to avoid same prefix, if not same, 
-                if words[pos_word][pos_char] != words[pos_word+1][pos_char]:
-                    char_to_next[words[pos_word][pos_char]].add(words[pos_word+1][pos_char])
+        for word_pos in range(n - 1):
+            for char_pos in range(min(len(words[word_pos]), len(words[word_pos+1]))):
+                print(words[word_pos][char_pos], words[word_pos+1][char_pos])
+                if words[word_pos][char_pos] != words[word_pos+1][char_pos]:
+                    char_to_nexts[words[word_pos][char_pos]].add(words[word_pos+1][char_pos])
                     break
                 
-        return char_to_next
+        return char_to_nexts
         
+    
     def topological_sort(self, graph):
+        print('graph: ', graph)
         indegree = {node:0 for node in graph}
         
         for node in graph:
             for neighber in graph[node]:
-                indegree[neighber] += 1
+                indegree[neighber] += 1 
                 
-        queue = [node for node in graph if indegree[node] == 0 ]
+        print(indegree)
+        
+        queue = [node for node in indegree if indegree[node] == 0]
         heapify(queue)
+        print(queue)
         
         order = ''
         while queue:
-            node = heappop(queue)
-            order += node
-            for neighber in graph[node]:
-                indegree[neighber] -= 1 
+            curr = heappop(queue)
+            order += curr 
+            for neighber in graph[curr]:
+                indegree[neighber] -= 1
                 if indegree[neighber] == 0:
                     heappush(queue, neighber)
-                    
+                
+        
         return order if len(order) == len(graph) else ''
-            
         
         
-              
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                    
+                
