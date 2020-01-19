4fe7b99dcbb9b9a96e41e7e68b5737c898099081
Tian Feng <feng.txy@outlook.com>
2020-01-19 15:44:09 -0800

    Update 18. Subsets II.py

--- 6DFS/18. Subsets II.py
+++ 6DFS/18. Subsets II.py
@@ -5,22 +5,19 @@ class Solution:
     """
     def subsetsWithDup(self, nums):
         # write your code here
-        if not nums:
-            return [[]]
         nums.sort()
-        ans = []
-        self.dfs(nums, 0, [], ans)
-        return ans
+        combinations = []
+        self.dfs(nums, 0, [], combinations)
+        return combinations
         
-    def dfs(self, nums, index, path, ans):
-        ans.append(path[:])
+        
+    def dfs(self, nums, index, combination, combinations):
+        combinations.append(combination[:])
+        
         for i in range(index, len(nums)):
-            """
-            eg：[1,1,1,2,2]：
-            当index是2，i是3，会有重复，所以跳过
-            """
-            if i>0 and nums[i] == nums[i-1] and i>index:
+            if i > index and nums[i] == nums[i-1]:
                 continue
-            path.append(nums[i])
-            self.dfs(nums, i+1, path, ans)
-            path.pop()
+            combination.append(nums[i])
+            self.dfs(nums, i + 1, combination, combinations)
+            combination.pop()
+        
