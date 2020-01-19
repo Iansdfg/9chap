6cd9d9be802466fd1be5eef2ec822b0b75f60b88
Tian Feng <feng.txy@outlook.com>
2020-01-19 15:29:44 -0800

    Update 17. Subsets.py

--- 6DFS/17. Subsets.py
+++ 6DFS/17. Subsets.py
@@ -6,37 +6,17 @@ class Solution:
     def subsets(self, nums):
         # write your code here
         nums.sort()
-        ans = []
-        self.findsub(nums,0, [], ans)
-        return ans
+        result = []
+        self.dfs(nums, 0, [], result)
+        return result
         
-    def findsub(self, nums,level, path, ans):
-        if len(nums) == level:
-            ans.append(path[:])
-            return ans
-        path.append(nums[level])
-        self.findsub(nums,level+1, path, ans)
-        path.pop()
-        self.findsub(nums,level+1, path, ans)
-    
-    
-class Solution:
-    """
-    @param nums: A set of numbers
-    @return: A list of lists
-    """
-    def subsets(self, nums):
-        # write your code here
-        nums.sort()
-        results = []
-        self.dfs(nums, 0, [], results)
-        return results
         
-    def dfs(self, nums, index, path, results):
-        results.append(path[:])
-        for i in range(index, len(nums)):
+        
+    def dfs(self, nums, level, path, result):
+        result.append(path[:])
+        
+        for i in range(level, len(nums)):
             path.append(nums[i])
-            self.dfs(nums, i+1, path, results)
+            self.dfs(nums, i + 1, path, result)
             path.pop()
-        
-
+            
