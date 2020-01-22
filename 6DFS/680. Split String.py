3f3e81a15e82d8c9ca154534ff99fbce536b53ab
Tian Feng <feng.txy@outlook.com>
2020-01-21 21:22:01 -0800

    Update 680. Split String.py

--- 6DFS/680. Split String.py
+++ 6DFS/680. Split String.py
@@ -3,21 +3,22 @@ class Solution:
     @param: : a string to be split
     @return: all possible split string array
     """
+
     def splitString(self, s):
         # write your code here
-        if not s:
-            return[[]]
-        ans = []
-        self.dfs(s, [], ans)
-        return ans
-
-    def dfs(self, s, path, ans):
-        if s=='':
-            ans.append(path[:])
-            return ans
-        for i in (1,2):
-            if len(s)>=i:
-                path.append(s[:i])
-                self.dfs(s[i:], path, ans)
-                path.pop()
-       
+        results = []
+        self.dfs(s, [], results)
+        return results
+        
+    def dfs(self, s, path, results):
+        if s == '':
+            results.append(path[:])
+            return
+        
+        for i in range(2):
+            if i >= len(s):
+                continue
+            
+            path.append(s[:i+1])
+            self.dfs(s[i+1:], path, results)
+            path.pop()
