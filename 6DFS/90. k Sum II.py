2db5231a56fe1ede14c719ae06d903cdecfd486b
Tian Feng <feng.txy@outlook.com>
2020-01-22 16:43:14 -0800

    Update 90. k Sum II.py

--- 6DFS/90. k Sum II.py
+++ 6DFS/90. k Sum II.py
@@ -7,50 +7,21 @@ class Solution:
     """
     def kSumII(self, A, k, target):
         # write your code here
-        if A==[]:
-            return[[]]
+        A.sort()
         results = []
         self.dfs(A, k, target, 0, [], results)
         return results
         
     def dfs(self, A, k, target, index, path, results):
-        if len(path)>k:
+        if sum(path) > target or len(path) > k:
             return
-        if target < 0:
-            return
-        if target == 0 and len(path)==k:
+        
+        if sum(path) == target and len(path) == k:
             results.append(path[:])
             return
-        # print(path, results)
+        
         for i in range(index, len(A)):
             path.append(A[i])
-            self.dfs(A, k, target-A[i], i+1, path, results)
-            path.pop()
-        
-class Solution:
-    """
-    @param: A: an integer array
-    @param: k: a postive integer <= length(A)
-    @param: targer: an integer
-    @return: A list of lists of integer
-    """
-    def kSumII(self, A, k, target):
-        # write your code here
-        if A==[]:
-            return[[]]
-        results = []
-        self.dfs(A, k, target, 0, [], results)
-        return results
-        
-    def dfs(self, A, k, target, index, path, results):
-        if target == 0 and k == 0:
-            results.append(path[:])
-            return
-        if k==0 or target < 0:
-            return
-        # print(path, results)
-        for i in range(index, len(A)):
-            path.append(A[i])
-            self.dfs(A, k-1, target-A[i], i+1, path, results)
+            self.dfs(A, k, target, i+1, path, results)
             path.pop()
         
