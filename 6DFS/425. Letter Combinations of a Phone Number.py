d351ef7504a1d5888944763b645b8c337fe14cf0
Tian Feng <feng.txy@outlook.com>
2020-01-24 12:40:07 -0800

    Update 425. Letter Combinations of a Phone Number.py

--- 6DFS/425. Letter Combinations of a Phone Number.py
+++ 6DFS/425. Letter Combinations of a Phone Number.py
@@ -1,32 +1,35 @@
+num_to_alpa = {
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxzy'
+        }
+        
 class Solution:
     """
     @param digits: A digital string
     @return: all posible letter combinations
     """
-    KEYBOARD = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
-        }
     def letterCombinations(self, digits):
         # write your code here
         if digits == '':
             return []
+            
         results = []
-        self.dfs(digits, 0, [], results)
+        self.dfs(digits, [], results)
         return results
         
-    def dfs(self, digits, index, path, results):
-        if len(path) == len(digits):
-            results.append(''.join(path[:]))
+    def dfs(self, digits, path, results):
+        if digits == '':
+            results. append(''.join(path))
             return
-        
-        for letter in self.KEYBOARD[digits[index]]:
-            path.append(letter)
-            self.dfs(digits, index+1, path, results)
+            
+        chars = num_to_alpa[digits[0]]
+        for i in range(len(chars)):
+            path.append(chars[i])
+            self.dfs(digits[1:], path, results)
             path.pop()
