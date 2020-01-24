fb771b5ea5900be41c8fe0c77337b0b60e69adcb
Tian Feng <feng.txy@outlook.com>
2020-01-23 16:51:52 -0800

    Update 33. N-Queens.py

--- 6DFS/33. N-Queens.py
+++ 6DFS/33. N-Queens.py
@@ -14,14 +14,14 @@ class Solution:
         if len(path) == n:
             chessboard = self.draw_chessboard(path)
             results.append(chessboard)
-            
+            # results.append(path[:])
             return
         
         for i in range(n):
             if visited[i]:
                 continue
             
-            if not self.is_valid(path, len(path), i):
+            if not self.is_valid(path, i):
                 continue
             
             visited[i] = 1
@@ -30,19 +30,21 @@ class Solution:
             path.pop()
             visited[i] = 0
             
-    def is_valid(self, cols, row, col):
-        for r, c in enumerate(cols):
-            if c == col:
-                return False
-            if r - c == row - col or r + c == row + col:
+    def is_valid(self, path, i):
+        len_path = len(path)
+        for pos, val in enumerate(path):
+            # 两个点在斜线上的判断方式用一个等式
+            # Math.abs(x1 - x2) == Math.abs(y1 - y2) 即可。
+            # 根据x1 + y1 == x2 + y2 和 x1 - y1 == x2 - y2
+            if pos - val == len_path - i or pos + val == len_path + i:
                 return False
         return True
         
-    def draw_chessboard(self, cols):
-        n = len(cols)
+    def draw_chessboard(self, path):
+        n = len(path)
         board = []
         for i in range(n):
-            row = ['Q' if j == cols[i] else '.' for j in range(n)]
+            row = ['Q' if j == path[i] else '.' for j in range(n)]
             board.append(''.join(row))
         return board
             
