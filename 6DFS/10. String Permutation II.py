3c28dfb8bc3d7fbf25962dc742b16ef3ffb0ea05
Tian Feng <feng.txy@outlook.com>
2020-01-27 11:26:52 -0800

    Update 10. String Permutation II.py

--- 6DFS/10. String Permutation II.py
+++ 6DFS/10. String Permutation II.py
@@ -6,28 +6,29 @@ class Solution:
     def stringPermutation2(self, str):
         # write your code here
         str = sorted(str)
-        visited = [0] * len(str)
         results = []
-        self.dfs(str, [], results, visited)
+        visited = [0] * len(str)
+        self.dfs(str, visited, [], results)
         return results
-           
-    def dfs(self, str, path, results, visited):
+        
+    def dfs(self, str, visited, path, results):
         
         if len(path) == len(str):
+            # print(path)
             results.append(''.join(path[:]))
-            
+            return
+        
         for i in range(len(str)):
             
             if visited[i]:
                 continue
             
-            if i > 0 and str[i] == str[i-1] and not visited[i-1]:
+            if i>0 and str[i] == str[i - 1] and not visited[i - 1]:
                 continue
             
+            visited[i] = 1 
             path.append(str[i])
-            visited[i] = 1
-            self.dfs(str, path, results, visited)
-            visited[i] = 0
+            self.dfs(str, visited, path, results)
             path.pop()
+            visited[i] = 0
             
-     
