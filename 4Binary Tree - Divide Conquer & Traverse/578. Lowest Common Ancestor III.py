b70beb8c8b8e70936c607e3ae348d377934ae0d5
Tian Feng <feng.txy@outlook.com>
2020-01-28 17:46:27 -0800

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -5,6 +5,8 @@ class TreeNode:
         this.val = val
         this.left, this.right = None, None
 """
+
+
 class Solution:
     """
     @param: root: The root of the binary tree.
@@ -14,33 +16,28 @@ class Solution:
     """
     def lowestCommonAncestor3(self, root, A, B):
         # write your code here
-        if not root:
-            return None 
-            
-        A_exist, B_exist, LCA = self.helper(root, A, B)
-        if A_exist and B_exist:
-            return LCA
-        else:
-            return None
-     
+        has_A, has_B, lca  = self.helper(root, A, B)
+        
+        return lca 
+        
+        
     def helper(self, root, A, B):
-        # return A_exist, B_exist, LCA
+        # return has_A, has_B, lca 
         if not root:
-            return False, False, None
+            return False, False, None 
             
-        left_A_exist, left_B_exist, left_LCA = self.helper(root.left, A, B)
-        right_A_exist, right_B_exist, right_LCA = self.helper(root.right, A, B)
+        left_has_A, left_has_B, left_lca = self.helper(root.left, A, B)
+        right_has_A, right_has_B, right_lca = self.helper(root.right, A, B)
         
-        A_exist = left_A_exist or right_A_exist or root is A
-        B_exist =  left_B_exist or right_B_exist or root is B
+        has_A = left_has_A or right_has_A or A == root 
+        has_B = left_has_B or right_has_B or B == root
         
-        if A == root or B == root:
-            return A_exist, B_exist, root
-  
-        if right_LCA and left_LCA:
-            return A_exist, B_exist, root
-        elif right_LCA:
-            return A_exist, B_exist, right_LCA
-        elif left_LCA:
-            return A_exist, B_exist, left_LCA
-        return A_exist, B_exist, None
+        if has_A and has_B:
+            if left_lca and not right_lca:
+                return has_A, has_B, left_lca
+            elif right_lca and not left_lca:
+                return has_A, has_B, right_lca
+            else:
+                return has_A, has_B, root
+        else:
+            return has_A, has_B, None 
