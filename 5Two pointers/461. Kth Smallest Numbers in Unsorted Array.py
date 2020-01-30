8d35c08ba73c9670a21dbb100bcd0e746ff7b214
Tian Feng <feng.txy@outlook.com>
2020-01-29 18:33:19 -0800

    Update 461. Kth Smallest Numbers in Unsorted Array.py

--- 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
+++ 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
@@ -1,4 +1,4 @@
-vclass Solution:
+class Solution:
     """
     @param k: An integer
     @param nums: An integer array
@@ -6,30 +6,39 @@ vclass Solution:
     """
     def kthSmallest(self, k, nums):
         # write your code here
-        if not nums or k<1 or k>len(nums):
-            return None
-        return self.quick_select(k-1, nums, 0, len(nums)-1)
-
-    def quick_select(self, k, nums, start, end):
-        if start>=end:
-            return nums[k]
+        if k > len(nums):
+            return -1 
             
+        start, end = 0, len(nums)-1
+        return self.quick_select(k - 1, nums, start, end)
+        
+        
+    def quick_select( self, k, nums, start, end):
+        
+        if start == end:
+            return nums[start]
+    
         left, right = start, end
-        pivot = nums[(start + end)//2]
-        while left<=right:
-            while left <= right and nums[left]<pivot:
-                left+=1
-            while left <= right and nums[right]>pivot:
-                right-=1
-            if left<=right:
-                nums[right], nums[left] = nums[left], nums[right] 
-                left+=1
-                right-=1
+        
+        pivot = nums[(left + right)//2]
+        
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1 
+            while left <= right and nums[right] > pivot:
+                right -= 1 
+            if left <= right:
+                nums[right], nums[left] = nums[left], nums[right]
+                left +=1 
+                right -= 1
+                
             
         if k <= right:
-            return self.quick_select(k, nums, start, right)
-        elif k >= left:
-            return self.quick_select(k, nums, left, end)
-        return nums[k]
+            return self.quick_select(k , nums, start, right)
+        elif left <= k:
+            return self.quick_select(k , nums, left, end)
+        else:
+            return nums[k]
                 
-  
+
+            
