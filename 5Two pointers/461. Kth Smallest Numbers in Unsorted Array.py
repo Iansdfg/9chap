e121ebc9737b5e544ae00f4038738ccaf00b477e
Tian Feng <feng.txy@outlook.com>
2020-02-05 12:09:48 -0800

    Update 461. Kth Smallest Numbers in Unsorted Array.py

--- 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
+++ 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
@@ -6,39 +6,38 @@ class Solution:
     """
     def kthSmallest(self, k, nums):
         # write your code here
-        if k > len(nums):
-            return -1 
-            
-        start, end = 0, len(nums)-1
-        return self.quick_select(k - 1, nums, start, end)
+        start, end = 0, len(nums) - 1 
+        return self.helper(k - 1 , nums, start, end)
         
         
-    def quick_select( self, k, nums, start, end):
-        
+    def helper(self, k, nums, start, end):
         if start == end:
             return nums[start]
-    
+            
+            
         left, right = start, end
-        
-        pivot = nums[(left + right)//2]
+    
+        pivot =nums[(left + right)//2] 
         
         while left <= right:
             while left <= right and nums[left] < pivot:
                 left += 1 
             while left <= right and nums[right] > pivot:
                 right -= 1 
+                
             if left <= right:
-                nums[right], nums[left] = nums[left], nums[right]
-                left +=1 
-                right -= 1
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1 
+                right -= 1 
                 
-            
+                
         if k <= right:
-            return self.quick_select(k , nums, start, right)
-        elif left <= k:
-            return self.quick_select(k , nums, left, end)
-        else:
-            return nums[k]
-                
-
+            return self.helper(k, nums, start, right)
             
+        elif k >= left :
+            return self.helper(k, nums, left, end)
+            
+        return nums[k] 
+            
+            
+        
