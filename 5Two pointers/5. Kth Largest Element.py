f031bdabb8d0301fecc712af8c4054b9d356f61f
Tian Feng <feng.txy@outlook.com>
2020-02-05 19:38:49 -0800

    Update 5. Kth Largest Element.py

--- 5Two pointers/5. Kth Largest Element.py
+++ 5Two pointers/5. Kth Largest Element.py
@@ -6,29 +6,34 @@ class Solution:
     """
     def kthLargestElement(self, n, nums):
         # write your code here
-        if not nums or n<1 or n>len(nums):
-            return None
-        return self.quick_select(n-1, nums, 0, len(nums)-1)
-    
-    
-    def quick_select(self, n, nums, start, end):
+        start, end = 0, len(nums) - 1 
+        return self.helper(n-1, nums, start, end)
+        
+    def helper(self, n, nums, start, end):
         if start == end:
-            return nums[n]
+            return nums[start]
+        
         left, right = start, end
-        pivot = nums[(start + end) // 2]
-        while left<=right:
-            while left <= right and nums[left]>pivot:
-                left+=1
-            while left <= right and nums[right]<pivot:
-                right-=1
+        
+        pivot = nums[ (left + right) // 2 ]
+        
+        while left <= right:
+            while left <= right and nums[left] > pivot:
+                left += 1 
+                
+            while left <= right and nums[right] < pivot:
+                right -= 1 
+                
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
-                left+=1
-                right-=1
-            
-        if n<=right:
-            return self.quick_select(n, nums, start, right)
-        elif n>=left:
-            return self.quick_select(n, nums, left, end)
-       
-        return nums[n]
+                left += 1 
+                right -= 1 
+                
+        if n <= right:
+            return self.helper(n, nums, start, right)
+        elif n >= left:
+            return self.helper(n, nums, left, end)
+        else:
+            return nums[n]
+        
+        
