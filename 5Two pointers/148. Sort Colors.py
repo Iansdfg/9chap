83c6809e08da2f7aa67412f1b6eeb1f8ea3ceada
Tian Feng <feng.txy@outlook.com>
2020-02-05 17:07:17 -0800

    Update 148. Sort Colors.py

--- 5Two pointers/148. Sort Colors.py
+++ 5Two pointers/148. Sort Colors.py
@@ -1,4 +1,3 @@
-###two-pointer
 class Solution:
     """
     @param nums: A list of integer which is 0, 1 or 2 
@@ -6,51 +5,17 @@ class Solution:
     """
     def sortColors(self, nums):
         # write your code here
-        one_start = self.sort_by_target(nums, 0, 0)
-        # print(one_start)
-        two_start = self.sort_by_target(nums, 1, one_start)
+        left, mid, right = 0, 0, len(nums) - 1 
         
-        return nums
-
-    def sort_by_target(self, nums, target,start):
-        left, right = start, len(nums)-1
-        
-        while left<=right:
-            while left<=right and nums[left]<= target:
-                left+=1
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                mid += 1 
+                left += 1 
                 
-            while left<=right and nums[right]> target:
-                right-=1
+            elif nums[mid] == 1:
+                mid += 1 
                 
-            if  left<=right:
-                nums[left],nums[right] = nums[right], nums[left]
-                left+=1
-                right-=1
-                
-        return left
-       
-        
-###three-pointer
-class Solution:
-    """
-    @param nums: A list of integer which is 0, 1 or 2 
-    @return: nothing
-    """
-    def sortColors(self, nums):
-        # write your code here
-        if not nums:return nums
-        
-        left, right = 0, len(nums)-1
-        
-        i = 0
-        while i<=right:
-            if nums[i] == 0:
-                nums[left], nums[i] = nums[i], nums[left]
-                i+=1
-                left+=1
-            elif nums[i] == 1:
-                i+=1
-            elif nums[i] == 2:
-                nums[right], nums[i] = nums[i], nums[right]
-                right-=1
-           
+            elif nums[mid] == 2:
+                nums[right], nums[mid] = nums[mid], nums[right]
+                right -= 1 
