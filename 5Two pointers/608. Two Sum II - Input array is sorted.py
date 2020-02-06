f2ae5a99a4e5a7e42137cd48bb049c8a12d54cb0
Tian Feng <feng.txy@outlook.com>
2020-02-06 11:20:11 -0800

    Update 608. Two Sum II - Input array is sorted.py

--- 5Two pointers/608. Two Sum II - Input array is sorted.py
+++ 5Two pointers/608. Two Sum II - Input array is sorted.py
@@ -6,28 +6,12 @@ class Solution:
     """
     def twoSum(self, nums, target):
         # write your code here
-        dic = dict()
-        for pos, val in enumerate(nums):
-            if val not in dic:
-                dic[target-val] = pos+1
+        left, right = 0, len(nums) - 1 
+        while left <= right:
+            if nums[left] + nums[right] > target:
+                right -= 1 
+            elif nums[left] + nums[right] < target:
+                left += 1 
             else:
-                return [dic[val], pos+1]
-
-class Solution:
-    """
-    @param nums: an array of Integer
-    @param target: target = nums[index1] + nums[index2]
-    @return: [index1 + 1, index2 + 1] (index1 < index2)
-    """
-    def twoSum(self, nums, target):
-        # write your code here
-        l,r = 0, len(nums)-1
-        while l<r:
-            summ = nums[l] + nums[r]
-            if summ == target:
-                return [l+1,r+1]
-            elif summ<target:
-                l+=1
-            elif summ>target:
-                r-=1
-        return []
+                return [left + 1, right + 1]
+        
