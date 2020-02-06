abf547a2caa08fd056d4e9e162ad45ed202ff45b
Tian Feng <feng.txy@outlook.com>
2020-02-06 12:07:32 -0800

    Update 57. 3Sum.py

--- 5Two pointers/57. 3Sum.py
+++ 5Two pointers/57. 3Sum.py
@@ -6,24 +6,28 @@ class Solution:
     def threeSum(self, numbers):
         # write your code here
         numbers.sort()
-        res = []
-        for pos, number in enumerate(numbers):
-            if pos>0 and numbers[pos] == numbers[pos-1]:
+        results = []
+        
+        for pos in range(len(numbers)):
+            if pos > 0 and numbers[pos] == numbers[pos - 1]:
                 continue
-            left, right = pos+1, len(numbers)-1
+            
+            left, right = pos + 1, len(numbers) - 1 
             while left < right:
-                three_sum = number+ numbers[left] + numbers[right]
+                three_sum = numbers[pos] + numbers[left] + numbers[right]
                 if three_sum == 0:
-                    res.append([number, numbers[left], numbers[right]]) 
-                    
-                    while left < right and numbers[left] == numbers[left+1]:
-                        left+=1
-                    while left < right and numbers[right] == numbers[right-1]:
-                        right-=1                  
-                    left+=1
-                    right-=1              
+                    results.append([numbers[pos], numbers[left],  numbers[right]])
+                    left += 1 
+                    right -= 1 
+                elif three_sum > 0:
+                    right -= 1 
                 elif three_sum < 0:
-                    left += 1
-                elif three_sum  > 0:
-                    right -= 1
-        return res
+                    left += 1 
+                    
+                while left < right and left > pos + 1 and numbers[left] == numbers[left - 1]:
+                    left += 1 
+                while left < right and right < len(numbers) - 1  and numbers[right] == numbers[right + 1]:
+                    right -= 1 
+                    
+        return results
+                
