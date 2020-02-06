f08819943effda59e97423d854343028b13e6c66
Tian Feng <feng.txy@outlook.com>
2020-02-06 12:45:40 -0800

    Update 382. Triangle Count.py

--- 5Two pointers/382. Triangle Count.py
+++ 5Two pointers/382. Triangle Count.py
@@ -6,43 +6,13 @@ class Solution:
     def triangleCount(self, S):
         # write your code here
         S.sort()
-        ans = 0
-        for i in range(len(S)):
-            left, right = 0,i-1
-            while left<right:
-                if S[left]+S[right]>S[i]:
-                    ans += right-left
-                    right-=1
+        result = 0
+        for pos in range(len(S)):
+            left, right = 0, pos - 1  
+            while left < right:
+                if S[left] + S[right] > S[pos]:
+                    result += right - left
+                    right -= 1 
                 else:
-                    left+=1 
-        return ans
-                
-class Solution:
-    """
-    @param S: A list of integers
-    @return: An integer
-    """
-    def triangleCount(self, S):
-        # write your code here
-        S.sort()
-        S.reverse()
-        length = len(S)
-        res = 0
-        for pos, num in enumerate(S):
-            left = pos+1
-            res+=self.greater_than(S[left:], num)
-        return res
-    
-    
-    def greater_than(self, nums, target):
-        nums.sort()
-        ans = 0
-        left, right = 0, len(nums)-1
-        while left<right:
-            two_sum = nums[left] + nums[right]
-            if two_sum>target:
-                ans+=right-left
-                right-=1
-            else:
-                left+=1
-        return ans
+                    left += 1 
+        return result
