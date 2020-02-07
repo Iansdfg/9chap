fe548059db8e12e8235e39abadd46a087ea2f5cc
Tian Feng <feng.txy@outlook.com>
2020-02-06 17:00:34 -0800

    Update 609. Two Sum - Less than or equal to target.py

--- 5Two pointers/609. Two Sum - Less than or equal to target.py
+++ 5Two pointers/609. Two Sum - Less than or equal to target.py
@@ -6,16 +6,16 @@ class Solution:
     """
     def twoSum5(self, nums, target):
         # write your code here
-        l, r = 0, len(nums)-1
-        res = 0
         nums.sort()
-        while l < r:
-            two_sum = nums[l] + nums[r]
-            if two_sum > target:
-                r -= 1
-            else:
-                res += r - l
-                l += 1
-        return res
+        count = 0
+        left, right = 0, len(nums)-1 
+        while left < right:
+            two_sum = nums[left] + nums[right]
+            if two_sum <= target:
+                count += right - left
+                left += 1 
+                
+            elif two_sum > target:
+                right -= 1 
         
-        
+        return count
