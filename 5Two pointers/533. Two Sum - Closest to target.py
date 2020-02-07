d2bc1022c6f62aa94eea9c23703b431ae023e0c4
Tian Feng <feng.txy@outlook.com>
2020-02-06 22:52:11 -0800

    Update 533. Two Sum - Closest to target.py

--- 5Two pointers/533. Two Sum - Closest to target.py
+++ 5Two pointers/533. Two Sum - Closest to target.py
@@ -7,18 +7,20 @@ class Solution:
     def twoSumClosest(self, nums, target):
         # write your code here
         nums.sort()
+        left, right = 0, len(nums) - 1 
         res = float('inf')
-        left, right = 0, len(nums)-1
-        while left<right:
-            two_sum = nums[left]+nums[right]
-            if two_sum == target:
-                return 0
-            elif two_sum > target:
-                res = min(res, abs(two_sum-target))
-                right-=1
+        
+        while left < right:
+            two_sum = nums[left] + nums[right]
+            if two_sum > target:
+                res = min(res, abs(two_sum - target))
+                right -= 1 
             elif two_sum < target:
-                res = min(res, abs(two_sum-target))
-                left+=1
-        return res
+                res = min(res, abs(two_sum - target))
+                left += 1 
+            elif two_sum == target:
+                return 0
                 
+        return res 
                 
+            
