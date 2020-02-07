53887f51029126801857239f221c625a5334513f
Tian Feng <feng.txy@outlook.com>
2020-02-06 17:06:36 -0800

    Update 443. Two Sum - Greater than target.py

--- 5Two pointers/443. Two Sum - Greater than target.py
+++ 5Two pointers/443. Two Sum - Greater than target.py
@@ -4,20 +4,16 @@ class Solution:
     @param target: An integer
     @return: an integer
     """
-
     def twoSum2(self, nums, target):
         # write your code here
         nums.sort()
-        ans = 0
-        left, right = 0, len(nums)-1
-        while left<right:
-            two_sum = nums[left]+nums[right]
-            if two_sum>target:
-                ans += right-left
-                right-=1
+        count = 0
+        left, right = 0, len(nums) - 1 
+        while left < right:
+            two_sum = nums[left] + nums[right]
+            if two_sum > target:
+                count += right - left
+                right -= 1 
             else:
-                left+=1
-
-        return ans
-            
-
+                left += 1 
+        return count
