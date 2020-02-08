3984a2212bbb9b75eeb08d547482bcc30d941dcc
Tian Feng <feng.txy@outlook.com>
2020-02-07 23:59:02 -0800

    Update 521. Remove Duplicate Numbers in Array.py

--- 5Two pointers/521. Remove Duplicate Numbers in Array.py
+++ 5Two pointers/521. Remove Duplicate Numbers in Array.py
@@ -5,30 +5,16 @@ class Solution:
     """
     def deduplication(self, nums):
         # write your code here
-        if len(nums) == 0: return 0 
+        if len(nums) == 0:
+            return 0
         nums.sort()
-
-        res = 1
-        for i in range(1,len(nums)):
-            if nums[i-1]!=nums[i]:
-                nums[res] = nums[i]
-                res+=1
-        return res
+        slow = 1 
+        for fast in range(1, len(nums)):
+            if nums[fast - 1] != nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1 
                 
-    class Solution:
-    """
-    @param nums: an array of integers
-    @return: the number of unique integers
-    """
-    def deduplication(self, nums):
-        # write your code here
-        visited = set()
-        left,right = 0,0
-        while right<len(nums):
-            if nums[right] not in visited:
-                visited.add(nums[right])
-                nums[left],nums[right] = nums[right],nums[left]
-                left += 1
-            right+=1
-        return left
- 
+        return slow
+                
+            
+            
