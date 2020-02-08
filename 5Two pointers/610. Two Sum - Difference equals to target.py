754028c2648f30080f9ab2b168107a4b4c9378ba
Tian Feng <feng.txy@outlook.com>
2020-02-07 21:32:16 -0800

    Update 610. Two Sum - Difference equals to target.py

--- 5Two pointers/610. Two Sum - Difference equals to target.py
+++ 5Two pointers/610. Two Sum - Difference equals to target.py
@@ -6,37 +6,18 @@ class Solution:
     """
     def twoSum7(self, nums, target):
         # write your code here
+        num_pos = []
+        for pos, val in enumerate(nums):
+            num_pos.append([val, pos]) 
+        
+        num_pos.sort()
         target = abs(target)
-        numbers = [(pos+1, val) for pos, val in enumerate(nums)]
-        numbers = sorted(numbers, key = lambda x:x[1])
-        dic = {}
-        for ele in numbers:
-            # print(ele)
-            if ele[1] not in dic:
-                dic[ele[1]+target] = ele[0]
-            else:
-                return sorted([dic[ele[1]], ele[0]])
-            
-            
-class Solution:
-    """
-    @param nums: an array of Integer
-    @param target: an integer
-    @return: [index1 + 1, index2 + 1] (index1 < index2)
-    """
-    def twoSum7(self, nums, target):
-        # write your code here
-        target = abs(target)
-        numbers = [(pos+1, val) for pos, val in enumerate(nums)]
-        numbers = sorted(numbers, key = lambda x:x[1])
-        dic = {}
-        length = len(numbers)
-        right = 0
-        for left in range(length):
-            if right == left:
-                right+=1
-            while right<length and numbers[right][1]-numbers[left][1]<target:
-                right+=1
-            if right<length and numbers[right][1]-numbers[left][1]==target:
-                return sorted([numbers[left][0], numbers[right][0]])
-            
+        right = 1 
+        
+        for left in range(len(num_pos)):
+            while right < len(num_pos) and num_pos[right][0] - num_pos[left][0] < target:
+                right += 1 
+            if left != right and num_pos[right][0] - num_pos[left][0] == target:
+                result = [num_pos[right][1]+1, num_pos[left][1]+1]
+                result.sort()
+                return result
