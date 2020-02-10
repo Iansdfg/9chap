9dbebed697c193fe08b6dc1ca2df64cb5c674c14
Tian Feng <feng.txy@outlook.com>
2020-02-10 13:33:46 -0800

    Update 130. Heapify.py

--- 7HashHeap/130. Heapify.py
+++ 7HashHeap/130. Heapify.py
@@ -5,44 +5,27 @@ class Solution:
     """
     def heapify(self, A):
         # write your code here
-        for order in range(len(A)):
-            self.siftup(A, order)
+        # 从 n//2 位开始
+        for index in range(len(A)//2, -1, -1):
+            self.siftdown(A, index)
             
             
-    def siftup(self, A, order):
-        while order:
-            father_order = (order - 1) // 2
-            if A[order] > A[father_order]:
-                break
-            A[order], A[father_order] = A[father_order], A[order]
-            order = father_order
-
-            
-            
-class Solution:
-    """
-    @param: A: Given an integer array
-    @return: nothing
-    """
-    def heapify(self, A):
-        # write your code here
-        for index in range(len(A)-1, -1, -1):
-            self.siftup(A, index)
-            
-            
-    def siftup(self, A, index):
-        while index * 2 + 1 < len(A):
+    def siftdown(self, A, index):
+        # 如果在长度内
+        while index < len(A):
             left_child = index * 2 + 1
             right_child = index * 2 + 2 
             mini_index = index
+            # 如果左子小，左子的index为最小
             if left_child < len(A) and A[left_child] < A[mini_index]:
                 mini_index = left_child
+            # 如果右子小，右子的index为最小
             if right_child < len(A) and A[right_child] < A[mini_index]:
                 mini_index = right_child
-                
+            # 如果父节点最小 进入下一轮
             if mini_index == index:
                 break 
-            
+
             A[index], A[mini_index] = A[mini_index], A[index]
             index = mini_index
             
