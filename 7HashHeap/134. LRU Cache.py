394fc26f97ee36e843edc719310ed16f5e83be2a
Tian Feng <feng.txy@outlook.com>
2020-02-11 15:40:19 -0800

    Update 134. LRU Cache.py

--- 7HashHeap/134. LRU Cache.py
+++ 7HashHeap/134. LRU Cache.py
@@ -1,11 +1,9 @@
 class LinkedNode:
-    
-    def __init__(self, val = None, key = None, next=None):
+    def __init__(self, val = None, key = None, next = None):
         self.val = val
         self.key = key
         self.next = next
-        
-    # do intialization if necessary
+
 class LRUCache:
     """
     @param: capacity: An integer
@@ -14,33 +12,35 @@ class LRUCache:
         # do intialization if necessary
         self.key_to_prev = dict()
         self.capacity = capacity
-        self.dummy = LinkedNode()
+        self.dummy = LinkedNode(0)
         self.tail = self.dummy
         
-        
-    # prev--node--next--...---tail
-    # prev--next--...---tail--node
-    def kick(self, prev):
-        node = prev.next
+    # prev--node--next---...--tail
+    # prev--next---...--tail--node
+    def kick_append(self, prev):
+        node = prev.next 
         if node == self.tail:
             return
-        prev.next = node.next
-        if node.next:
-            self.key_to_prev[node.next.key] = prev
-            node.next = None
+        nextt = node.next
+        prev.next = nextt
+        
+        if nextt:
+            self.key_to_prev[nextt.key] = prev
+            node.next = None 
         self.append(node)
-    
+
     def append(self, node):
+        self.tail.next = node 
         self.key_to_prev[node.key] = self.tail
-        self.tail.next = node
         self.tail = node
         
-    def pop_front(self):
+    def pop_front(self,):
         head = self.dummy.next
+        nextt = head.next 
+        self.key_to_prev[nextt.key] = self.dummy
+        self.dummy.next = nextt
+        head.next = None 
         del self.key_to_prev[head.key]
-        self.dummy.next = head.next
-        self.key_to_prev[head.next.key] = self.dummy
-        
         
     """
     @param: key: An integer
@@ -49,9 +49,14 @@ class LRUCache:
     def get(self, key):
         # write your code here
         if key not in self.key_to_prev:
-            return -1
-        self.kick(self.key_to_prev[key])
-        return self.key_to_prev[key].next.val
+            return -1 
+        
+        prev = self.key_to_prev[key]
+        node = prev.next
+        
+        self.kick_append(prev)
+        
+        return node.val
         
     """
     @param: key: An integer
@@ -61,10 +66,16 @@ class LRUCache:
     def set(self, key, value):
         # write your code here
         if key in self.key_to_prev:
-            self.kick(self.key_to_prev[key])
-            self.key_to_prev[key].next.val = value
+            prev = self.key_to_prev[key]
+            node = prev.next 
+            node.val = value
+            
+            self.kick_append(prev)
+            
         else:
-            self.append(LinkedNode(value, key))
-            if self.capacity < len(self.key_to_prev):
+            node = LinkedNode(value, key)
+            self.append(node)
+            if len(self.key_to_prev) > self.capacity:
                 self.pop_front()
-           
+                
+  
