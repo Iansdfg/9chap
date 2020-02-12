2fd28373c86216648aa9bb4a04e3a5b34d7229ca
Tian Feng <feng.txy@outlook.com>
2020-02-12 13:13:42 -0800

    Update 960. First Unique Number in Data Stream II.py

--- 7HashHeap/960. First Unique Number in Data Stream II.py
+++ 7HashHeap/960. First Unique Number in Data Stream II.py
@@ -1,16 +1,17 @@
-class LinkedNode:
-    def __init__(self, val):
+class linkedNode:
+    def __init__(self, val = None, key = None, next = None):
+        # do intialization if necessar
         self.val = val
-        self.next = None
-
-
+        self.key = key
+        self.next = next
+        
 class DataStream:
     def __init__(self):
         # do intialization if necessary
-        self.dummy = LinkedNode(-1)
-        self.tail = self.dummy 
-        self.num_to_prev = dict()
-        self.dupulicated = set()
+        self.key_to_prev = dict()
+        self.dummy = linkedNode(0)
+        self.tail = self.dummy
+        self.visited = set()
           
     """
     @param num: next number in stream
@@ -18,41 +19,39 @@ class DataStream:
     """
     def add(self, num):
         # write your code here
-        if num in self.dupulicated:
+        if num in self.visited:
             return
         
-        if num not in self.num_to_prev:
-            self.push_back(num)
-            return
-        
-        self.dupulicated.add(num)
-        self.remove(num)
+        if num in self.key_to_prev:
+            self.kick(num)
+            self.visited.add(num)
+        else:
+            self.tail.next = linkedNode(num)
+            self.key_to_prev[num] = self.tail
+            self.tail = self.tail.next
             
-            
-    def remove(self, num):
-        prev = self.num_to_prev[num]
-        del self.num_to_prev[num]
+                
+    def kick(self,key):
+        prev = self.key_to_prev[key]
+        node = prev.next
+        nextt = node.next
         
-        prev.next = prev.next.next
-        if prev.next:
-            self.num_to_prev[prev.next.val] = prev
+        prev.next = nextt
+        node.next = None
+        if nextt:
+            self.key_to_prev[nextt.val] = prev
         else:
             self.tail = prev
-        
-    
-    def push_back(self, num):
-        node = LinkedNode(num)
-        self.tail.next = node
-        self.num_to_prev[num] = self.tail
-        self.tail = node
-        
+        del self.key_to_prev[node.val]
+
 
     """
     @return: the first unique number in stream
     """
     def firstUnique(self):
         # write your code here
-        if not self.dummy.next:
-            return None
-        return self.dummy.next.val
+        if self.dummy.next:
+            return self.dummy.next.val
+        else:
+            return -1 
         
