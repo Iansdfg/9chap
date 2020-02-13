6e032b343af4842bfefd988c1b2326f3a8dd37de
Tian Feng <feng.txy@outlook.com>
2020-02-13 13:43:09 -0800

    Create 165. Merge Two Sorted Lists.py

---
+++ 7HashHeap/165. Merge Two Sorted Lists.py
@@ -0,0 +1,34 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param l1: ListNode l1 is the head of the linked list
+    @param l2: ListNode l2 is the head of the linked list
+    @return: ListNode head of linked list
+    """
+    def mergeTwoLists(self, l1, l2):
+        # write your code here
+        dummy = ListNode(0)
+        tail = dummy 
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+            
+        return dummy.next
