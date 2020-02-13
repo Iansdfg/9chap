c1e6745310833325746a2ef7f2a6ea4e40a72ab4
Tian Feng <feng.txy@outlook.com>
2020-02-13 15:22:55 -0800

    Update 601. Flatten 2D Vector.py

--- 7HashHeap/601. Flatten 2D Vector.py
+++ 7HashHeap/601. Flatten 2D Vector.py
@@ -35,3 +35,31 @@ class Vector2D(object):
 # Your Vector2D object will be instantiated and called as such:
 # i, v = Vector2D(vec2d), []
 # while i.hasNext(): v.append(i.next())
+from collections import deque
+class Vector2D(object):
+
+    # @param vec2d {List[List[int]]}
+    def __init__(self, vec2d):
+        # Initialize your data structure here
+        self.queue = deque([])
+        for row in vec2d:
+            self.queue += row
+        
+        
+
+    # @return {int} a next element
+    def next(self):
+        # Write your code here
+        return self.queue.popleft()
+        
+
+    # @return {boolean} true if it has next element
+    # or false
+    def hasNext(self):
+        # Write your code here
+        return len(self.queue) != 0
+        
+
+# Your Vector2D object will be instantiated and called as such:
+# i, v = Vector2D(vec2d), []
+# while i.hasNext(): v.append(i.next())
