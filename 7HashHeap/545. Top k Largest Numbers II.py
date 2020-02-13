2c036d88f8a0a1c495896b1bd880f13d697b2e11
Tian Feng <feng.txy@outlook.com>
2020-02-13 13:16:06 -0800

    Update 545. Top k Largest Numbers II.py

--- 7HashHeap/545. Top k Largest Numbers II.py
+++ 7HashHeap/545. Top k Largest Numbers II.py
@@ -1,5 +1,4 @@
-from heapq import heappop, heappush, heappushpop
-
+from heapq import heappop, heappush, heapify
 class Solution:
     """
     @param: k: An integer
@@ -7,8 +6,7 @@ class Solution:
     def __init__(self, k):
         # do intialization if necessary
         self.heap = []
-        self.k = k 
-        
+        self.k = k
 
     """
     @param: num: Number to be added
@@ -16,11 +14,8 @@ class Solution:
     """
     def add(self, num):
         # write your code here
-        if self.k > len(self.heap):
-            heappush(self.heap, num)
-            
-        elif self.k <= len(self.heap) and num > self.heap[0]:
-            heappush(self.heap, num)
+        heappush(self.heap, num)
+        if len(self.heap)>self.k:
             heappop(self.heap)
             
     """
