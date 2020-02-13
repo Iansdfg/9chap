93270fad3b63d6fad23d2274d877096b5b8214b3
Tian Feng <feng.txy@outlook.com>
2020-02-12 20:17:14 -0800

    Create 171. Anagrams.py

---
+++ 7HashHeap/171. Anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param strs: A list of strings
+    @return: A list of strings
+    """
+    def anagrams(self, strs):
+        # write your code here
+        anagram_to_list = dict()
+        for string in strs:
+            anagram = ''.join(sorted(string))
+            if anagram in anagram_to_list:
+                anagram_to_list[anagram].append(string)
+            else:
+                anagram_to_list[anagram] = [string]
+
+        results = []
+        for key in anagram_to_list:
+            if len(anagram_to_list[key]) >= 2:
+                results += anagram_to_list[key]
+        return results
