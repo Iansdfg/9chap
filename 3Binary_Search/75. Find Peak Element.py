78f74183e2b53346d9988d66605a6a9930c3ccb7
Tian Feng <feng.txy@outlook.com>
2020-02-15 12:38:46 -0800

    Update 75. Find Peak Element.py

--- 3Binary_Search/75. Find Peak Element.py
+++ 3Binary_Search/75. Find Peak Element.py
@@ -5,16 +5,16 @@ class Solution:
     """
     def findPeak(self, A):
         # write your code here
-        start, end = 1, len(A) - 2
-        
-        while start + 1 <  end:
-            mid = (start + end) // 2
-            if A[mid] < A[mid - 1]:
-                end = mid
-            elif A[mid] < A[mid + 1]:
-                start = mid
+        left, right = 0, len(A) - 1 
+        while left + 1 < right:
+            mid = (left + right) // 2 
+            if mid == 0 or mid == len(A) - 1:
+                return mid
+            if A[mid-1] < A[mid] < A[mid+1]:
+                left = mid 
+            elif A[mid-1] > A[mid] > A[mid+1]:
+                right = mid 
+            elif A[mid-1] > A[mid] and A[mid] < A[mid+1]:
+                right = mid
             else:
-                end = mid
-                
-        return start if A[start]>A[end] else end
-            
+                return mid 
