293af1cd7f39c74272ef1aa59ddaf96fed7beea8
Tian Feng <feng.txy@outlook.com>
2020-02-15 13:09:09 -0800

    Update 428. Pow(x, n).py

--- 3Binary_Search/428. Pow(x, n).py
+++ 3Binary_Search/428. Pow(x, n).py
@@ -6,15 +6,16 @@ class Solution:
     """
     def myPow(self, x, n):
         # write your code here
-        if n < 0:
-            x = 1/x
-            n = -n
-        base = x 
-        ans = 1 
+        if n<0:
+            n = -n 
+            x = 1/x 
+            
+        base, ans = x, 1 
         while n:
-            if n % 2 == 1:
-                ans *= base
-            base *= base 
-            n = n // 2 
+            if n % 2:
+                base *= ans
+            else:
+                base *= base 
+            n //= 2
         return ans 
             
