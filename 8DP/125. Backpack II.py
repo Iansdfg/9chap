9d32410e1186755990b5fba78148383fd772c0ea
Tian Feng <feng.txy@outlook.com>
2020-02-18 10:47:23 -0800

    Create  125. Backpack II.py

---
+++ 8DP/125. Backpack II.py
@@ -0,0 +1,22 @@
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @param V: Given n items with value V[i]
+    @return: The maximum value
+    """
+    def backPackII(self, m, A, V):
+        # write your code here
+        n = len(A) 
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        
+        for row in range(1, n + 1):
+            for col in range(1, m + 1):
+                if  A[row - 1] > col:
+                    dp[row][col] = dp[row - 1][col]
+                    continue
+                take = dp[row - 1][col - A[row - 1]] + V[row - 1]
+                no_take = dp[row - 1][col]
+                dp[row][col] = max(take, no_take)
+                
+        return dp[-1][-1]
