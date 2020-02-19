36b7a3a56524d32e77782354cba476fc4c12a378
Tian Feng <feng.txy@outlook.com>
2020-02-19 11:15:27 -0800

    Update 107. Word Break.py

--- 8DP/107. Word Break.py
+++ 8DP/107. Word Break.py
@@ -10,18 +10,17 @@ class Solution:
             return True
         if not dict:
             return False 
-        n = len(s)
-        # dp[j]若為True 表示該數列可以透過dictionary組出0~j的字串
-        dp = [0] * (n + 1) 
+        len_s = len(s)
+        dp = [0] * (len_s + 1)
         dp[0] = 1 
-        max_len = max([len(word) for word in dict])
-        for i in range(1, n + 1):
-            for j in range(max(i-max_len, 0), i):
+        max_len = max([len(w) for w in dict])
+        for i in range(len_s+1):
+            for j in range(max(0, i - max_len), i):
                 if not dp[j]:
                     continue
+                
                 if s[j:i] in dict:
                     dp[i] = 1
-                    break
-        return bool(dp[-1])
+                    break 
                     
-            
+        return bool(dp[-1])
