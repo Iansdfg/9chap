8e222b68ce48e9289f0a048b797fee5e52a40115
Tian Feng <feng.txy@outlook.com>
2020-02-19 12:31:47 -0800

    Create  76. Longest Increasing Subsequence.py

---
+++ 8DP/76. Longest Increasing Subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        # write your code here
+        if not nums:
+            return 0
+            
+        dp = [1]*len(nums)
+        
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] >= nums[i]:
+                    continue
+                dp[i] = max(dp[i], dp[j] + 1)
+                
+        return max(dp)
