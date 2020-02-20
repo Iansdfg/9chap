cfd8a9dc15c4987a3aef956dd3d551f2f6b5ab51
Tian Feng <feng.txy@outlook.com>
2020-02-20 11:58:52 -0800

    Update 86. Binary Search Tree Iterator.py

--- 4Binary Tree - Divide Conquer & Traverse/86. Binary Search Tree Iterator.py
+++ 4Binary Tree - Divide Conquer & Traverse/86. Binary Search Tree Iterator.py
@@ -12,7 +12,6 @@ while iterator.hasNext():
     do something for node 
 """
 
-
 class BSTIterator:
     """
     @param: root: The root of binary tree.
@@ -21,29 +20,38 @@ class BSTIterator:
         # do intialization if necessary
         dummy = TreeNode(0)
         dummy.right = root
-        self.stack =[dummy]
+        self.stack = [dummy]
         self.next()
+        
 
     """
     @return: True if there has next node, or false
     """
     def hasNext(self, ):
         # write your code here
-        return bool(self.stack)
+        return len(self.stack) != 0
 
     """
     @return: return next node
     """
     def next(self, ):
         # write your code here
+ 
         curr = self.stack.pop()
-        res = curr
+        ans = curr
         if curr.right:
-            curr = curr.right
+            curr = curr.right 
             while curr:
                 self.stack.append(curr)
-                curr = curr.left 
-        return res
-                    
-                
-   
+                curr = curr.left
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
