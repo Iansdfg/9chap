7c5274643e18fa2daeedfcd4ac0627f81c87a179
Tian Feng <feng.txy@outlook.com>
2020-02-19 18:05:25 -0800

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -5,8 +5,6 @@ class TreeNode:
         this.val = val
         this.left, this.right = None, None
 """
-
-
 class Solution:
     """
     @param: root: The root of the binary tree.
@@ -16,36 +14,29 @@ class Solution:
     """
     def lowestCommonAncestor3(self, root, A, B):
         # write your code here
-        has_A, has_B, node_has_AorB = self.helper(root, A, B)
-        
-        if has_A and has_B:
-            return node_has_AorB
-        return None 
-        
+        exist_A, exist_B, node_has_AorB = self.helper(root, A, B)
+        return node_has_AorB if exist_A and exist_B else None
         
     def helper(self, root, A, B):
-        # return has_A, has_B, node_has_AorB
+        # return exist_A, exist_B, node_has_AorB
         if not root:
             return False, False, None 
             
-        left_has_A, left_has_B, left_node_has_AorB = self.helper(root.left, A, B)
-        right_has_A, right_has_B, right_node_has_AorB = self.helper(root.right, A, B)
+        left_exist_A, left_exist_B, left_node_has_AorB = self.helper(root.left, A, B)
+        right_exist_A, right_exist_B, right_node_has_AorB = self.helper(root.right, A, B)
         
-        has_A = left_has_A or right_has_A or A == root
-        has_B = left_has_B or right_has_B or B == root 
+        exist_A = left_exist_A or right_exist_A or root == A
+        exist_B = left_exist_B or right_exist_B or root == B
         
         if root == A or root == B:
-            return has_A, has_B, root
+            return exist_A, exist_B, root
         
-        elif left_node_has_AorB and right_node_has_AorB:
-            return has_A, has_B, root 
-            
+        if left_node_has_AorB and right_node_has_AorB:
+            return exist_A, exist_B, root 
         elif left_node_has_AorB:
-            return has_A, has_B, left_node_has_AorB
-            
+            return exist_A, exist_B, left_node_has_AorB
         elif right_node_has_AorB:
-            return has_A, has_B, right_node_has_AorB
+            return exist_A, exist_B, right_node_has_AorB
+        else:
+            return exist_A, exist_B, None
             
-        return has_A, has_B, None 
-        
-        
