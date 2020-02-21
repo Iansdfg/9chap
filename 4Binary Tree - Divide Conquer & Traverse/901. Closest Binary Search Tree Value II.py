fe5af1f73aab9543bd242522dea80b4b2c39c746
Tian Feng <feng.txy@outlook.com>
2020-02-21 11:12:55 -0800

    Update 901. Closest Binary Search Tree Value II.py

--- 4Binary Tree - Divide Conquer & Traverse/901. Closest Binary Search Tree Value II.py
+++ 4Binary Tree - Divide Conquer & Traverse/901. Closest Binary Search Tree Value II.py
@@ -15,134 +15,69 @@ class Solution:
     """
     def closestKValues(self, root, target, k):
         # write your code here
-        stack = self.get_inorder(root)
-        tar_pos = self.binary_search(stack, target)
-        result = self.find_k_close(stack, target, tar_pos, k)
-        return result
+        closest = self.find_closest(root, target)
+        # print('closest: ', closest.val)
+        successor = self.find_successor(root, closest)
+        # print('successor: ', successor.val)
+        predesessor = self.find_predesessor(root, closest)
+        # print('predesessor: ', predesessor.val)
         
-    def get_inorder(self, root):
-        dummy = TreeNode(0)
-        dummy.right = root
-        result, stack = [], [dummy]
-        while stack:
-            curr = stack.pop()
-            if curr.right:
-                curr = curr.right
-                while curr:
-                    stack.append(curr)
-                    curr = curr.left
-            if stack:
-                result.append(stack[-1].val)
-        return result
-        
-    def binary_search(self, arrary, target):
-        left, right = 0, len(arrary) - 1
-        while left + 1 < right:
-            mid = (left + right) // 2
-            if target < arrary[mid]:
-                right = mid
-            elif target > arrary[mid]:
-                left = mid
-            else:
-                return mid
-        return left if abs(arrary[left] - target) < abs(arrary[right] - target) else right
-        
-    def find_k_close(self, arrary, target, target_pos, k):
-        left, right = target_pos-1, target_pos+1
-        result = [arrary[target_pos]]
-        while len(result) < k:
-            if self.left_closer(arrary, left, right, target):
-                result.append(arrary[left])
-                left -= 1
-            else:
-                result.append(arrary[right])
-                right += 1
-        return result
-        
-    def left_closer(self, arrary, left, right, target):
-        if left < 0:
-            return False
-        if right >= len(arrary):
-            return True
-        return abs(arrary[left] - target) < abs(arrary[right] - target)
-        
-        
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
-class Solution:
-    """
-    @param root: the given BST
-    @param target: the given target
-    @param k: the given k
-    @return: k values in the BST that are closest to the target
-    """
-    def closestKValues(self, root, target, k):
-        # write your code here
-        closest_node = self.find_closed_val(root, target)
-        succesor = self.find_succesor(root, closest_node)
-        precesor = self.find_precesor(root, closest_node)
-        results = [closest_node.val]
-        while len(results)<k:
-            print(results)
-            if succesor and precesor:
-                if abs(succesor.val - target) < abs(precesor.val - target):
-                    results.append(succesor.val)
-                    succesor = self.find_succesor(root, succesor)
+        results = [closest.val]
+    
+        while len(results) < k:
+            if successor and predesessor :
+                if abs(successor.val - target) < abs(predesessor.val - target):
+                    results.append(successor.val)
+                    successor = self.find_successor(root, successor)
                 else:
-                    results.append(precesor.val)
-                    precesor = self.find_precesor(root, precesor)
-            elif not succesor:
-                results.append(precesor.val)
-                precesor = self.find_precesor(root, precesor)
-            elif not precesor:
-                results.append(succesor.val)
-                succesor = self.find_succesor(root, succesor)
+                    results.append(predesessor.val)
+                    predesessor = self.find_predesessor(root, predesessor)
+            elif successor:
+                results.append(successor.val)
+                successor = self.find_successor(root, successor)
+            elif predesessor:
+                results.append(predesessor.val)
+                predesessor = self.find_predesessor(root, predesessor)
+            
         return results
         
-    def find_closed_val(self, root, target):
-        lower, upper = root, root
+    def find_closest(self, root, target):
+        upper, lower = root, root 
         while root:
-            if root.val > target:
+            if root.val < target:
                 lower = root
-                root = root.left
-            elif root.val < target:
-                upper = root
                 root = root.right
+            elif root.val > target:
+                upper = root
+                root = root.left
             else:
                 return root
-        return upper if abs(upper.val - target) < abs(lower.val - target) else lower
-        
-    def find_succesor(self, root, node):
-        ans = None 
+        return upper if abs(upper.val - target) <  abs(lower.val - target) else lower
+
+    def find_successor(self, root, node):
+        ans = None
         while root:
-            if root.val > node.val:
+            if root.val <= node.val:
+                root = root.right
+            elif root.val > node.val:
                 if not ans or ans.val > root.val:
                     ans = root
                 root = root.left
-            elif root.val < node.val:
-                root = root.right
-            elif root.val == node.val:
-                root = root.right
-        return ans 
+        return ans
         
-    def find_precesor(self, root, node):
-        ans = None 
+    def find_predesessor(self, root, node):
+        ans = None
         while root:
-            if root.val > node.val:
-                root = root.left
-            elif root.val < node.val:
-                if not ans or ans.val < root.val:
+            if root.val < node.val:
+                if not ans or root.val > ans.val:
                     ans = root
                 root = root.right
-            elif root.val == node.val:
+            elif root.val >= node.val:
                 root = root.left
-        return ans 
-          
+        return ans
         
-    
+        
+       
+        
+        
+        
