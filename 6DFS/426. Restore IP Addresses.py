371bec893cc7c30e5d011a6b6e02c2311503a402
Tian Feng <feng.txy@outlook.com>
2020-02-23 11:10:16 -0800

    Update 426. Restore IP Addresses.py

--- 6DFS/426. Restore IP Addresses.py
+++ 6DFS/426. Restore IP Addresses.py
@@ -5,26 +5,25 @@ class Solution:
     """
     def restoreIpAddresses(self, s):
         # write your code here
-        if len(s)<4:
-            return []
         results = []
         self.dfs(s, [], results)
         return results
         
     def dfs(self, s, path, results):
-        if s == '' and len(path) == 4:
-            results.append('.'.join(path[:]))
-            return
         
-        for i in range(1, 4):
+        if s == '' and len(path) == 4:
+            print(path)
+            results.append('.'.join(path))
+            return
+          
+        for i in [1, 2, 3]:
             if i > len(s):
                 return
-            ip_part = s[:i]
-            if int(ip_part) > 255:
+            segment = s[:i]
+            if int(segment) > 255:
                 continue
-            if str(int(ip_part)) != ip_part:
+            if str(int(segment)) != segment:
                 continue
-            path.append(ip_part)
+            path.append(segment)
             self.dfs(s[i:], path, results)
             path.pop()
-        
