f62b3a0dcd99e45a0b256e82410ffed3cce0eb5f
Tian Feng <feng.txy@outlook.com>
2020-02-23 10:47:07 -0800

    Update 570. Find the Missing Number II.py

--- 6DFS/570. Find the Missing Number II.py
+++ 6DFS/570. Find the Missing Number II.py
@@ -4,30 +4,30 @@ class Solution:
     @param str: a string with number from 1-n in random order and miss one number
     @return: An integer
     """
-    def findMissing2(self, n, str):
+    def findMissing2(self, n, s):
         # write your code here
         results = []
-        self.dfs(n, str, [], results)
-        summ = 0
-        for ele in results[0]:
-            summ += int(ele) 
-            
-        return (n*(n+1))//2 - summ
+        self.dfs(n, s, [], results)
         
-        
-    def dfs(self, n, str, path, results):
-        if str == '' and len(path) == n-1:
+        for i in range(1, n+1):
+            if str(i) not in results[0]:
+                return i
+                
+                
+    def dfs(self, n, s, path, results):
+        if s == '' and len(path) == n-1:
+            # print(path)
             results.append(path[:])
             return
         
-        for i in range(1, 3):
-            if i>len(str):
-                return
-            num = str[:i]
-            if num in path or int(num) < 1 or int(num) > n:
+        
+        for i in [1, 2]:
+            if i > len(s):
                 continue
-            if num[0] == '0':
+            if int(s[:i])>n or int(s[:i]) == 0 or s[:i][0] == '0':
                 continue
-            path.append(num)
-            self.dfs(n, str[i:], path, results)
+            if s[:i] in path:
+                continue
+            path.append(s[:i])
+            self.dfs(n, s[i:], path, results)
             path.pop()
