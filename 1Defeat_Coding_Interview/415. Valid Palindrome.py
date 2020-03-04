9be3b26fb66813f7ee1830fb5de1aa8593962282
Tian Feng <feng.txy@outlook.com>
2020-03-04 11:27:31 -0800

    Update 415. Valid Palindrome.py

--- 1Defeat_Coding_Interview/415. Valid Palindrome.py
+++ 1Defeat_Coding_Interview/415. Valid Palindrome.py
@@ -5,14 +5,15 @@ class Solution:
     """
     def isPalindrome(self, s):
         # write your code here
-        string  = ''
-        
-        for char in s:
-            if char.isalnum():
-                
-                if char.isdigit():
-                    string += char
-                else:
-                    string += char.lower()
-                    
-        return string[:] == string[::-1]
+        left, right = 0, len(s) - 1 
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1 
+            while left < right and not s[right].isalnum():
+                right -= 1 
+            if left < right and s[right].lower() != s[left].lower():
+                return False 
+            left += 1 
+            right -= 1 
+        return True 
+            
