06b1e53568caa4eb316cfe133a209010e9fdde26
Tian Feng <feng.txy@outlook.com>
2020-03-04 11:18:03 -0800

    Update 13. Implement strStr().py

--- 1Defeat_Coding_Interview/13. Implement strStr().py
+++ 1Defeat_Coding_Interview/13. Implement strStr().py
@@ -6,13 +6,11 @@ class Solution:
     """
     def strStr(self, source, target):
         # Write your code here
-        if target == '': return 0
-        
-        target_lenth = len(target)
-        target_source = len(source)
-        
-        for i in range(target_source-target_lenth+1):
-            if source[i:i+target_lenth] == target:
-                return i
-            
+        if source == target or target ==:
+            return 0
+        for pos, val in enumerate(source):
+            if val == target[0]:
+                if target == source[pos:pos + len(target)]:
+                    return pos 
         return -1
+                        
