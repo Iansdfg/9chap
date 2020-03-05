99bdef702d685c84e46483d77df5348628cd361c
Tian Feng <feng.txy@outlook.com>
2020-03-04 19:11:03 -0800

    Update 1790. Rotate String II.py

--- 1Defeat_Coding_Interview/1790. Rotate String II.py
+++ 1Defeat_Coding_Interview/1790. Rotate String II.py
@@ -7,6 +7,20 @@ class Solution:
     """
     def RotateString2(self, str, left, right):
         # write your code here
+        offset = (left - right) % len(str)
+        mainStr = str[offset:]
+        subStr = str[:offset]
+        return mainStr + subStr
+    
+class Solution:
+    """
+    @param str: An array of char
+    @param left: a left offset
+    @param right: a right offset
+    @return: return a rotate string
+    """
+    def RotateString2(self, str, left, right):
+        # write your code here
         lenth = len(str)
         left = left % lenth
         right = right % lenth
