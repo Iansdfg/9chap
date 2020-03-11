270cc0c8965138489f4b97d4d88a416d86476eb1
Tian Feng <feng.txy@outlook.com>
2020-03-11 16:07:54 -0700

    Update 787. The Maze.py

--- 2BFS/787. The Maze.py
+++ 2BFS/787. The Maze.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 class Solution:
     """
     @param maze: the maze
@@ -6,53 +7,41 @@ class Solution:
     @param destination: the destination
     @return: whether the ball could stop at the destination
     """
-    directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    derictions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
     def hasPath(self, maze, start, destination):
         # write your code here
         queue = deque([start])
-        visited = set((start[0], start[1]))
+        visited = set()
+        
         while queue:
             curr_x, curr_y = queue.popleft()
+            print(curr_x, curr_y)
             visited.add((curr_x, curr_y))
             if [curr_x, curr_y] == destination:
-                return True
-                
-            for delta_x, delta_y in self.directions:
+                    return True
+                    
+            for delta_x, delta_y in self.derictions:
                 next_x, next_y = curr_x + delta_x, curr_y + delta_y
                 
                 while self.keep_rolling(maze, next_x, next_y):
-                    next_x += delta_x
-                    next_y += delta_y
-                # at this time maze[x][y] == 1 so go back
-                next_x -= delta_x
-                next_y -= delta_y
-                    
-                if self.is_valid(maze, next_x, next_y, visited):
-                    queue.append([next_x, next_y])
-            
-        return False
-        
+                    next_x, next_y = next_x + delta_x, next_y + delta_y
+
+                next_x, next_y = next_x - delta_x, next_y - delta_y
+                
+                if (next_x, next_y) not in visited:
+                    queue.append((next_x, next_y))
+                
+        return False 
+                
     def keep_rolling(self, maze, x, y):
-        rows = len(maze)
-        cols = len(maze[0])
+        rows, cols = len(maze), len(maze[0])
         if x < 0 or x >= rows:
             return False 
         if y < 0 or y >= cols:
-            return False 
+            return False
         if maze[x][y] == 1:
-            return False
-        return True
-        
-    def is_valid(self, maze, x, y, visited):
-        rows = len(maze)
-        cols = len(maze[0])
-        
-        if x < 0 or x >= rows:
             return False 
-        if y < 0 or y >= cols:
-            return False 
-        if maze[x][y] == 1:
-            return False
-        if (x, y) in visited:
-            return False
         return True
+                
+            
