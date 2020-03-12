099ad0ffd7b12aaf4abbd6a94ff02baa1c2b9629
Tian Feng <feng.txy@outlook.com>
2020-03-11 22:42:45 -0700

    Update 624. Remove Substrings.py

--- 2BFS/624. Remove Substrings.py
+++ 2BFS/624. Remove Substrings.py
@@ -12,20 +12,18 @@ class Solution:
         visited = set([s])
         
         min_len = len(s)
-        
         while queue:
             curr = queue.popleft()
+        
             for sub in dict:
-                found = curr.find(sub) 
+                found = curr.find(sub)
+                min_len = min(min_len, len(curr))
                 while found != -1:
                     new_s = curr[:found] + curr[found + len(sub):]
                     if new_s not in visited:
-                        min_len = min(min_len, len(new_s))
-                        queue.append(new_s)
                         visited.add(new_s)
-                    
+                        queue.append(new_s)
+                        
                     found = curr.find(sub, found + 1)
-        
+                    
         return min_len
-            
-        
