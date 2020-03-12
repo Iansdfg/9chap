88520cf8453e552838fa868bc1aa741d41f00f19
Tian Feng <feng.txy@outlook.com>
2020-03-11 21:46:34 -0700

    Create 624. Remove Substrings.py

---
+++ 2BFS/624. Remove Substrings.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    """
+    @param: s: a string
+    @param: dict: a set of n substrings
+    @return: the minimum length
+    """
+    def minLength(self, s, dict):
+        # write your code here
+        queue = deque([s])
+        visited = set([s])
+        
+        min_len = len(s)
+        
+        while queue:
+            curr = queue.popleft()
+            for sub in dict:
+                found = curr.find(sub) 
+                while found != -1:
+                    new_s = curr[:found] + curr[found + len(sub):]
+                    if new_s not in visited:
+                        min_len = min(min_len, len(new_s))
+                        queue.append(new_s)
+                        visited.add(new_s)
+                    
+                    found = curr.find(sub, found + 1)
+        
+        return min_len
+            
+        
