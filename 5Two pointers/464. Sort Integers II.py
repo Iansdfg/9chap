8465cc4322c6264d8299f8d348809c8243a8aae4
Tian Feng <feng.txy@outlook.com>
2020-03-14 14:41:20 -0700

    Update 464. Sort Integers II.py

--- 5Two pointers/464. Sort Integers II.py
+++ 5Two pointers/464. Sort Integers II.py
@@ -5,30 +5,27 @@ class Solution:
     """
     def sortIntegers2(self, A):
         # write your code here
-        return self.quick_sort(A, 0, len(A)-1)
-            
-    def quick_sort(self, A, start, end):
+        start, end = 0, len(A) - 1 
+        self.helper(A, start, end)
         
+    def helper(self, A, start, end):
+        print(start, end)
         if start >= end:
-            return
+            return 
         
         left, right = start, end
+        pivit = A[(left + right)//2]
         
-        pivot = A[(start+end)//2]
-        
-        while left<=right:
-            while left<=right and pivot>A[left]:
-                left+=1
-            
-            while left<=right and pivot<A[right]:
-                right-=1
+        while left <= right:
+            while left <= right and A[left] < pivit:
+                left += 1 
+            while left <= right and A[right] > pivit:
+                right -= 1 
                 
-            if left<=right:
-                A[right], A[left] = A[left],A[right]
+            if left <= right:
+                A[left], A[right] = A[right], A[left]
+                left += 1 
+                right -= 1 
                 
-                right-=1
-                left+=1
-        
-        self.quick_sort(A, start, right)
-        self.quick_sort(A, left, end)
-       
+        self.helper(A, start, right)
+        self.helper(A, left, end)
