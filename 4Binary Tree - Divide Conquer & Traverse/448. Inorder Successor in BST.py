bf251bcb847e7bcf69433d6340ce3034e535b0ff
Tian Feng <feng.txy@outlook.com>
2020-03-15 11:09:58 -0700

    Update 448. Inorder Successor in BST.py

--- 4Binary Tree - Divide Conquer & Traverse/448. Inorder Successor in BST.py
+++ 4Binary Tree - Divide Conquer & Traverse/448. Inorder Successor in BST.py
@@ -16,6 +16,45 @@ class Solution:
     """
     def inorderSuccessor(self, root, p):
         # write your code here
+        if not root:
+            return root
+            
+        curr = root
+        last = None
+        
+        while True:
+            if curr == p:
+                curr = curr.right
+                
+            if not curr:
+                return last
+            
+            if curr.val > p.val:
+                last = curr 
+                curr = curr.left
+            else:
+                curr = curr.right
+                
+
+
+"""
+Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the BST.
+    @param: p: You need find the successor node of p.
+    @return: Successor of p.
+    """
+    def inorderSuccessor(self, root, p):
+        # write your code here
         ans = None  
         
         while root:
