5c9f5513256f624356a4c9155613ee2a731ab488
Tian Feng <feng.txy@outlook.com>
2020-03-15 10:30:25 -0700

    Update 5. Kth Largest Element.py

--- 5Two pointers/5. Kth Largest Element.py
+++ 5Two pointers/5. Kth Largest Element.py
@@ -9,10 +9,9 @@ class Solution:
         start, end = 0, len(nums) - 1 
         return self.helper(n - 1, nums, start, end)
         
-        
     def helper(self, n, nums, start, end):
         if start >= end:
-            return nums[start]
+            return nums[n]
             
         left, right = start, end
         pivot = nums[(left + right)//2]
@@ -20,17 +19,20 @@ class Solution:
         while left <= right:
             while left <= right and nums[left] > pivot:
                 left += 1 
-            while left <= right and nums[right] < pivot:
+                
+            while left <= right and nums[right] <  pivot:
                 right -= 1 
-            
+                
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1 
                 right -= 1 
                 
+        
         if n <= right:
             return self.helper(n, nums, start, right)
         elif n >= left:
             return self.helper(n, nums, left, end)
         else:
             return nums[n]
+        
