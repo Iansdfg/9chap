98690f0f927a3665db561dea73871454cc621290
Tian Feng <feng.txy@outlook.com>
2020-03-15 22:20:10 -0700

    Update 95. Validate Binary Search Tree.py

--- 4Binary Tree - Divide Conquer & Traverse/95. Validate Binary Search Tree.py
+++ 4Binary Tree - Divide Conquer & Traverse/95. Validate Binary Search Tree.py
@@ -13,16 +13,16 @@ class Solution:
     """
     def isValidBST(self, root):
         # write your code here
-        min_val, max_val, valid = self.helper(root)
-        return valid
+        _, _, is_valid = self.helper(root)
+        return is_valid
         
     def helper(self, root):
-        # return min, max, valid 
+        # return max, min, valid 
         if not root:
             return None, None, True 
             
-        left_min, left_max, left_valid = self.helper(root.left)
-        right_min, right_max, right_valid = self.helper(root.right)
+        left_max, left_min, left_valid = self.helper(root.left)
+        right_max, right_min, right_valid = self.helper(root.right)
         
         if not left_valid or not right_valid:
             return None, None, False 
@@ -30,10 +30,10 @@ class Solution:
         if left_max and left_max >= root.val:
             return None, None, False 
             
-        if right_min and right_min <=root.val:
+        if right_min and right_min <= root.val:
             return None, None, False 
+            
+        min_val = left_min if left_min else root.val 
+        max_val = right_max if right_max else root.val 
         
-        min_val = left_min if left_min else root.val
-        max_val = right_max if right_max else root.val
-        
-        return min_val, max_val, True
+        return max_val, min_val, True
