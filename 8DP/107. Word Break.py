21a324781696889a456216e788e773ba8490dcf9
Tian Feng <feng.txy@outlook.com>
2020-03-19 11:28:11 -0700

    Update 107. Word Break.py

--- 8DP/107. Word Break.py
+++ 8DP/107. Word Break.py
@@ -24,3 +24,24 @@ class Solution:
                     break 
                     
         return bool(dp[-1])
+class Solution:
+    """
+    @param: s: A string
+    @param: dict: A dictionary of words dict
+    @return: A boolean
+    """
+    def wordBreak(self, s, dict):
+        # write your code here
+        n = len(s)
+        dp = [0]*(n + 1)
+        dp[0] = 1
+        
+        for i in range(n + 1):
+            for j in range(i):
+                if not dp[j]:
+                    continue
+                if s[j:i] in dict:
+                    dp[i] = 1 
+                    break 
+                
+        return bool(dp[-1])
