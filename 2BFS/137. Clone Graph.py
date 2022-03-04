b69b88779f3b67ee97418079e826a426fd7871ba
Tian Feng <feng.txy@outlook.com>
2022-03-04 13:05:39 -0800

    Update 137. Clone Graph.py

--- 2BFS/137. Clone Graph.py
+++ 2BFS/137. Clone Graph.py
@@ -1,47 +1,40 @@
+from lintcode import (
+    UndirectedGraphNode,
+)
+
 """
+Definition for a UndirectedGraphNode:
 class UndirectedGraphNode:
-     def __init__(self, x):
-         self.label = x
-         self.neighbors = []
+    def __init__(self, label):
+        self.label = label
+        self.neighbors = []
 """
-
 from collections import deque
-
 class Solution:
     """
     @param node: A undirected graph node
     @return: A undirected graph node
     """
-    def cloneGraph(self, node):
+    def clone_graph(self, node):
         # write your code here
         if not node:
-            return node
-            
-        nodes = self.get_nodes(node)
-        old_to_new = {}
-    
-        for old_node in nodes:
-            new_node = UndirectedGraphNode(old_node.label)
-            old_to_new[old_node] = new_node
-        # print(old_to_new)
-            
-        for old_node in nodes:
-            new_node = old_to_new[old_node]
-            for neighbor in old_node.neighbors:
-                new_node.neighbors.append(old_to_new[neighbor])
-                
-        return old_to_new[node]
-            
-    def get_nodes(self, node):
+            return None 
+
         queue = deque([node])
-        nodes = set()
-        
+        ori_clo = {}
+        root = UndirectedGraphNode(node.label)
+        ori_clo[node] = root
+
         while queue:
-            node = queue.popleft()
-            for neighbor in node.neighbors:
-                if neighbor in nodes:
-                    continue
-                queue.append(neighbor)
-            nodes.add(node)
-        return nodes
-        
+            ori_curr = queue.popleft()
+            # print('label', ori_curr.label)
+
+            for neighbor in ori_curr.neighbors:
+                # print('neighbor', neighbor.label)
+                if neighbor not in ori_clo:
+                    ori_clo[neighbor] = UndirectedGraphNode(neighbor.label)
+                    queue.append(neighbor)
+
+                ori_clo[ori_curr].neighbors.append(ori_clo[neighbor])
+
+        return root
