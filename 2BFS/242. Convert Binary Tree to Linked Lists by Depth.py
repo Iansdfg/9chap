da09c4905bd61bca6eaf810db31cf414133f3343
Tian Feng <feng.txy@outlook.com>
2022-03-03 23:34:11 -0800

    Update 242. Convert Binary Tree to Linked Lists by Depth.py

--- 2BFS/242. Convert Binary Tree to Linked Lists by Depth.py
+++ 2BFS/242. Convert Binary Tree to Linked Lists by Depth.py
@@ -33,3 +33,51 @@ class Solution:
                     queue.append(node.right)
             result.append(head.next)
         return result 
+   
+
+##########################
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+"""
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {ListNode[]} a lists of linked list
+    def binaryTreeToLists(self, root):
+        # Write your code here
+        if not root:
+            return []
+        queue = [root]
+        res = []
+        dummy = ListNode(0)
+        last_node = None
+
+        while queue:
+            dummy.next = None
+            last_node = dummy
+            
+            for i in range(len(queue)):
+                curr = queue.pop(0)
+                last_node.next = ListNode(curr.val)
+                last_node = last_node.next
+
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+
+            res.append(dummy.next)
+
+        return res 
+
+            
+            
+
