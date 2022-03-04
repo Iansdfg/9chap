91c173482adbea73fdccaf2c0d1f796a0e9b083b
Tian Feng <feng.txy@outlook.com>
2022-03-03 17:19:30 -0800

    Update 7. Serialize and Deserialize Binary Tree.py

--- 2BFS/7. Serialize and Deserialize Binary Tree.py
+++ 2BFS/7. Serialize and Deserialize Binary Tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 """
 Definition of TreeNode:
 class TreeNode:
@@ -5,8 +7,8 @@ class TreeNode:
         self.val = val
         self.left, self.right = None, None
 """
-
 from collections import deque
+
 class Solution:
     """
     @param root: An object of TreeNode, denote the root of the binary tree.
@@ -15,21 +17,20 @@ class Solution:
     can be easily deserialized by your own "deserialize" method later.
     """
     def serialize(self, root):
-        # write your code here
+        if root is None:
+            return ""
+        
         queue = deque([root])
-        result = []
+        res = []
         while queue:
-            node = queue.popleft()
-            if not node:
-                result.append(None)
-                continue
-            
-            result.append(node)
-            queue.append(node.left if node.left else None)
-            queue.append(node.right if node.right else None)
-            
-        return result
-            
+            curr_node = queue.popleft()
+            res.append(str(curr_node.val) if curr_node else '#')
+            if curr_node:
+                queue.append(curr_node.left)
+                queue.append(curr_node.right)
+        return ' '.join(res)
+
+
     """
     @param data: A string serialized by your serialize method.
     This method will be invoked second, the argument data is what exactly
@@ -40,20 +41,29 @@ class Solution:
     """
     def deserialize(self, data):
         # write your code here
-        data_queue = deque(data)
-        root = data_queue.popleft()
-        queue = deque([root])
-        while queue:
-            node = queue.popleft()
-            if not node:
-                continue
-            
-            nextt = data_queue.popleft()
-            node.left = nextt
-            queue.append(nextt)
-            
-            nextt = data_queue.popleft()
-            node.right = nextt
-            queue.append(nextt)
-        
+        if not data:
+            return None
+    
+        bfs_order = [    
+            TreeNode(int(val)) if val != '#' else None
+            for val in data.split()
+        ]
+        root = bfs_order[0]
+        father_p, child_p = 0, 1
+        nodes = [root]
+
+        while father_p < len(nodes):
+            node = nodes[father_p]
+            father_p += 1 
+
+            node.left = bfs_order[child_p]
+            node.right = bfs_order[child_p + 1]
+            child_p += 2
+
+            if node.left:
+                nodes.append(node.left)
+            if node.right:
+                nodes.append(node.right)
+                
         return root
+
