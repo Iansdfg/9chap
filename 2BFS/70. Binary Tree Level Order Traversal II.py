231c0062b1eac0e1ba37628c54bbf7fc7856d598
Tian Feng <feng.txy@outlook.com>
2022-03-03 16:03:02 -0800

    Update 70. Binary Tree Level Order Traversal II.py

--- 2BFS/70. Binary Tree Level Order Traversal II.py
+++ 2BFS/70. Binary Tree Level Order Traversal II.py
@@ -1,3 +1,7 @@
+from lintcode import (
+    TreeNode,
+)
+
 """
 Definition of TreeNode:
 class TreeNode:
@@ -5,29 +9,34 @@ class TreeNode:
         self.val = val
         self.left, self.right = None, None
 """
-from  collections import deque
 
 class Solution:
     """
     @param root: A tree
     @return: buttom-up level order a list of lists of integer
     """
-    def levelOrderBottom(self, root):
+    def level_order_bottom(self, root):
         # write your code here
         if not root:
-            return []
-        queue = deque([root])
-        result = [[root.val]]
+            return [] 
+        
+        queue = [root]
+        res = []
         while queue:
-            path = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
+            next_q = []
+            res = [[node.val for node in queue]]+ res
+            for node in queue:
+                
                 if node.left:
-                    path.append(node.left.val)
-                    queue.append(node.left)
+                    next_q.append(node.left)
                 if node.right:
-                    path.append(node.right.val)
-                    queue.append(node.right)
-            if path:
-                result.append(path)
-        return result[::-1]
+                    next_q.append(node.right)
+
+            queue = next_q
+        return res 
+
+
+
+
+
+        
