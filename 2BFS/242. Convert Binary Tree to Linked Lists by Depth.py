95f8140c9dd42ba3f769ce73b54159988964ef84
Tian Feng <feng.txy@outlook.com>
2022-03-04 17:22:57 -0800

    Update 242. Convert Binary Tree to Linked Lists by Depth.py

--- 2BFS/242. Convert Binary Tree to Linked Lists by Depth.py
+++ 2BFS/242. Convert Binary Tree to Linked Lists by Depth.py
@@ -11,7 +11,6 @@ class ListNode:
         self.next = None
 """
 from collections import deque
-
 class Solution:
     # @param {TreeNode} root the root of binary tree
     # @return {ListNode[]} a lists of linked list
@@ -20,64 +19,22 @@ class Solution:
         if not root:
             return []
         queue = deque([root])
-        result = []
-        while queue:
-            head = curr = ListNode(0)
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                curr.next = ListNode(node.val)
-                curr = curr.next
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            result.append(head.next)
-        return result 
-   
-
-##########################
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        this.val = val
-        this.left, this.right = None, None
-Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-"""
-class Solution:
-    # @param {TreeNode} root the root of binary tree
-    # @return {ListNode[]} a lists of linked list
-    def binaryTreeToLists(self, root):
-        # Write your code here
-        if not root:
-            return []
-        queue = [root]
-        res = []
         dummy = ListNode(0)
-        last_node = None
-
+        last_node = None 
+        res = []
         while queue:
-            dummy.next = None
             last_node = dummy
-            
+
             for i in range(len(queue)):
-                curr = queue.pop(0)
-                last_node.next = ListNode(curr.val)
+                curr_tree = queue.popleft()
+                node = ListNode(curr_tree.val)
+                last_node.next = node
                 last_node = last_node.next
-
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-
+                
+                if curr_tree.left:
+                    queue.append(curr_tree.left)
+                if curr_tree.right:
+                    queue.append(curr_tree.right)
+            
             res.append(dummy.next)
-
-        return res 
-
-            
-            
-
+        return res
