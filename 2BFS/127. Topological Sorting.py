0aeba00005b684b7254117263dbdd614c9d28e5d
Tian Feng <feng.txy@outlook.com>
2022-03-05 21:43:59 -0800

    Update 127. Topological Sorting.py

--- 2BFS/127. Topological Sorting.py
+++ 2BFS/127. Topological Sorting.py
@@ -1,38 +1,40 @@
 """
-Definition for a Directed graph node
 class DirectedGraphNode:
-    def __init__(self, x):
-        self.label = x
-        self.neighbors = []
+     def __init__(self, x):
+         self.label = x
+         self.neighbors = []
 """
 
 
+from collections import deque
 class Solution:
     """
-    @param: graph: A list of Directed graph node
+    @param graph: A list of Directed graph node
     @return: Any topological order for the given graph.
     """
     def topSort(self, graph):
-        # write your code here
-        node_indegree_dic = self.get_node_indegree(graph)
-        
-        start = [n for n in node_indegree_dic if node_indegree_dic[n] == 0]
         order = []
-        queue = collections.deque(start)
+        node_to_indegree = self.get_indegree(graph)
+        start_nodes = [
+            key for key in node_to_indegree if node_to_indegree[key] == 0]
+        queue = deque(start_nodes)
+
         while queue:
-            curr_node = queue.popleft()
-            order.append(curr_node)
-            for neighbor in curr_node.neighbors:
-                node_indegree_dic[neighbor] -=1
-                if node_indegree_dic[neighbor] == 0:
+            node = queue.popleft()
+            order.append(node)
+            for neighbor in node.neighbors:
+                node_to_indegree[neighbor] -= 1 
+                if node_to_indegree[neighbor] == 0:
                     queue.append(neighbor)
         return order
-                
-    def get_node_indegree(self,graph):
-        indegree_dic ={x:0 for x in graph}
         
+
+    
+    def get_indegree(self, graph):
+        node_to_indegree = {x: 0 for x in graph}
+
         for node in graph:
             for neighbor in node.neighbors:
-                indegree_dic[neighbor]+=1
+                node_to_indegree[neighbor] += 1
                 
-        return indegree_dic
+        return node_to_indegree
