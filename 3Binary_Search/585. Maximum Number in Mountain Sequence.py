abb5e68c081a8de629c2322d757f6c5ea4c9b77f
Tian Feng <feng.txy@outlook.com>
2022-03-07 12:12:25 -0800

    Update 585. Maximum Number in Mountain Sequence.py

--- 3Binary_Search/585. Maximum Number in Mountain Sequence.py
+++ 3Binary_Search/585. Maximum Number in Mountain Sequence.py
@@ -3,15 +3,19 @@ class Solution:
     @param nums: a mountain sequence which increase firstly and then decrease
     @return: then mountain top
     """
-    def mountainSequence(self, nums):
+    def mountain_sequence(self, nums):
         # write your code here
-        start, end = 0, len(nums) - 1
+        if not nums:
+            return -1
+
+        start, end = 0, len(nums) - 1 
+
         while start + 1 < end:
-            mid = (start + end)//2
-            print(mid)
-            if nums[mid] > nums[mid - 1]:
-                start = mid 
-            elif nums[mid] < nums[mid - 1]:
-                end = mid 
+            mid = (start + end) // 2 
+
+            if nums[mid] >= nums[mid + 1]:
+                end = mid
+            elif nums[mid] <= nums[mid + 1]:
+                start = mid
         
         return max(nums[start], nums[end])
