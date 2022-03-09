2884e321367b415c466a8c76ea578506d4e68c66
Tian Feng <feng.txy@outlook.com>
2022-03-08 22:34:15 -0800

    Update 95. Validate Binary Search Tree.py

--- 4Binary Tree - Divide Conquer & Traverse/95. Validate Binary Search Tree.py
+++ 4Binary Tree - Divide Conquer & Traverse/95. Validate Binary Search Tree.py
@@ -37,3 +37,51 @@ class Solution:
         max_val = right_max if right_max else root.val 
         
         return max_val, min_val, True
+
+    
+    
+    
+    
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    def is_valid_b_s_t(self, root):
+        # write your code here
+        max_value, min_value, is_bst = self.dfs(root)
+        return is_bst
+
+    def dfs(self, root):
+        #max_value, min_value, is_bst
+        if not root:
+            return None, None, True
+
+        left_max_value, left_min_value, left_is_bst = self.dfs(root.left)
+        right_max_value, right_min_value, right_is_bst = self.dfs(root.right)
+
+        if not left_is_bst or not right_is_bst:
+            return None, None, False 
+        if left_max_value and left_max_value >= root.val:
+            return None, None, False 
+        if right_min_value and right_min_value <= root.val:
+            return None, None, False 
+            
+        min_value = left_min_value if left_min_value else root.val 
+        max_value = right_max_value if right_max_value else root.val 
+
+        return  max_value, min_value, True
+
+
