589acadb74b737bf27d10cf5158a777117a9907c
Tian Feng <feng.txy@outlook.com>
2022-03-10 20:56:03 -0800

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -5,6 +5,8 @@ class TreeNode:
         this.val = val
         this.left, this.right = None, None
 """
+
+
 class Solution:
     """
     @param: root: The root of the binary tree.
@@ -14,28 +16,32 @@ class Solution:
     """
     def lowestCommonAncestor3(self, root, A, B):
         # write your code here
-        exist_A, exist_B, node_has_AorB = self.helper(root, A, B)
-        return node_has_AorB if exist_A and exist_B else None
-        
-    def helper(self, root, A, B):
-        # return exist_A, exist_B, node_has_AorB
-        if not root:
-            return False, False, None 
-            
-        left_exist_A, left_exist_B, left_node_has_AorB = self.helper(root.left, A, B)
-        right_exist_A, right_exist_B, right_node_has_AorB = self.helper(root.right, A, B)
-        
-        exist_A = left_exist_A or right_exist_A or root == A
-        exist_B = left_exist_B or right_exist_B or root == B
-        
-        if root == A or root == B:
-            return exist_A, exist_B, root
-        
-        if left_node_has_AorB and right_node_has_AorB:
-            return exist_A, exist_B, root 
-        elif left_node_has_AorB:
-            return exist_A, exist_B, left_node_has_AorB
-        elif right_node_has_AorB:
-            return exist_A, exist_B, right_node_has_AorB
+        has_A, has_B, LCA = self.dfs(root, A, B)
+        if has_A and has_B:
+            return LCA
         else:
-            return exist_A, exist_B, None
+            return None 
+        
+    def dfs(self, root, A, B):
+        #has_A, has_B, LCA
+        if not root:
+            return False, False, None  
+
+        left_has_A, left_has_B, left_LCA = self.dfs(root.left, A, B)
+        right_has_A, right_has_B, right_LCA = self.dfs(root.right, A, B)
+        
+        a = left_has_A or right_has_A or A == root
+        b = left_has_B or right_has_B or B == root
+
+        if A == root or B == root:
+            return a, b, root
+
+        if left_LCA and right_LCA:
+            return a, b, root
+        if left_LCA:
+            return a, b, left_LCA
+        if right_LCA:
+            return a, b, right_LCA
+        
+        return a, b, None
+
