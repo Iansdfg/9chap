e6d7b6127bb4ef067f49aa67f66372fc4f3e48d3
Tian Feng <feng.txy@outlook.com>
2022-03-10 21:18:20 -0800

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -7,7 +7,69 @@ class TreeNode:
 """
 
 
+class Solution:"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+
+
 class Solution:
+    """
+    @param: root: The root of the binary tree.
+    @param: A: A TreeNode
+    @param: B: A TreeNode
+    @return: Return the LCA of the two nodes.
+    """
+    def lowestCommonAncestor3(self, root, A, B):
+        # write your code here
+        has_A, has_B, lca = self.dfs(root, A, B)
+        if has_A and has_B:
+            return lca
+        return None
+
+    def dfs(self, root, A, B):
+        #has_A, has_B, lca 
+        if not root:
+            return False, False, None
+        
+        left_has_A, left_has_B, left_lca = self.dfs(root.left, A, B)
+        right_has_A, right_has_B, right_lca = self.dfs(root.right, A, B)
+
+        has_A = left_has_A or right_has_A or root == A
+        has_B = left_has_B or right_has_B or root == B
+
+        if root == A or root == B:
+            return has_A, has_B, root 
+        
+        if left_lca and right_lca:
+            return has_A, has_B, root 
+        if left_lca:
+            return has_A, has_B, left_lca 
+        if right_lca:
+            return has_A, has_B, right_lca 
+        
+        return has_A, has_B, None 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
     @param: root: The root of the binary tree.
     @param: A: A TreeNode
