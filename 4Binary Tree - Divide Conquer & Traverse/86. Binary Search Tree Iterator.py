deb560d3768b597fb6bea44b05bfdffc26c888c1
Tian Feng <feng.txy@outlook.com>
2022-03-11 23:13:31 -0800

    Update 86. Binary Search Tree Iterator.py

--- 4Binary Tree - Divide Conquer & Traverse/86. Binary Search Tree Iterator.py
+++ 4Binary Tree - Divide Conquer & Traverse/86. Binary Search Tree Iterator.py
@@ -19,55 +19,6 @@ class BSTIterator:
     """
     def __init__(self, root):
         # do intialization if necessary
-        dummy = TreeNode(0)
-        dummy.right = root
-        self.stack = [dummy]
-        self.next()
-
-    """
-    @return: True if there has next node, or false
-    """
-    def hasNext(self, ):
-        # write your code here
-        return bool(self.stack)
-
-    """
-    @return: return next node
-    """
-    def next(self, ):
-        # write your code here
-        curr = self.stack.pop()
-        ans = curr
-        if curr.right:
-            curr = curr.right
-            while curr:
-                self.stack.append(curr)
-                curr = curr.left
-                
-        return ans
-        
-#+++++++++2022+++++++++#
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-
-Example of iterate a tree:
-iterator = BSTIterator(root)
-while iterator.hasNext():
-    node = iterator.next()
-    do something for node 
-"""
-
-
-class BSTIterator:
-    """
-    @param: root: The root of binary tree.
-    """
-    def __init__(self, root):
-        # do intialization if necessary
         dummy = TreeNode(-1)
         dummy.right = root
         self.stack = [dummy]
@@ -79,7 +30,7 @@ class BSTIterator:
     def hasNext(self):
         # write your code here
         if self.stack:
-            return True
+            return True 
         return False 
 
     """
@@ -87,22 +38,11 @@ class BSTIterator:
     """
     def _next(self):
         # write your code here
+        curr = self.stack.pop()
+        res = curr
+        curr = curr.right
+        while curr:
+            self.stack.append(curr)
+            curr = curr.left
 
-        node = self.stack.pop()
-        res = node
-        if node.right:
-            node = node.right  
-            while node:
-                self.stack.append(node)
-                node = node.left
         return res
-
-
-
-
-
-
-
-
-
-        
