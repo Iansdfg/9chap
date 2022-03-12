ada0dd8e909f7e4c168cf74780d622e738b285e0
Tian Feng <feng.txy@outlook.com>
2022-03-11 23:47:07 -0800

    Update 900. Closest Binary Search Tree Value.py

--- 4Binary Tree - Divide Conquer & Traverse/900. Closest Binary Search Tree Value.py
+++ 4Binary Tree - Divide Conquer & Traverse/900. Closest Binary Search Tree Value.py
@@ -1,3 +1,7 @@
+from lintcode import (
+    TreeNode,
+)
+
 """
 Definition of TreeNode:
 class TreeNode:
@@ -12,20 +16,22 @@ class Solution:
     @param target: the given target
     @return: the value in the BST that is closest to the target
     """
-    def closestValue(self, root, target):
+    def closest_value(self, root, target):
         # write your code here
-        upper = root 
-        lower = root
-        
+        higher, lower = root.val, root.val
+
         while root:
             if root.val > target:
-                upper = root
-                root = root.left
+                higher = root.val
+                root = root.left 
             elif root.val < target:
-                lower = root
-                root = root.right
+                lower = root.val
+                root = root.right 
             else:
                 return root.val
+        if abs(lower - target) > abs(higher - target):
+            return higher
+        else:
+            return lower
+
         
-        return lower.val if abs(lower.val - target) < abs(upper.val - target) else upper.val
-                
