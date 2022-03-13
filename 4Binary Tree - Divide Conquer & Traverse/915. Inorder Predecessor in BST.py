07eb8eb8b39e5be560896d4ac1ccabb2def16515
Tian Feng <feng.txy@outlook.com>
2022-03-13 16:34:40 -0700

    Update 915. Inorder Predecessor in BST.py

--- 4Binary Tree - Divide Conquer & Traverse/915. Inorder Predecessor in BST.py
+++ 4Binary Tree - Divide Conquer & Traverse/915. Inorder Predecessor in BST.py
@@ -24,38 +24,3 @@ class Solution:
         return res
     
     
- """
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
-class Solution:
-    """
-    @param root: the given BST
-    @param p: the given node
-    @return: the in-order predecessor of the given node in the BST
-    """
-    def inorderPredecessor(self, root, p):
-        # write your code here
-        if not root:
-            return root
-        curr = root
-        last = None
-        
-        while True:
-            if curr == p:
-                curr = curr.left
-                
-            if not curr:
-                return last
-            
-            if curr.val > p.val:
-                curr = curr.left
-    
-            else:
-                last = curr 
-                curr = curr.right
-                
