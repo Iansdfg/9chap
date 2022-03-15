23cd1c09ff3fd0aa2250bfbdc814a3f17c3da5d5
Tian Feng <feng.txy@outlook.com>
2022-03-14 20:01:36 -0700

    Update 415. Valid Palindrome.py

--- 5Two pointers/415. Valid Palindrome.py
+++ 5Two pointers/415. Valid Palindrome.py
@@ -3,18 +3,17 @@ class Solution:
     @param s: A string
     @return: Whether the string is a valid palindrome
     """
-    def isPalindrome(self, s):
+    def is_palindrome(self, s):
         # write your code here
-        left, right = 0, len(s) - 1 
-        while left < right:
-            while left < right and not s[left].isalnum():
-                left += 1 
-            while left < right and not s[right].isalnum():
-                right -= 1 
-            if left < right and s[left].lower() == s[right].lower():
-                left += 1 
-                right -= 1 
-            elif left < right and s[left].lower()  != s[right].lower():
+        start, end = 0, len(s) - 1
+        while start < end:
+            while start < end and not s[start].isalnum():
+                start += 1 
+            while start < end and not s[end].isalnum():
+                end -= 1 
+            print(s[start], s[end])
+            if start < end and s[start].lower() !=  s[end].lower():
                 return False 
+            start += 1 
+            end -= 1 
         return True 
-        
