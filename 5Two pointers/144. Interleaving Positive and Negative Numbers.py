10afa96b2ba0d5867aa0a9cdd4f1c0dd02eb0506
Tian Feng <feng.txy@outlook.com>
2022-03-16 16:57:01 -0700

    Update 144. Interleaving Positive and Negative Numbers.py

--- 5Two pointers/144. Interleaving Positive and Negative Numbers.py
+++ 5Two pointers/144. Interleaving Positive and Negative Numbers.py
@@ -4,82 +4,38 @@ class Solution:
     @return: nothing
     """
     def rerange(self, A):
-        
+        # write your code here
         pos, neg = 0, 0
-        
         for num in A:
-            if num > 0: 
+            if num > 0:
                 pos += 1 
             else:
                 neg += 1 
-                
-        if pos == neg:
-            self.pos_neg_equal(A)
-        elif pos > neg:
-            self.pos_more(A)
-        else:
-            self.neg_more(A)
-            
-            
-    def pos_neg_equal(self, A):
-            
-        start, end = 0, len(A) - 1
-                
-        while start <= end: 
-            while start <= end and A[start] < 0:
-                start += 1 
-            while start <= end and A[end] > 0:
-                end -= 1 
-            if start <= end: 
-                A[start], A[end] = A[end], A[start] 
-                start += 1 
-                end -= 1 
         
-        start, end = 1, len(A) - 2
+        self.partition(A, pos > neg)
+        self.insert(A, pos == neg)
+
+    def partition(self, A, start_positive):
+        flag = 1 if start_positive else -1
+        left, right = 0, len(A) - 1 
+        while left <= right:
+            while left <= right and A[left] * flag > 0:
+                left += 1 
+            while left <= right and A[right] * flag < 0:
+                right -= 1 
+            if left <= right:
+                A[left], A[right] = A[right], A[left]
+                left += 1 
+                right -= 1 
+    
+    def insert(self, A, same_len):
+        start, end = 1, len(A) - 1 
+        if same_len:
+            end = len(A) - 2
         while start < end:
-            A[start], A[end] = A[end], A[start] 
-            start, end = start + 2, end - 2
-            
-            
-    def pos_more(self, A):
-            
-        start, end = 0, len(A) - 1
-                
-        while start <= end: 
-            while start <= end and A[start] > 0:
-                start += 1 
-            while start <= end and A[end] < 0:
-                end -= 1 
-            if start <= end: 
-                A[start], A[end] = A[end], A[start] 
-                start += 1 
-                end -= 1 
-        
-        start, end = 1, len(A) - 1
-        while start < end:
-            A[start], A[end] = A[end], A[start] 
-            start, end = start + 2, end - 2
-            
-            
-    def neg_more(self, A):
-            
-        start, end = 0, len(A) - 1
-                
-        while start <= end: 
-            while start <= end and A[start] < 0:
-                start += 1 
-            while start <= end and A[end] > 0:
-                end -= 1 
-            if start <= end: 
-                A[start], A[end] = A[end], A[start] 
-                start += 1 
-                end -= 1 
-        
-        start, end = 1, len(A) - 1
-        while start < end:
-            A[start], A[end] = A[end], A[start] 
-            start, end = start + 2, end - 2
-                
-                    
-        
-            
+            A[start], A[end] = A[end], A[start]
+            start += 2 
+            end -= 2
+
+
+
