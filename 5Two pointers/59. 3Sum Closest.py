6c15e34fa60fa35615969126a0ab427bcff4fdd2
Tian Feng <feng.txy@outlook.com>
2022-03-16 10:43:52 -0700

    Update 59. 3Sum Closest.py

--- 5Two pointers/59. 3Sum Closest.py
+++ 5Two pointers/59. 3Sum Closest.py
@@ -4,31 +4,23 @@ class Solution:
     @param target: An integer
     @return: return the sum of the three integers, the sum closest target.
     """
-    def threeSumClosest(self, numbers, target):
+    def three_sum_closest(self, numbers, target):
         # write your code here
         numbers.sort()
-        mini = float('inf')
-        res = 0
-        for i in range(len(numbers)):
-            left, right = i+1 , len(numbers) - 1 
-            while left < right:
-                
-                three_sum = numbers[i] + numbers[left] + numbers[right]
-                
-                if three_sum > target:
-                    if abs(three_sum - target) < mini:
-                        mini = abs(three_sum - target)
-                        res = three_sum
-                    right -= 1 
-                    
-                elif three_sum < target:
-                    if abs(three_sum - target) < mini:
-                        mini = abs(three_sum - target)
-                        res = three_sum
-                    left += 1 
-                    
-                elif three_sum == target:
-                    return three_sum
-                    
-        return res 
-                
+        min_val, ans = float('inf'), None
+        for pos in range(len(numbers)):
+            start, end = pos + 1, len(numbers) - 1
+            while start < end:
+                value = numbers[pos] + numbers[start] + numbers[end]
+
+                if value > target:
+                    end -= 1 
+                elif value < target:
+                    start += 1 
+                else:
+                    return value
+                if abs(value - target) < min_val:
+                    min_val = abs(value - target)
+                    ans = value
+        return ans
+            
