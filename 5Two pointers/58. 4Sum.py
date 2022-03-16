bab7e2d9f023e35cdf0af0ba9c4caca7178766cf
Tian Feng <feng.txy@outlook.com>
2022-03-16 12:59:54 -0700

    Update 58. 4Sum.py

--- 5Two pointers/58. 4Sum.py
+++ 5Two pointers/58. 4Sum.py
@@ -3,36 +3,35 @@ class Solution:
     @param numbers: Give an array
     @param target: An integer
     @return: Find all unique quadruplets in the array which gives the sum of zero
+             we will sort your return value in output
     """
-    def fourSum(self, numbers, target):
+    def four_sum(self, numbers, target):
         # write your code here
         numbers.sort()
-        res = []
         print(numbers)
-        for first_pos in range(len(numbers)):
-            if first_pos > 0 and numbers[first_pos] == numbers[first_pos - 1]:
+        res = []
+        for fir_pos in range(len(numbers)):
+            if fir_pos > 0 and numbers[fir_pos] == numbers[fir_pos - 1]:
+                continue
+            for sec_pos in range(fir_pos + 1, len(numbers)):
+                if sec_pos > fir_pos + 1 and numbers[sec_pos] == numbers[sec_pos - 1]:
                     continue
-            
-            for second_pos in range(first_pos+1, len(numbers)):
-                if second_pos > first_pos+1 and numbers[second_pos] == numbers[second_pos - 1]:
-                    continue
-                
-                left, right = second_pos + 1, len(numbers)-1
-                while left < right:
-                    four_sum = numbers[first_pos] + numbers[second_pos] + numbers[left] + numbers[right]
-                    if four_sum == target:
-                        res.append([numbers[first_pos], numbers[second_pos], numbers[left], numbers[right]])
-                        left += 1 
-                        right -= 1 
-                    elif four_sum > target:
-                        right -= 1 
+                start, end = sec_pos + 1, len(numbers) - 1
+                while start < end:
+                    print([numbers[fir_pos], numbers[sec_pos], numbers[start], numbers[end]])   
+                    four_sum = numbers[fir_pos] + numbers[sec_pos] + numbers[start] + numbers[end]
+                    if four_sum > target:
+                        end -= 1 
                     elif four_sum < target:
-                        left += 1 
+                        start += 1 
+                    else:
+                        res.append([numbers[fir_pos], numbers[sec_pos], numbers[start], numbers[end]])
+                        start += 1 
+                        end -= 1 
+                    #two while look remove redundency inside two sum
+                    while end > start and start > sec_pos + 1 and numbers[start]  == numbers[start - 1]:
+                        start += 1 
+                    while end > start and end < len(numbers) - 1 and numbers[end]  == numbers[end + 1]:
+                        end -= 1 
                         
-                    while left < right and left > second_pos+1 and numbers[left] == numbers[left - 1]:
-                        left += 1 
-                    while left < right and right < len(numbers) - 1 and numbers[right] == numbers[right + 1]:
-                        right -= 1 
-                  
-        return res 
-                    
+        return res
