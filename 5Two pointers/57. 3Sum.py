b51f42a472ddd38ba7c51359d307115a28de841a
Tian Feng <feng.txy@outlook.com>
2022-03-15 17:26:04 -0700

    Update 57. 3Sum.py

--- 5Two pointers/57. 3Sum.py
+++ 5Two pointers/57. 3Sum.py
@@ -3,31 +3,30 @@ class Solution:
     @param numbers: Give an array numbers of n integer
     @return: Find all unique triplets in the array which gives the sum of zero.
     """
-    def threeSum(self, numbers):
+    def three_sum(self, numbers):
         # write your code here
-        numbers.sort()
+        numbers = sorted(numbers)
         results = []
-        
-        for pos in range(len(numbers)):
+        for pos, val in enumerate(numbers):
             if pos > 0 and numbers[pos] == numbers[pos - 1]:
                 continue
-            
-            left, right = pos + 1, len(numbers) - 1 
-            while left < right:
-                three_sum = numbers[pos] + numbers[left] + numbers[right]
-                if three_sum == 0:
-                    results.append([numbers[pos], numbers[left],  numbers[right]])
-                    left += 1 
-                    right -= 1 
-                elif three_sum > 0:
-                    right -= 1 
-                elif three_sum < 0:
-                    left += 1 
-                    
-                while left < right and left > pos + 1 and numbers[left] == numbers[left - 1]:
-                    left += 1 
-                while left < right and right < len(numbers) - 1  and numbers[right] == numbers[right + 1]:
-                    right -= 1 
-                    
+            results += self.two_sum(numbers, pos + 1, len(numbers) - 1, val)
         return results
-                
+
+    def two_sum(self, numbers, start, end, target):
+        neg_target = target * -1
+        res = []
+        last = None
+        while start < end:
+            val = numbers[start] + numbers[end]
+            if val > neg_target:
+                end -= 1
+            elif val < neg_target:
+                start += 1 
+            else:
+                if (numbers[start], numbers[end]) != last:
+                    res.append((target, numbers[start], numbers[end]))
+                last = (numbers[start], numbers[end])
+                end -= 1
+                start += 1 
+        return res 
