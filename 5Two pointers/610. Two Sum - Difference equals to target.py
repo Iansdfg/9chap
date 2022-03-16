2650677acda7630606a579214bbe3c97c4683048
Tian Feng <feng.txy@outlook.com>
2022-03-16 12:10:34 -0700

    Update 610. Two Sum - Difference equals to target.py

--- 5Two pointers/610. Two Sum - Difference equals to target.py
+++ 5Two pointers/610. Two Sum - Difference equals to target.py
@@ -2,22 +2,18 @@ class Solution:
     """
     @param nums: an array of Integer
     @param target: an integer
-    @return: [index1 + 1, index2 + 1] (index1 < index2)
+    @return: [num1, num2] (index1 < index2)
     """
-    def twoSum7(self, nums, target):
+    def two_sum7(self, nums, target):
         # write your code here
-        num_pos = []
-        for pos, val in enumerate(nums):
-            num_pos.append([val, pos]) 
-        
-        num_pos.sort()
         target = abs(target)
-        right = 1 
-        
-        for left in range(len(num_pos)):
-            while right < len(num_pos) and num_pos[right][0] - num_pos[left][0] < target:
-                right += 1 
-            if left != right and num_pos[right][0] - num_pos[left][0] == target:
-                result = [num_pos[right][1]+1, num_pos[left][1]+1]
-                result.sort()
-                return result
+        start, end = 0, 1
+        while end < len(nums):
+            if nums[end] - nums[start] == target:
+                return [nums[start], nums[end]]
+            elif nums[end] - nums[start] < target:
+                end += 1 
+            elif nums[end] - nums[start] > target:
+                start += 1 
+                end = max(start + 1, end)
+        return [-1, -1]
