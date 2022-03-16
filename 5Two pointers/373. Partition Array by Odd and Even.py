0a84fc2fcc1c8493d447c23a85d93a6bb8a328c1
Tian Feng <feng.txy@outlook.com>
2022-03-16 14:37:18 -0700

    Update 373. Partition Array by Odd and Even.py

--- 5Two pointers/373. Partition Array by Odd and Even.py
+++ 5Two pointers/373. Partition Array by Odd and Even.py
@@ -5,19 +5,18 @@ class Solution:
     """
     def partitionArray(self, nums):
         # write your code here
-        left, right = 0, len(nums)-1 
-        
-        
+        left, right = 0, len(nums) - 1
         while left <= right:
             while left <= right and nums[left] % 2 != 0:
                 left += 1 
             while left <= right and nums[right] % 2 == 0:
-                right -= 1
-                
-                
+                right -= 1 
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
-                left += 1 
                 right -= 1 
-            
+                left += 1 
+        return nums
+
+
+
 
