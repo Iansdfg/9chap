9b16ffaf85061b2837b55d1abda88c539ee630b5
Tian Feng <feng.txy@outlook.com>
2022-03-17 12:24:20 -0700

    Update 148. Sort Colors.py

--- 5Two pointers/148. Sort Colors.py
+++ 5Two pointers/148. Sort Colors.py
@@ -1,22 +1,19 @@
 class Solution:
     """
-    @param nums: A list of integer which is 0, 1 or 2 
+    @param nums: A list of integer which is 0, 1 or 2
     @return: nothing
     """
-    
-    def sortColors(self, nums):
+    def sort_colors(self, nums):
         # write your code here
         left, mid, right = 0, 0, len(nums) - 1 
-        
         while mid <= right:
             if nums[mid] == 0:
                 nums[left], nums[mid] = nums[mid], nums[left]
                 mid += 1 
-                left += 1 
-                
+                left += 1
             elif nums[mid] == 1:
                 mid += 1 
-                
             elif nums[mid] == 2:
-                nums[right], nums[mid] = nums[mid], nums[right]
+                nums[mid], nums[right] = nums[right], nums[mid]
                 right -= 1 
+        
