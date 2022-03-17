0f110368ad4abd1795727c3ccd15f84fd3570aa5
Tian Feng <feng.txy@outlook.com>
2022-03-17 11:58:39 -0700

    Update 461. Kth Smallest Numbers in Unsorted Array.py

--- 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
+++ 5Two pointers/461. Kth Smallest Numbers in Unsorted Array.py
@@ -4,40 +4,30 @@ class Solution:
     @param nums: An integer array
     @return: kth smallest element
     """
-    def kthSmallest(self, k, nums):
+    def kth_smallest(self, k, nums):
         # write your code here
-        start, end = 0, len(nums) - 1 
-        return self.helper(k - 1 , nums, start, end)
-        
-        
-    def helper(self, k, nums, start, end):
-        if start == end:
-            return nums[start]
-            
-            
+        return self.quck_select(k - 1, nums, 0, len(nums) - 1)
+
+    def quck_select(self, k, nums, start, end):
         left, right = start, end
-    
-        pivot =nums[(left + right)//2] 
-        
+
+        pivot = nums[(left + right) // 2]
         while left <= right:
             while left <= right and nums[left] < pivot:
                 left += 1 
             while left <= right and nums[right] > pivot:
                 right -= 1 
-                
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
-                left += 1 
-                right -= 1 
-                
-                
-        if k <= right:
-            return self.helper(k, nums, start, right)
-            
-        elif k >= left :
-            return self.helper(k, nums, left, end)
-            
-        return nums[k] 
-            
-            
-        
+                left += 1
+                right -= 1
+        print(left, right, k)
+
+        if right >= k: 
+            #>=: the partitioned part is not sorted. 
+            #when k == right right might not be the target
+            return self.quck_select(k, nums, start, right)
+        elif left <= k:
+            return self.quck_select(k, nums, left, end);
+        else:#when right < key < left
+            return nums[k];
