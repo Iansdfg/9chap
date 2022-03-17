c07578a3ad23cab75c153cae7b5c3762cd8be07d
Tian Feng <feng.txy@outlook.com>
2022-03-17 16:43:05 -0700

    Update 15. Permutations.py

--- 6DFS/15. Permutations.py
+++ 6DFS/15. Permutations.py
@@ -1,27 +1,25 @@
 class Solution:
     """
-    @param: nums: A list of integers.
+    @param nums: A list of integers.
     @return: A list of permutations.
     """
     def permute(self, nums):
         # write your code here
-        nums.sort()
-        results = []
-        visited = [0] * len(nums)
-        self.dfs(nums, visited, [], results)
-        return results
+        visited = [0 for i in range(len(nums))]
+        permutations = []
+        self.dfs(nums, visited, [], permutations)
+        return permutations
+
+    def dfs(self, nums, visited, permutation, permutations):
+        if len(permutation) == len(nums):
+           permutations.append(permutation[:])
+           return
         
-    def dfs(self, nums, visited, path, results):
-        if len(path) == len(nums):
-            results.append(path[:])
-            
         for i in range(len(nums)):
             if visited[i]:
                 continue
-            
-            visited[i] = 1 
-            path.append(nums[i])
-            self.dfs(nums, visited, path, results)
-            path.pop()
+            permutation.append(nums[i])
+            visited[i] = 1
+            self.dfs(nums, visited, permutation, permutations)
             visited[i] = 0
-            
+            permutation.pop()
