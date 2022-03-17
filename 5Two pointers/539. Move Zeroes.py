0776bdf1cb9a1186a14cf001ded53fc0224b7d75
Tian Feng <feng.txy@outlook.com>
2022-03-17 13:32:05 -0700

    Update 539. Move Zeroes.py

--- 5Two pointers/539. Move Zeroes.py
+++ 5Two pointers/539. Move Zeroes.py
@@ -3,24 +3,18 @@ class Solution:
     @param nums: an integer array
     @return: nothing
     """
-    def moveZeroes(self, nums):
-        write your code here
-        if len(nums) < 2 :
-            return nums
-            
-        slow = 0
-        while nums[slow]:
-            slow += 1 
-            if slow >= len(nums):
-                return nums
-         
-        fast = slow + 1 
-        while fast < len(nums):
-            if nums[fast] == 0:
-                fast += 1 
-            elif nums[fast] != 0:
-                nums[fast], nums[slow] = nums[slow], nums[fast]
-                slow += 1 
-                
-        return nums
-            
+    def move_zeroes(self, nums):
+        # write your code here
+        left, right = 0, 0 
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left] = nums[right]
+                left += 1 
+            right += 1
+
+        while left < len(nums):
+            if nums[left] != 0: 
+                nums[left] = 0
+            left += 1
+
+
