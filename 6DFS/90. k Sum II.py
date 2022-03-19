49263c9928e1736d49599791731edb543548c010
Tian Feng <feng.txy@outlook.com>
2022-03-18 18:52:06 -0700

    Update 90. k Sum II.py

--- 6DFS/90. k Sum II.py
+++ 6DFS/90. k Sum II.py
@@ -1,25 +1,29 @@
 class Solution:
     """
-    @param: A: an integer array
-    @param: k: a postive integer <= length(A)
-    @param: targer: an integer
+    @param a: an integer array
+    @param k: a postive integer <= length(A)
+    @param target: an integer
     @return: A list of lists of integer
     """
-    def kSumII(self, A, k, target):
+    def k_sum_i_i(self, a, k, target):
         # write your code here
-        A.sort()
-        results = []
-        self.dfs(A, k, target, 0, [], results)
-        return results
-        
-    def dfs(self, A, k, target, index, path, results):
-        if sum(path)>target or len(path) > k:
-            return 
-        if sum(path) == target and len(path) == k:
-            results.append(path[:])
-            
-        for i in range(index, len(A)):
-            path.append(A[i])
-            self.dfs(A, k, target, i + 1, path, results)
-            path.pop()
-           
+        a.sort()
+        combinations = []
+        self.dfs(a, k, target, 0, [], combinations)
+        return combinations
+
+
+    def dfs(self, a, k, target, index, combination, combinations):
+        if len(combination) > k or sum(combination) > target:
+            return
+
+        if len(combination) == k and sum(combination) == target:
+            combinations.append(combination[:])
+
+        for i in range(index, len(a)):
+            if i > 0 and a[i] == a[i-1]:
+                continue
+            combination.append(a[i])
+            self.dfs(a, k, target, i + 1, combination, combinations)
+            combination.pop()
+
