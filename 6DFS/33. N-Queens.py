ab1661d4250fcdac2cce6d1dd0dd3f1ecdecb75b
Tian Feng <feng.txy@outlook.com>
2022-03-21 21:45:20 -0700

    Update 33. N-Queens.py

--- 6DFS/33. N-Queens.py
+++ 6DFS/33. N-Queens.py
@@ -1,57 +1,47 @@
 class Solution:
     """
-    @param: n: The number of queens
+    @param n: The number of queens
     @return: All distinct solutions
     """
-    def solveNQueens(self, n):
+    def solve_n_queens(self, n):
         # write your code here
-        results = []
-        visited = [0]*n
-        self.dfs(n, visited, [], results)
-        return results
-        
-    def dfs(self, n, visited, path, results):
-        if len(path) == n:
-            chessboard = self.draw_chessboard(path)
-            results.append(chessboard)
-            # results.append(path[:])
+        visited = [0 for i in range(n)]
+        premutations = []
+        self.dfs(n, visited, [], premutations)
+        premutations.sort()
+        return premutations
+
+    def dfs(self, n, visited, premutation, premutations):
+        if len(premutation) == n:
+            print(premutation)
+            premutations.append(self.draw(premutation))
             return
-        
+
         for i in range(n):
-            if visited[i]:
+            if visited[i] == 1:
                 continue
-            if not self.is_valid(path, i):
+            if not self.is_valid(premutation, i):
                 continue
-            
             visited[i] = 1
-            path.append(i)
-            self.dfs(n, visited, path, results)
-            path.pop()
+            premutation.append(i)
+            self.dfs(n, visited, premutation, premutations)
+            premutation.pop()
             visited[i] = 0
-            
-    def is_valid(self, path, i):
-        len_path = len(path)
-        # len_path is x, i is y 
-        # pos of path is row, val of path is col
-        # determian if abs(x - row) == abs(y - col)
-        for row, col in enumerate(path):
-            # 两个点在斜线上的判断方式用一个等式
-            # Math.abs(x - row) == Math.abs(y - col) 即可。
-            # 根据x + row == y + col 和 x - row == y - col
-            if row - col == len_path - i or row + col == len_path + i:
+
+    def is_valid(self, premutation, i):
+        #current corrdination, i is col, len(premutation) is row
+        # if valid, the next (x,y) and (row, col) not in diagonal line)
+        row, col = len(premutation), i
+        for next_row,next_col in enumerate(premutation):
+            if abs(row - next_row) == abs(col - next_col):
                 return False
-        return True
-        
-    def draw_chessboard(self, path):
+        return True 
+
+    def draw(self, premutation):
         board = []
-        for ele in path:
-            row = ['Q' if i == ele else '.' for i in range(len(path))]
-            # row = []
-            # for i in range(len(path)):
-            #     if i == ele:
-            #         row.append('Q') 
-            #     else: 
-            #         row.append('.') 
+        for num in premutation:
+            row = ['Q' if i == num else '.' for i in range(len(premutation))]
             board.append(''.join(row))
         return board
+
             
