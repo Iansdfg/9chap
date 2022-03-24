b15001b4b46aace838bc2fc255f16f7eff7fa894
Tian Feng <feng.txy@outlook.com>
2022-03-23 17:01:42 -0700

    Update 657. Insert Delete GetRandom O(1).py

--- 7HashHeap/657. Insert Delete GetRandom O(1).py
+++ 7HashHeap/657. Insert Delete GetRandom O(1).py
@@ -1,10 +1,11 @@
 import random
+
 class RandomizedSet:
     
     def __init__(self):
         # do intialization if necessary
-        self.val_to_pos = dict()
-        self.stack = []
+        self.nums = []
+        self.num_to_pos = dict()
 
     """
     @param: val: a value to the set
@@ -12,37 +13,38 @@ class RandomizedSet:
     """
     def insert(self, val):
         # write your code here
-        if val in self.stack:
-            return
-        self.stack.append(val)
-        self.val_to_pos[val] =  len(self.stack) - 1
-        
+        if val in self.num_to_pos:
+            return False
+        self.nums.append(val)
+        self.num_to_pos[val] = len(self.nums)-1
+
+
     """
     @param: val: a value from the set
     @return: true if the set contained the specified element or false
     """
     def remove(self, val):
         # write your code here
-        if val not in self.stack:
-            return
-        
-        pos = self.val_to_pos[val]
-        last_val = self.stack[-1]
-        
-        self.stack[pos] = last_val
-        self.val_to_pos[last_val] = pos
-        
-        self.stack.pop()
-        del self.val_to_pos[val]
-        
+        if val not in self.num_to_pos:
+            return False
+
+        pos = self.num_to_pos[val]
+        last = self.nums[-1]
+
+        self.nums[pos] = last
+        self.num_to_pos[last] = pos 
+
+        self.nums.pop()
+        del self.num_to_pos[val]
+
 
     """
     @return: Get a random element from the set
     """
     def getRandom(self):
         # write your code here
-        pos = random.randint(0, len(self.stack) - 1)
-        return self.stack[pos]
+        return self.nums[random.randint(0, len(self.nums)-1)]
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
