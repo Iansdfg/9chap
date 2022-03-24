8712ac611d55a43f2dde969e9eaf24c98074b116
Tian Feng <feng.txy@outlook.com>
2022-03-23 17:48:42 -0700

    Update 954. Insert Delete GetRandom O(1) - Duplicates allowed.py

--- 7HashHeap/954. Insert Delete GetRandom O(1) - Duplicates allowed.py
+++ 7HashHeap/954. Insert Delete GetRandom O(1) - Duplicates allowed.py
@@ -1,45 +1,57 @@
 from collections import defaultdict
-import random 
 class RandomizedCollection(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.stack = []
-        self.val_to_pos_list = defaultdict(set)
-        
+        self.nums = []
+        self.num2indexList = {}
+
     def insert(self, val):
         """
         Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
         :type val: int
         :rtype: bool
         """
-        self.stack.append(val)
-        self.val_to_pos_list[val].add(len(self.stack) - 1)
-        return len(self.val_to_pos_list[val]) == 1
-          
+        self.nums.append(val)
+        if val in self.num2indexList:
+            self.num2indexList[val].append(len(self.nums) - 1)
+            return False
+        else:
+            self.num2indexList[val] = [len(self.nums) - 1]
+            return True
+
     def remove(self, val):
         """
         Removes a value from the collection. Returns true if the collection contained the specified element.
         :type val: int
         :rtype: bool
         """
-        # pop() for set is pop random
-        if not self.val_to_pos_list[val]:
-            return
-        pos = self.val_to_pos_list[val].pop()
-        self.stack[pos] = None 
+        if val in self.num2indexList:
+            index_list = self.num2indexList[val]
+            last_pos = index_list.pop()
+            if not index_list:
+                del self.num2indexList[val]
+            if last_pos != len(self.nums) - 1:
+                last_num = self.nums[-1]
+                self.num2indexList[last_num][-1] = last_pos
+                self.nums[last_pos], self.nums[-1] = self.nums[-1], self.nums[last_pos]
+            self.nums.pop()
+            return True 
+        else:
+            return False 
         
+
     def getRandom(self):
         """
         Get a random element from the collection.
         :rtype: int
         """
-        ans = None 
-        while not ans:
-            ans = random.choice(self.stack)
-        return ans
+        import random
+        return random.choice(self.nums)
+        
+        
 
 
 # Your RandomizedCollection object will be instantiated and called as such:
