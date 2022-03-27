5e8ac9eab8a86930c6b9e15cab73193f4110e48f
Tian Feng <feng.txy@outlook.com>
2022-03-26 18:11:52 -0700

    Update 612. K Closest Points.py

--- 7HashHeap/612. K Closest Points.py
+++ 7HashHeap/612. K Closest Points.py
@@ -1,11 +1,14 @@
+from lintcode import (
+    Point,
+)
+import heapq
 """
-Definition for a point.
+Definition for a point:
 class Point:
-    def __init__(self, a=0, b=0):
-        self.x = a
-        self.y = b
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 """
-from heapq import heappush, heappop
 
 class Solution:
     """
@@ -14,19 +17,20 @@ class Solution:
     @param k: An integer
     @return: the k closest points
     """
-    def kClosest(self, points, origin, k):
+    def k_closest(self, points, origin, k):
         # write your code here
-        
         heap = []
         for point in points:
-            dis = self.distance(point, origin)
-            heappush(heap, (dis, point.x, point.y))
+            dist = self.get_distance(origin, point)
+            heapq.heappush(heap, (dist, point.x, point.y))
+        
         res = []
         for _ in range(k):
-            dis, point_x, point_y = heappop(heap)
-            res.append(Point(point_x, point_y))
-        return res
-            
-    
-    def distance(self, A, B):
-        return (A.x - B.x)**2 + (A.y - B.y)**2
+            dis, x, y  = heapq.heappop(heap)
+            res.append(Point(x, y))
+        return res 
+
+    def get_distance(self, origin, point):
+        x1, y1 = origin.x, origin.y
+        x2, y2 = point.x, point.y
+        return (x1 - x2)**2 + (y1 - y2)**2
