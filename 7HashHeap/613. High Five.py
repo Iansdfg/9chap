bd14284fe143230be58fa2232cb8883df242a895
Tian Feng <feng.txy@outlook.com>
2022-03-27 18:27:42 -0700

    Update 613. High Five.py

--- 7HashHeap/613. High Five.py
+++ 7HashHeap/613. High Five.py
@@ -1,3 +1,4 @@
+import heapq 
 '''
 Definition for a Record
 class Record:
@@ -5,34 +6,32 @@ class Record:
         self.id = id
         self.score = score
 '''
-from heapq import heapify, heappop
-
 class Solution:
     # @param {Record[]} results a list of <student_id, score>
     # @return {dict(id, average)} find the average of 5 highest scores for each person
     # <key, value> (student_id, average_score)
     def highFive(self, results):
         # Write your code here
-        id_to_scores = dict()
-        for result in results:
-            if result.id not in id_to_scores:
-                id_to_scores[result.id] = [-result.score]
+        id2scores = dict()
+        for record in results:
+            if record.id in id2scores:
+                id2scores[record.id].append(-record.score)
             else:
-                id_to_scores[result.id].append(-result.score) 
-        print(id_to_scores)
+                id2scores[record.id] = [-record.score]
         
-        id_to_high_five = dict()
-        for key in id_to_scores:
-            id_to_high_five[key] = self.find_one_ave(id_to_scores[key])
+        res = dict()
+        for key in id2scores:
+            avg = self.get_high5(id2scores[key])
+            res[key] = avg
+        return res
 
-        return id_to_high_five
-    
-    
-    def find_one_ave(self, array):
-        heapify(array)
+    def get_high5(self,scores):
+        heapq.heapify(scores)
         summ = 0
-        for i in range(5):
-            summ += -heappop(array)
-        return summ/5
-            
-        
+        for _ in range(5):
+            summ -= heapq.heappop(scores)
+        return summ * 1.0 /5
+
+
+
+
