f9d9581205cc89060d7f702dffbd58b9fd5cc4c9
Tian Feng <feng.txy@outlook.com>
2022-04-01 00:17:31 -0700

    Update 178. Graph Valid Tree.py

--- 2BFS/178. Graph Valid Tree.py
+++ 2BFS/178. Graph Valid Tree.py
@@ -1,34 +1,43 @@
-from collections import deque, defaultdict
-
+from collections import deque
 class Solution:
     """
     @param n: An integer
     @param edges: a list of undirected edges
     @return: true if it's a valid tree, or false
     """
-    def validTree(self, n, edges):
+    def valid_tree(self, n, edges):
         # write your code here
-        # valid tree: n-1 == len(edges) and all nodes can be visited 
-        if n-1 != len(edges):
+
+        if len(edges) != n-1:
             return False
-            
-        node_to_neighbers = defaultdict(list)
-        for x,y in edges:
-            node_to_neighbers[x].append(y)
-            node_to_neighbers[y].append(x)
-            
+
+        node_to_neighbers = {x:[] for x in range(n)}
         visited = set()
+
+        for edge in edges:
+            v1, v2 = edge[0], edge[1]
+            node_to_neighbers[v1].append(v2)
+            node_to_neighbers[v2].append(v1)
+
+        print(node_to_neighbers)
+
         queue = deque([0])
-        
+
         while queue:
             curr = queue.popleft()
-            if curr not in visited:
-                visited.add(curr)
             visited.add(curr)
             for neighber in node_to_neighbers[curr]:
-                if neighber not in visited:
-                    visited.add(neighber)
-                    queue.append(neighber)
-                
-        return len(visited) == n
-                
+                if neighber in visited:
+                    continue
+                queue.append(neighber)
+
+        return len(visited) == n 
+
+
+        
+
+        
+
+        
+
+        
