95bc3e777706c34aa65fb68b0c4aefb8458824ee
Tian Feng <feng.txy@outlook.com>
2022-04-02 18:51:07 -0700

    Update 376. Binary Tree Path Sum.py

--- 4Binary Tree - Divide Conquer & Traverse/376. Binary Tree Path Sum.py
+++ 4Binary Tree - Divide Conquer & Traverse/376. Binary Tree Path Sum.py
@@ -1,3 +1,7 @@
+from lintcode import (
+    TreeNode,
+)
+
 """
 Definition of TreeNode:
 class TreeNode:
@@ -6,28 +10,27 @@ class TreeNode:
         self.left, self.right = None, None
 """
 
-
 class Solution:
     """
-    @param: root: the root of binary tree
-    @param: target: An integer
+    @param root: the root of binary tree
+    @param target: An integer
     @return: all valid paths
+             we will sort your return value in output
     """
-    def binaryTreePathSum(self, root, target):
+    def binary_tree_path_sum(self, root, target):
         # write your code here
-        results = []
-        self.dfs(root, target, [], results)
-        return results
-        
-    def dfs(self, root, target, path, results):
+        paths = []
+        self.dfs(root, target, [], paths)
+        return paths
+
+    def dfs(self, root, target, path, paths):
         if not root:
-            return
-        
+            return 
+
         path.append(root.val)
-        
         if not root.left and not root.right and sum(path) == target:
-            results.append(path[:])
-            
-        self.dfs(root.left, target, path, results)
-        self.dfs(root.right, target, path, results)
+            paths.append(path[:])
+      
+        self.dfs(root.left, target, path, paths)
+        self.dfs(root.right, target, path, paths)
         path.pop()
