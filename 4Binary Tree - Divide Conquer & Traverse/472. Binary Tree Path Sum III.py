12fca7de38b2091b7bc7fe69db126b1a1efe2ef1
Tian Feng <feng.txy@outlook.com>
2022-04-02 23:13:16 -0700

    Update 472. Binary Tree Path Sum III.py

--- 4Binary Tree - Divide Conquer & Traverse/472. Binary Tree Path Sum III.py
+++ 4Binary Tree - Divide Conquer & Traverse/472. Binary Tree Path Sum III.py
@@ -1,40 +1,52 @@
-"""
+from lintcode import (
+    ParentTreeNode,
+)
+
+""" 
 Definition of ParentTreeNode:
 class ParentTreeNode:
     def __init__(self, val):
         self.val = val
         self.parent, self.left, self.right = None, None, None
 """
+
 class Solution:
     """
-    @param: root: the root of binary tree
-    @param: target: An integer
+    @param root: the root of binary tree
+    @param target: An integer
     @return: all valid paths
+             we will sort your return value in output
     """
-    def binaryTreePathSum3(self, root, target):
+    def binary_tree_path_sum3(self, root, target):
         # write your code here
-        results = []
-        self.helper(root, target, results)
-        return results
-        
-    def helper(self, root, target, results):
+        paths = []
+        self.iterator(root, target, [], paths)
+        return paths
+
+    def iterator(self, root, target, path, paths):
         if not root:
-            return 
-        
-        self.dfs(root, target, None, [], results)
-        
-        self.helper(root.left, target, results)
-        self.helper(root.right, target, results)
-        
-    def dfs(self, root, target, prev, path, results):
+            return
+        print('root: ', root.val)
+        # do dfs in every node. 
+        self.dfs(root, target, root, path, paths)
+        self.iterator(root.left, target, path, paths)
+        self.iterator(root.right, target, path, paths)
+
+    def dfs(self, root, target, prev, path, paths):
         path.append(root.val)
+        print(path)
         if sum(path) == target:
-            print(path)
-            results.append(path[:])
+            paths.append(path[:])
+
         for next_node in [root.parent, root.left, root.right]:
             if not next_node:
                 continue
-            if next_node == prev:
+            if prev == next_node:
                 continue
-            self.dfs(next_node, target, root, path, results)
+            self.dfs(next_node, target, root, path, paths)
+
         path.pop()
+
+        
+
+            
