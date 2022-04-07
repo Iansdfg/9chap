7fda01737fb8b3ffa5c3c118fb0b2f35d2735516
Tian Feng <feng.txy@outlook.com>
2022-04-07 11:23:35 -0700

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -54,6 +54,16 @@ class Solution:
         return has_A, has_B, None 
 
 
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+
+
+class Solution:
     """
     @param: root: The root of the binary tree.
     @param: A: A TreeNode
@@ -62,32 +72,33 @@ class Solution:
     """
     def lowestCommonAncestor3(self, root, A, B):
         # write your code here
-        has_A, has_B, LCA = self.dfs(root, A, B)
-        if has_A and has_B:
-            return LCA
-        else:
-            return None 
-        
+        foundA, foundB, candidit = self.dfs(root, A, B)
+        if foundA and foundB:
+            return candidit
+
+        return None 
+
     def dfs(self, root, A, B):
-        #has_A, has_B, LCA
+        #found A, found B, candidit
         if not root:
-            return False, False, None  
+            return False, False, None 
 
-        left_has_A, left_has_B, left_LCA = self.dfs(root.left, A, B)
-        right_has_A, right_has_B, right_LCA = self.dfs(root.right, A, B)
-        
-        a = left_has_A or right_has_A or A == root
-        b = left_has_B or right_has_B or B == root
+        left_A, left_B, left_cand = self.dfs(root.left, A, B)
+        right_A, right_B, right_cand = self.dfs(root.right, A, B)
 
-        if A == root or B == root:
-            return a, b, root
+        found_a = left_A or right_A or A == root
+        found_b = left_B or right_B or B == root
 
-        if left_LCA and right_LCA:
-            return a, b, root
-        if left_LCA:
-            return a, b, left_LCA
-        if right_LCA:
-            return a, b, right_LCA
-        
-        return a, b, None
+        if root == A or root == B:
+            return found_a, found_b, root
 
+        if left_cand and right_cand:
+            return found_a, found_b, root
+
+        if left_cand:
+            return found_a, found_b, left_cand
+
+        if right_cand:
+            return found_a, found_b, right_cand
+
+        return False, False, None 
