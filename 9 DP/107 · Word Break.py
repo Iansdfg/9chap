c0ddbe9b7b56803f627e49b87ce2a18e975c63f9
Tian Feng <feng.txy@outlook.com>
2022-04-11 23:51:47 -0700

    Update 107 · Word Break.py

--- 9 DP/107 · Word Break.py
+++ 9 DP/107 · Word Break.py
@@ -6,21 +6,17 @@ class Solution:
     """
     def word_break(self, s, word_set):
         # write your code here
-        if not s:
-            return True
-        if not word_set:
-            return False 
-        dp = [False for _ in range(len(s)+1)]
 
-        max_len = max([len(word) for word in word_set])
-        dp[0] = True
+        dp = [False for i in range(len(s) + 1)]
+        dp[0] = True 
 
-        for i in range(1, len(s) + 1):
+        max_len = 0
+        for word in word_set:
+            max_len = max(max_len, len(word))
+            
+        for i in range(len(s) + 1):
             for j in range(max(i - max_len, 0), i):
-                if not dp[j]:
-                    continue
-                if s[j:i] in word_set:
-                    
-                    dp[i] = True
-                    break 
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True 
+
         return dp[-1]
