2ded54d270bedba5be2c20fe22e94c4b5ff2a5c0
Tian Feng <feng.txy@outlook.com>
2022-04-11 23:57:59 -0700

    Update 578. Lowest Common Ancestor III.py

--- 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
+++ 4Binary Tree - Divide Conquer & Traverse/578. Lowest Common Ancestor III.py
@@ -7,15 +7,6 @@ class TreeNode:
 """
 
 
-class Solution:"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        this.val = val
-        this.left, this.right = None, None
-"""
-
-
 class Solution:
     """
     @param: root: The root of the binary tree.
@@ -25,80 +16,33 @@ class Solution:
     """
     def lowestCommonAncestor3(self, root, A, B):
         # write your code here
-        has_A, has_B, lca = self.dfs(root, A, B)
-        if has_A and has_B:
-            return lca
-        return None
-
-    def dfs(self, root, A, B):
-        #has_A, has_B, lca(foundA or foundB)
-        if not root:
-            return False, False, None
-        
-        left_has_A, left_has_B, left_lca = self.dfs(root.left, A, B)
-        right_has_A, right_has_B, right_lca = self.dfs(root.right, A, B)
-
-        has_A = left_has_A or right_has_A or root == A
-        has_B = left_has_B or right_has_B or root == B
-
-        if root == A or root == B:
-            return has_A, has_B, root 
-        
-        if left_lca and right_lca:
-            return has_A, has_B, root 
-        if left_lca:
-            return has_A, has_B, left_lca 
-        if right_lca:
-            return has_A, has_B, right_lca 
-        
-        return has_A, has_B, None 
-
-
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        this.val = val
-        this.left, this.right = None, None
-"""
-
-
-class Solution:
-    """
-    @param: root: The root of the binary tree.
-    @param: A: A TreeNode
-    @param: B: A TreeNode
-    @return: Return the LCA of the two nodes.
-    """
-    def lowestCommonAncestor3(self, root, A, B):
-        # write your code here
-        foundA, foundB, candidit = self.dfs(root, A, B)
+        foundA, foundB, foundCandid = self.dfs(root, A, B)
         if foundA and foundB:
-            return candidit
-
+            return foundCandid
         return None 
 
+
     def dfs(self, root, A, B):
-        #found A, found B, candidit
+        #foundA, foundB, foundCandid
+
         if not root:
-            return False, False, None 
+            return False, False, None
 
-        left_A, left_B, left_cand = self.dfs(root.left, A, B)
-        right_A, right_B, right_cand = self.dfs(root.right, A, B)
+        left_foundA, left_foundB, left_foundCandid = self.dfs(root.left, A, B)
+        right_foundA, right_foundB, right_foundCandid = self.dfs(root.right, A, B)
 
-        found_a = left_A or right_A or A == root
-        found_b = left_B or right_B or B == root
+
+        foundA = left_foundA or right_foundA or root == A 
+        foundB = left_foundB or right_foundB or root == B 
 
         if root == A or root == B:
-            return found_a, found_b, root
+            return foundA, foundB, root
 
-        if left_cand and right_cand:
-            return found_a, found_b, root
-
-        if left_cand:
-            return found_a, found_b, left_cand
-
-        if right_cand:
-            return found_a, found_b, right_cand
-
-        return False, False, None 
+        if left_foundCandid and right_foundCandid:
+            return foundA, foundB, root
+        if left_foundCandid:
+            return foundA, foundB, left_foundCandid
+        if right_foundCandid:
+            return foundA, foundB, right_foundCandid
+        
+        return foundA, foundB, None 
