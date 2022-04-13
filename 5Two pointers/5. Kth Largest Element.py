0ad0cb00ddc4eddb29ad85dfc39f024378e22431
Tian Feng <feng.txy@outlook.com>
2022-04-12 17:44:21 -0700

    Update 5. Kth Largest Element.py

--- 5Two pointers/5. Kth Largest Element.py
+++ 5Two pointers/5. Kth Largest Element.py
@@ -8,29 +8,31 @@ class Solution:
         # write your code here
         # right bondary is len(nums) - 1 b/c we need to use num[right] to partition
         # the Kth largest the index is k - 1 
-        return self.helper(k - 1 , nums, 0, len(nums)-1)
+        return self.helper(k - 1, nums, 0, len(nums)-1)
+
 
     def helper(self, k, nums, start, end):
         if start == end:
-            return nums[k]
+                return nums[start]
+            
+        left, right = start, end 
 
-        left, right = start, end
-
-        mid = (start + end) // 2
+        mid = (left + right) // 2
         pivot = nums[mid]
 
         while left <= right:
             while left <= right and nums[left] > pivot:
                 left += 1 
             while left <= right and nums[right] < pivot:
-                right -= 1
+                right -= 1 
 
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1 
-                right -= 1
+                right -= 1 
 
-        # start, right, left, end
+        # 判断条件是看 k 在 start-right, right-left, left-end 中 
+        # 而不是比较 k 和 mid 的关系
         # use k <= right/left because if k == right/left we want to partition with right/left
         if k <= right:
             return self.helper(k, nums, start, right)
@@ -38,3 +40,4 @@ class Solution:
             return self.helper(k, nums, left, end)
         else:
             return nums[k]
+
