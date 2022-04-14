00c3054247196bb9719ebd7a618b23e70a5487f3
Tian Feng <feng.txy@outlook.com>
2022-04-14 10:28:00 -0700

    Update 31. Partition Array.py

--- 5Two pointers/31. Partition Array.py
+++ 5Two pointers/31. Partition Array.py
@@ -6,14 +6,16 @@ class Solution:
     """
     def partition_array(self, nums, k):
         # write your code here
-        left, right = 0, len(nums) - 1
+
+        left, right = 0, len(nums) - 1 
+
         while left <= right:
             while left <= right and nums[left] < k:
-                left += 1 
+                left += 1
             while left <= right and nums[right] >= k:
-                right -= 1
+                right -= 1 
             if left <= right:
-                nums[left], nums[right] = nums[right], nums[left]
+                nums[left], nums[right] = nums[right], nums[left] 
                 left += 1 
                 right -= 1 
         return left
