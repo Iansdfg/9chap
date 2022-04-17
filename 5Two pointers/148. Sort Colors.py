e3263bfc8bfffec8159837d435f21e46d3d87e2b
Tian Feng <feng.txy@outlook.com>
2022-04-16 17:37:50 -0700

    Update 148. Sort Colors.py

--- 5Two pointers/148. Sort Colors.py
+++ 5Two pointers/148. Sort Colors.py
@@ -7,13 +7,13 @@ class Solution:
         # write your code here
         left, mid, right = 0, 0, len(nums) - 1
         while mid <= right:
-            #if use <, list cannot be iterated thruly 
             if nums[mid] == 0:
-                nums[mid], nums[left] = nums[left], nums[mid]
-                left += 1 
+                nums[left], nums[mid] = nums[mid], nums[left] 
                 mid += 1 
+                left += 1
             elif nums[mid] == 1:
                 mid += 1 
-            elif nums[mid] == 2:
+            else:
                 nums[mid], nums[right] = nums[right], nums[mid]
                 right -= 1 
+
