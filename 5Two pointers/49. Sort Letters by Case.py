e8e09b96588b28d5636877754000987d23dbe52a
Tian Feng <feng.txy@outlook.com>
2022-04-18 11:12:06 -0700

    Update 49. Sort Letters by Case.py

--- 5Two pointers/49. Sort Letters by Case.py
+++ 5Two pointers/49. Sort Letters by Case.py
@@ -1,22 +1,17 @@
 class Solution:
     """
-    @param: chars: The letter array you should sort by Case
+    @param chars: The letter array you should sort by Case
     @return: nothing
     """
     def sortLetters(self, chars):
         # write your code here
         left, right = 0, len(chars) - 1 
-        
         while left <= right:
-            while left <= right and chars[left].islower():
-                left += 1  
-                
-            while left <= right and chars[right].isupper():
-                right -= 1  
-                
+            while left <= right and str(chars[left]).islower():
+                left += 1 
+            while left <= right and not str(chars[right]).islower():
+                right -= 1
             if left <= right:
                 chars[left], chars[right] = chars[right], chars[left]
                 left += 1 
                 right -= 1 
-                
-            
