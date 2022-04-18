3e4f6b245ba4d693c4d62cd5fb5b59c77f177f2e
Tian Feng <feng.txy@outlook.com>
2022-04-18 11:47:06 -0700

    Update 607. Two Sum III - Data structure design.py

--- 5Two pointers/607. Two Sum III - Data structure design.py
+++ 5Two pointers/607. Two Sum III - Data structure design.py
@@ -1,27 +1,24 @@
 class TwoSum:
+    num_freq= dict()
     """
     @param number: An integer
     @return: nothing
     """
-    def __init__(self):
-        self.num_to_cnt = {}
-
     def add(self, number):
         # write your code here
-        if number in self.num_to_cnt:
-            self.num_to_cnt[number] += 1
-        else:
-            self.num_to_cnt[number] = 1 
-
+        if number not in self.num_freq:
+            self.num_freq[number] = 0
+        self.num_freq[number] += 1
+        
     """
     @param value: An integer
     @return: Find if there exists any pair of numbers which sum is equal to the value.
     """
     def find(self, value):
         # write your code here
-        for num in self.num_to_cnt:
-            if value - num != num and value - num in self.num_to_cnt :
+        for num in self.num_freq:
+            self.num_freq[num] -= 1 
+            if value - num in self.num_freq and self.num_freq[value - num]!=0:
                 return True 
-            if value - num == num and self.num_to_cnt[num] > 1:
-                return True 
+            self.num_freq[num] += 1
         return False 
