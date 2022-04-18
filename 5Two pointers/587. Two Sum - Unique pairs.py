d882c724213fca65ad02644611549d64fbf1746a
Tian Feng <feng.txy@outlook.com>
2022-04-18 13:05:10 -0700

    Update 587. Two Sum - Unique pairs.py

--- 5Two pointers/587. Two Sum - Unique pairs.py
+++ 5Two pointers/587. Two Sum - Unique pairs.py
@@ -4,28 +4,34 @@ class Solution:
     @param target: An integer
     @return: An integer
     """
-    def twoSum6(self, nums, target):
+    def two_sum6(self, nums, target):
         # write your code here
         nums.sort()
-        res = 0
         left, right = 0, len(nums) - 1 
-        while left < right:
+        res = 0
 
-            if nums[right] + nums[left] > target:
-                right -= 1 
-            elif nums[right] + nums[left] < target:
+        while left < right:
+            twoSum = nums[left] + nums[right]
+            if twoSum < target:
                 left += 1 
+            elif twoSum > target:
+                right -= 1 
             else:
+                print(left, right)
                 res += 1 
                 left += 1 
                 right -= 1 
-                
+
+
+            #最后去重， 否则会多算 eg# ‘2,2,2,2,2’ 中间的2会多算一次
             while left < right and left > 0 and nums[left] == nums[left - 1]:
                 left += 1 
             while left < right and right < len(nums) - 1 and nums[right] == nums[right + 1]:
-                right -= 1 
-            
-        return res
+                right -= 1
+
+        return res 
+
+
 
     class Solution:
     """
