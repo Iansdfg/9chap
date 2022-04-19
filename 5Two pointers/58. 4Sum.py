bc6e3dac3ca860dcccce9b3cd9534b2463d259fa
Tian Feng <feng.txy@outlook.com>
2022-04-18 18:24:53 -0700

    Update 58. 4Sum.py

--- 5Two pointers/58. 4Sum.py
+++ 5Two pointers/58. 4Sum.py
@@ -8,30 +8,26 @@ class Solution:
     def four_sum(self, numbers, target):
         # write your code here
         numbers.sort()
-        print(numbers)
         res = []
-        for fir_pos in range(len(numbers)):
-            if fir_pos > 0 and numbers[fir_pos] == numbers[fir_pos - 1]:
+        last_pari = None 
+        for first in range(len(numbers)):
+            if first > 0 and numbers[first] == numbers[first - 1]:
                 continue
-            for sec_pos in range(fir_pos + 1, len(numbers)):
-                if sec_pos > fir_pos + 1 and numbers[sec_pos] == numbers[sec_pos - 1]:
+            for second in range(first + 1, len(numbers)):
+                if second > first + 1 and numbers[second] == numbers[second - 1]:
                     continue
-                start, end = sec_pos + 1, len(numbers) - 1
-                while start < end:
-                    print([numbers[fir_pos], numbers[sec_pos], numbers[start], numbers[end]])   
-                    four_sum = numbers[fir_pos] + numbers[sec_pos] + numbers[start] + numbers[end]
+                left, right = second + 1, len(numbers) - 1 
+                while left < right:
+                    four_sum = numbers[first] + numbers[second] + numbers[left] + numbers[right]
                     if four_sum > target:
-                        end -= 1 
+                        right -= 1 
                     elif four_sum < target:
-                        start += 1 
+                        left += 1 
                     else:
-                        res.append([numbers[fir_pos], numbers[sec_pos], numbers[start], numbers[end]])
-                        start += 1 
-                        end -= 1 
-                    #two while look remove redundency inside two sum
-                    while end > start and start > sec_pos + 1 and numbers[start]  == numbers[start - 1]:
-                        start += 1 
-                    while end > start and end < len(numbers) - 1 and numbers[end]  == numbers[end + 1]:
-                        end -= 1 
-                        
-        return res
+                        if [numbers[first], numbers[second], numbers[left], numbers[right]] != last_pari:
+                            res.append([numbers[first], numbers[second], numbers[left], numbers[right]])
+                            last_pari = [numbers[first], numbers[second], numbers[left], numbers[right]]
+                        left += 1 
+                        right -= 1 
+        return res 
+
