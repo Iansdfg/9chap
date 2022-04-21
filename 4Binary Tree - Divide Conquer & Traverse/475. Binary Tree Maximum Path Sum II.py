1410a39f8d02598c8f85f6c4f0e5ab4814385035
Tian Feng <feng.txy@outlook.com>
2022-04-21 13:33:59 -0700

    Update 475. Binary Tree Maximum Path Sum II.py

--- 4Binary Tree - Divide Conquer & Traverse/475. Binary Tree Maximum Path Sum II.py
+++ 4Binary Tree - Divide Conquer & Traverse/475. Binary Tree Maximum Path Sum II.py
@@ -1,3 +1,7 @@
+from lintcode import (
+    TreeNode,
+)
+
 """
 Definition of TreeNode:
 class TreeNode:
@@ -11,19 +15,20 @@ class Solution:
     @param root: the root of binary tree.
     @return: An integer
     """
-    def maxPathSum2(self, root):
+    max_val = float('-inf')
+    def max_path_sum2(self, root):
         # write your code here
-        return self.helper(root)
-        
-    def helper(self, root):
-        # return summ
-        if not root:
-            return 0
-            
-        left_sum = self.helper(root.left)
-        right_sum = self.helper(root.right)
-    
-        result = max(left_sum + root.val, right_sum + root.val, root.val) 
-        return result
-        
-        
+        self.dfs(root, root.val)
+        return self.max_val
+
+
+    def dfs(self, root, summ):
+        if summ > self.max_val:
+            self.max_val = summ
+
+        for next_node in [root.left, root.right]:
+            if not next_node:
+                continue
+            summ += next_node.val
+            self.dfs(next_node, summ)
+            summ -= next_node.val
