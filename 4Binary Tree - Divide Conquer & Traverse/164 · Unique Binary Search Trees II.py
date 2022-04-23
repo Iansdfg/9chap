96462487f2221aa3333300581d1f1c60ae0a98df
Tian Feng <feng.txy@outlook.com>
2022-04-22 21:54:30 -0700

    Update 164 · Unique Binary Search Trees II.py

--- 4Binary Tree - Divide Conquer & Traverse/164 · Unique Binary Search Trees II.py
+++ 4Binary Tree - Divide Conquer & Traverse/164 · Unique Binary Search Trees II.py
@@ -10,26 +10,21 @@ class Solution:
     # @return: A list of root
     def generateTrees(self, n):
         # write your code here
-        return self.dfs(n, 1, n)
+        return self.dfs(1, n)
 
-
-    def dfs(self, n, start, end):
+    def dfs(self, start, end):
         if start > end:
             return [None]
-
         res = []
+        for root_val in range(start, end + 1):
+            left_nodes = self.dfs(start, root_val - 1)
+            right_nodes = self.dfs(root_val + 1, end)
 
-        for rool_val in range(start, end + 1):
-            left_trees = self.dfs(n, start, rool_val-1)
-            right_trees = self.dfs(n, rool_val+1, end)
-
-            for left_tree in left_trees:
-                for right_tree in right_trees:
-                    root = TreeNode(rool_val)
-                    root.left = left_tree
-                    root.right = right_tree
-                    res.append(root)
-
-        return res 
-            
-
+            for left_node in left_nodes:
+                for right_node in right_nodes:
+                    curr = TreeNode(root_val)
+                    curr.left = left_node
+                    curr.right = right_node
+                    res.append(curr)
+        return res
+        
