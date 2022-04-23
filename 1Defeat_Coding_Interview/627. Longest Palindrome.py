1c3e9f92783c10be3bd11ddd90155dc051267078
Tian Feng <feng.txy@outlook.com>
2022-04-22 22:26:28 -0700

    Update 627. Longest Palindrome.py

--- 1Defeat_Coding_Interview/627. Longest Palindrome.py
+++ 1Defeat_Coding_Interview/627. Longest Palindrome.py
@@ -3,16 +3,19 @@ class Solution:
     @param s: a string which consists of lowercase or uppercase letters
     @return: the length of the longest palindromes that can be built
     """
-    def longestPalindrome(self, s):
+    def longest_palindrome(self, s):
         # write your code here
-        word_set = set()
-        count = 0
+        visited = set()
+        cnt = 0
         for char in s:
-            if char not in word_set:
-                word_set.add(char)
+            if char in visited:
+                visited.remove(char)
+                cnt += 1 
             else:
-                count += 1 
-                word_set.discard(char)
-        return min(count*2 + 1, len(s))
+                visited.add(char)
+
+        length = cnt * 2 
+        if visited:
+            return length + 1 
+        return length
             
-        
