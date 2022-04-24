c77cd013f8f285498abc326589a8af1c26cfad03
Tian Feng <feng.txy@outlook.com>
2022-04-24 13:48:36 -0700

    Update 140. Fast Power.py

--- 3Binary_Search/140. Fast Power.py
+++ 3Binary_Search/140. Fast Power.py
@@ -17,3 +17,26 @@ class Solution:
             base  = (base ** 2)%b
 
         return ans % b
+
+    
+class Solution:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+    def fast_power(self, a, b, n):
+        # write your code here
+        if n == 0:
+            return 1%b
+        if n == 1:
+            return a%b 
+
+        power = self.fast_power(a, b, n//2)
+        power = (power * power) % b 
+        
+        if n % 2: 
+            power = (power * a) % b
+        
+        return power
