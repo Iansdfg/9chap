81476086b764c40c9ea275c06a9453bbf357ff55
Tian Feng <feng.txy@outlook.com>
2022-04-23 23:35:26 -0700

    Update 228. Middle of Linked List.py

--- 5Two pointers/228. Middle of Linked List.py
+++ 5Two pointers/228. Middle of Linked List.py
@@ -1,5 +1,9 @@
+from lintcode import (
+    ListNode,
+)
+
 """
-Definition of ListNode
+Definition of ListNode:
 class ListNode(object):
     def __init__(self, val, next=None):
         self.val = val
@@ -11,13 +15,15 @@ class Solution:
     @param head: the head of linked list.
     @return: a middle node of the linked list
     """
-    def middleNode(self, head):
+    def middle_node(self, head):
         # write your code here
-        if not head: return head
-        dummy = ListNode(0)
+        if not head:
+            return None
+        dummy = ListNode(-1)
         dummy.next = head
-        fast = slow = dummy
+        slow, fast = dummy, dummy 
         while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-        return slow
+            slow = slow.next 
+            fast = fast.next.next 
+
+        return slow 
