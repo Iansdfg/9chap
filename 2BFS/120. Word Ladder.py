19544791e3a954326b16ca988622bd1f5d1da5d8
Tian Feng <feng.txy@outlook.com>
2022-04-23 23:08:03 -0700

    Update 120. Word Ladder.py

--- 2BFS/120. Word Ladder.py
+++ 2BFS/120. Word Ladder.py
@@ -1,94 +1,38 @@
-from collections import deque
+APLHA = "abcdefghijklmnopqrstuvwxyz"
 
+from collections import deque
 class Solution:
     """
-    @param: start: a string
-    @param: end: a string
-    @param: dict: a set of string
+    @param start: a string
+    @param end: a string
+    @param dict: a set of string
     @return: An integer
     """
-    def ladderLength(self, start, end, dict):
+    def ladder_length(self, start, end, dict):
         # write your code here
-        dict.add(end)
         queue = deque([start])
         visited = set([start])
-        count = 0
-        
-        while queue:
-            count += 1
-            # think word ladder as a trie, distance is height from root to 
-            # the leaf. So layering to check height.
-            for i in range(len(queue)):
-                word = queue.popleft()
-                if word == end:
-                    return count
-                
-                for new_word in self.get_next_words(word):
-                    if new_word in visited or new_word not in dict:
-                        continue
-                    queue.append(new_word)
-                    visited.add(new_word)
-        return 0
-        
-    def get_next_words(self, word):
-        words = []
-        for i in range(len(word)):
-            for char in 'abcdefghijklmnopqrstuvwxyz':
-                if word[i] == char:
-                    continue
-                new_word = word[:i] + char + word[i+1:]
-                words.append(new_word)
-        return words
-
-    
-    
-from collections import deque
-
-class Solution:
-    """
-    @param: start: a string
-    @param: end: a string
-    @param: dict: a set of string
-    @return: An integer
-    """
-    def ladderLength(self, start, end, dict):
-        # write your code here
-        dict.add(end)
-        queue = deque([start])
-        word_to_dist = {start:1}
+        step = 1
 
         while queue:
-            word = queue.popleft()
-            if word == end:
-                return word_to_dist[word]
-            
-            for new_word in self.get_next_words(word):
-                if new_word in word_to_dist or new_word not in dict:
-                    continue
-                queue.append(new_word)
-                word_to_dist[new_word] = word_to_dist[word] + 1
-        return 0
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                for i in range(len(curr)):
+                    for char in APLHA:
+                        new_word = curr[:i] + char + curr[i+1:]
+                        if new_word == end:
+                            return step + 1 
+                        if new_word in dict:
+                            if new_word in visited:
+                                continue
+                            queue.append(new_word)
+                            visited.add(new_word)
+            step += 1 
+
+        return -1
+    
+
         
-    def get_next_words(self, word):
-        words = []
-        for i in range(len(word)):
-            for char in 'abcdefghijklmnopqrstuvwxyz':
-                if word[i] == char:
-                    continue
-                new_word = word[:i] + char + word[i+1:]
-                words.append(new_word)
-        return words
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-                
-                
+
                         
+
