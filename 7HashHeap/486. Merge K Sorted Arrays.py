12ca915eafacb67f7a80338d6ad40c5a1f35da7d
Tian Feng <feng.txy@outlook.com>
2022-04-25 14:31:56 -0700

    Update 486. Merge K Sorted Arrays.py

--- 7HashHeap/486. Merge K Sorted Arrays.py
+++ 7HashHeap/486. Merge K Sorted Arrays.py
@@ -1,28 +1,27 @@
 import heapq
 class Solution:
-
     """
     @param arrays: k sorted integer arrays
     @return: a sorted array
     """
     def mergek_sorted_arrays(self, arrays):
         # write your code here
+
         heap = []
         res = []
         for array in arrays:
             if not array:
                 continue
-            heapq.heappush(heap, (array[0], array)) 
-
+            heapq.heappush(heap, (array[0], array))
+        
         while heap:
-            curr_val, curr_array = heapq.heappop(heap)
-            res.append(curr_val)
+            curr_num, curr_array = heapq.heappop(heap)
             curr_array.pop(0)
+            res.append(curr_num)
+
             if not curr_array:
                 continue
             heapq.heappush(heap, (curr_array[0], curr_array))
+
+        return res
             
-        return res 
-
-
-        
