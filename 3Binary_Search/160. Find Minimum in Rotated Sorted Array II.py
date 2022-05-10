94809c504b420907f762b89cefa528ed7b7406e5
Tian Feng <feng.txy@outlook.com>
2022-05-09 22:34:17 -0700

    Update 160. Find Minimum in Rotated Sorted Array II.py

--- 3Binary_Search/160. Find Minimum in Rotated Sorted Array II.py
+++ 3Binary_Search/160. Find Minimum in Rotated Sorted Array II.py
@@ -5,19 +5,19 @@ class Solution:
     """
     def find_min(self, nums):
         # write your code here
-        if not nums:
-            return -1
         start, end = 0, len(nums) - 1 
+
         while start + 1 < end:
-            mid = (start + end) // 2 
+            mid = (start + end)//2
             if nums[mid] < nums[end]:
-                end = mid
+                end = mid 
             elif nums[mid] > nums[end]:
-                start = mid
+                start = mid 
+            #why end -= 1 instead of start +=1:
+            #if arrary is not rotated, start += 1 
+            #will cut the min number, thus cannot 
+            #find the right answer. 
             else:
-                #why end -= 1 instead of start +=1:
-                #if arrary is not rotated, start += 1 
-                #will cut the min number, thus cannot 
-                #find the right answer. 
-                end -= 1 
+                end -= 1
+
         return min(nums[start], nums[end])
