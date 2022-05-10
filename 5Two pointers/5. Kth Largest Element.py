e8fc867b171e04884a1cbe05ad15a305781530d4
Tian Feng <feng.txy@outlook.com>
2022-05-09 22:02:42 -0700

    Update 5. Kth Largest Element.py

--- 5Two pointers/5. Kth Largest Element.py
+++ 5Two pointers/5. Kth Largest Element.py
@@ -8,19 +8,18 @@ class Solution:
         # write your code here
         # right bondary is len(nums) - 1 b/c we need to use num[right] to partition
         # the Kth largest the index is k - 1 
-        return self.helper(k - 1, nums, 0, len(nums)-1)
+        return self.quick_select(k - 1, nums, 0, len(nums) - 1)
 
 
-    def helper(self, k, nums, start, end):
-        if start == end:
-                return nums[start]
-            
-        left, right = start, end 
+    def quick_select(self, k, nums, start, end):
+        # if start == end:
+        #     return nums[start]
 
-        mid = (left + right) // 2
-        pivot = nums[mid]
+        left, right = start, end
 
-        while left <= right:
+        pivot = nums[(left + right)//2]
+
+        while left <= right: 
             while left <= right and nums[left] > pivot:
                 left += 1 
             while left <= right and nums[right] < pivot:
@@ -29,15 +28,17 @@ class Solution:
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1 
-                right -= 1 
-
+                right -= 1
         # 判断条件是看 k 在 start-right, right-left, left-end 中 
+        #[start:right][pivot][left:end]
         # 而不是比较 k 和 mid 的关系
         # use k <= right/left because if k == right/left we want to partition with right/left
         if k <= right:
-            return self.helper(k, nums, start, right)
+            return self.quick_select(k , nums, start, right)
         elif k >= left:
-            return self.helper(k, nums, left, end)
+            return self.quick_select(k , nums, left, end)
         else:
-            return nums[k]
+            return pivot
+
+        
 
