00bd3bbbd1cc83ce984a4c1198caae78c22f8397
Tian Feng <feng.txy@outlook.com>
2022-05-15 22:51:15 -0700

    Update 602 · Russian Doll Envelopes.py

--- 9 DP/602 · Russian Doll Envelopes.py
+++ 9 DP/602 · Russian Doll Envelopes.py
@@ -1,23 +1,22 @@
-###### O(n^2) ######
-
-from typing import (
-    List,
-)
-
 class Solution:
     """
     @param envelopes: a number of envelopes with widths and heights
     @return: the maximum number of envelopes
     """
-    def max_envelopes(self, envelopes: List[List[int]]) -> int:
+    def max_envelopes(self, envelopes):
         # write your code here
-        dp = [1 for i in range(len(envelopes))]
+        if not envelopes:
+            return 0
         envelopes.sort()
-
+        dp = [1 for i in range(len(envelopes))]
         for i in range(len(envelopes)):
             for j in range(i):
-                if envelopes[i][0] > envelopes[j][0] and envelopes[i][1] > envelopes[j][1] and dp[j] + 1 > dp[i]:
-                    dp[i] = dp[j] + 1 
+                if self.is_valid(envelopes[i], envelopes[j]):
+                    dp[i] = max(dp[i], dp[j] + 1)
 
         return max(dp)
-        
+
+    def is_valid(self, e1, e2):
+        if e1[0] > e2[0] and e1[1] > e2[1]:
+            return True 
+        return False 
