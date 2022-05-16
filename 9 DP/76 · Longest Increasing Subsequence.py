9f429056806e316e12afb4ceb5898dcd800905fa
Tian Feng <feng.txy@outlook.com>
2022-05-15 22:44:22 -0700

    Update 76 · Longest Increasing Subsequence.py

--- 9 DP/76 · Longest Increasing Subsequence.py
+++ 9 DP/76 · Longest Increasing Subsequence.py
@@ -9,13 +9,8 @@ class Solution:
             return 0
         dp = [1 for _ in range(len(nums))]
 
-        for i in range(len(dp)):
+        for i in range(len(nums)):
             for j in range(i):
                 if nums[j] < nums[i]:
-                    dp[i] = max(dp[i],dp[j]+ 1)
+                    dp[i] = max(dp[j] + 1, dp[i])
         return max(dp)
-
-                
-
-
-
