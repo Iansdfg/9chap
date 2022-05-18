3d937e94a3f4b30ceada4c90bc69b3f450c4a97d
Tian Feng <feng.txy@outlook.com>
2022-05-18 13:36:56 -0700

    Update 519 · Consistent Hashing.py

--- SystemDesign/519 · Consistent Hashing.py
+++ SystemDesign/519 · Consistent Hashing.py
@@ -9,7 +9,7 @@ class Solution:
         results = [[0,359,1]]
         #result[0], result[1] is #start, #end
         #result[2] is # of machine
-        #暴力找最大的编号最小的区间
+        #暴力编号最小的区间with max size
         for i in range(1, n):
             index = 0
             for j in range(i):
@@ -19,7 +19,7 @@ class Solution:
                 if j_size> max_size:
                     index = j
 
-            #found machine with max size
+            #found machine with max size min id
             start, end = results[index][0], results[index][1]
             results[index][1] = (start + end) / 2
             results.append([(start + end) / 2 + 1, end, i + 1])
