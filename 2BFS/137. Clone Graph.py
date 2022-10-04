df28ae12b5d7a8e7347e4d3c4d0dee9d1e12d6cd
Tian Feng <feng.txy@outlook.com>
2022-10-03 21:40:18 -0700

    Update 137. Clone Graph.py

--- 2BFS/137. Clone Graph.py
+++ 2BFS/137. Clone Graph.py
@@ -44,3 +44,46 @@ class Solution:
 
 
 
+#-----------------------10/3/2022-----------------------
+from lintcode import (
+    UndirectedGraphNode,
+)
+
+"""
+Definition for a UndirectedGraphNode:
+class UndirectedGraphNode:
+    def __init__(self, label):
+        self.label = label
+        self.neighbors = []
+"""
+
+from collections import deque 
+class Solution:
+    """
+    @param node: A undirected graph node
+    @return: A undirected graph node
+    """
+    def clone_graph(self, node: UndirectedGraphNode) -> UndirectedGraphNode:
+        # write your code here
+        if not node:
+            return node
+        queue = deque([node])
+        visited = dict()
+
+        while queue: 
+            curr = queue.popleft()
+            if curr in visited:
+                clone_curr = visited[curr]
+            else:
+                clone_curr = UndirectedGraphNode(curr.label)
+
+            visited[curr] = clone_curr
+
+            for neighbor in curr.neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    clone_neighbor = UndirectedGraphNode(neighbor.label)
+                    visited[neighbor] = clone_neighbor
+                clone_curr.neighbors.append(visited[neighbor])
+                
+        return visited[node]
