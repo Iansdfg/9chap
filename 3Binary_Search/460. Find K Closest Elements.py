87e347a8d9407ecc5ed6906b5b38c193af146d38
Tian Feng <feng.txy@outlook.com>
2022-10-17 20:10:48 -0700

    Update 460. Find K Closest Elements.py

--- 3Binary_Search/460. Find K Closest Elements.py
+++ 3Binary_Search/460. Find K Closest Elements.py
@@ -42,3 +42,58 @@ class Solution:
         return target - a[lower] <= a[upper] - target
         
          
+#######################10/18/2022#######################
+            
+            
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param a: an integer array
+    @param target: An integer
+    @param k: An integer
+    @return: an integer array
+    """
+    def k_closest_numbers(self, a: List[int], target: int, k: int) -> List[int]:
+        # write your code here
+        if k == 0:
+            return []
+        closest_pos = self.closest_number(a, target)
+        res = [a[closest_pos]]
+        left, right = closest_pos - 1,closest_pos + 1
+        while len(res) < k:
+            if self.left_closer(a, left, right, target):
+                res.append(a[left])
+                left -= 1 
+            else:
+                res.append(a[right])
+                right += 1 
+        return res 
+
+
+    def left_closer(self, a, left, right, target):
+        if right >= len(a):
+            return True  
+        return abs(a[left] - target) <= abs(a[right] - target)
+
+    def closest_number(self, a, target):
+        start, end = 0, len(a) - 1 
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if a[mid] > target:
+                end = mid 
+            elif a[mid] < target:
+                start = mid 
+            else:
+                return mid
+        if abs(a[start] - target) <= abs(a[end] - target):
+            return start
+        else:
+            return end
+
+
+
+
+
