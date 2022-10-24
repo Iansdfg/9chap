ffff60d4e4db138154bbe8f449768f04ba966a68
Tian Feng <feng.txy@outlook.com>
2022-10-23 17:51:04 -0700

    Update 88. Lowest Common Ancestor of a Binary Tree.py

--- 4Binary Tree - Divide Conquer & Traverse/88. Lowest Common Ancestor of a Binary Tree.py
+++ 4Binary Tree - Divide Conquer & Traverse/88. Lowest Common Ancestor of a Binary Tree.py
@@ -37,3 +37,45 @@ class Solution:
         if right_cand:
             return right_cand
         return None 
+    
+    
+ """
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the binary tree.
+    @param: A: A TreeNode in a Binary.
+    @param: B: A TreeNode in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """
+    def lowestCommonAncestor(self, root, A, B):
+        # write your code here
+        foundA, foundB, lca = self.dfs(root, A, B)
+        return lca
+
+    def dfs(self, root, A, B):
+        #return foundA, foundB, lca
+        if not root:
+            return False, False, None 
+
+        left_foundA, left_foundB, left_lca = self.dfs(root.left, A, B)
+        right_foundA, right_foundB, right_lca= self.dfs(root.right, A, B)
+
+        foundA = left_foundA or right_foundA or root == A 
+        foundB = left_foundB or right_foundB or root == B
+
+        lca = left_lca or right_lca
+        if foundA and foundB:
+            if not lca:
+                lca = root
+        else:
+            lca = None 
+
+        return foundA, foundB, lca
