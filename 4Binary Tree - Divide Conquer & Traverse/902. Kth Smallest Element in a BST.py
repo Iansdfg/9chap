879291da61e9e1741bc262270fd23b9224a2fdcf
Tian Feng <feng.txy@outlook.com>
2022-10-24 20:22:32 -0700

    Update 902. Kth Smallest Element in a BST.py

--- 4Binary Tree - Divide Conquer & Traverse/902. Kth Smallest Element in a BST.py
+++ 4Binary Tree - Divide Conquer & Traverse/902. Kth Smallest Element in a BST.py
@@ -30,3 +30,44 @@ class Solution:
                 stack.append(curr)
                 curr = curr.left
         return res
+    
+    
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param k: the given k
+    @return: the kth smallest element in BST
+    """
+    def kth_smallest(self, root: TreeNode, k: int) -> int:
+        # write your code here
+        dummy = TreeNode(0)
+        dummy.right = root
+        stack = [dummy]
+
+        for i in range(k):
+            curr = stack.pop()
+            if curr.right:
+                curr = curr.right
+                stack.append(curr)
+                while curr.left:
+                    curr = curr.left
+                    stack.append(curr)
+            
+            res = stack[-1].val
+
+        return res 
+
+
+            
