26076f0e8301f445c46e45ee4c953da1fcb4e5fa
Tian Feng <feng.txy@outlook.com>
2022-10-30 17:22:32 -0700

    Update 17. Subsets.py

--- 6DFS/17. Subsets.py
+++ 6DFS/17. Subsets.py
@@ -1,18 +1,25 @@
+from typing import (
+    List,
+)
+
 class Solution:
     """
     @param nums: A set of numbers
     @return: A list of lists
+             we will sort your return value in output
     """
-    def subsets(self, nums):
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         # write your code here
-        paths = []
-        self.dfs(nums, 0, [], paths)
-        return paths
+        nums.sort()
+        subsets = []
+        self.dfs(nums, 0, [], subsets)
+        return subsets
 
+    def dfs(self, nums, index, subset, subsets):
+        print(subset)
+        subsets.append(subset[:])
 
-    def dfs(self, nums, index, path, paths):
         for i in range(index, len(nums)):
-            path.append(nums[i])
-            self.dfs(nums, i + 1, path, paths)
-            path.pop()
-
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, subsets)
+            subset.pop()
