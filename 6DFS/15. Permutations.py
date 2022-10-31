f69965930b3d7ef9b455f13db7d6593c1f16cbc3
Tian Feng <feng.txy@outlook.com>
2022-10-30 19:42:18 -0700

    Update 15. Permutations.py

--- 6DFS/15. Permutations.py
+++ 6DFS/15. Permutations.py
@@ -28,3 +28,38 @@ class Solution:
 
             
             
+###################################################10/30/2022##############################################
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: A list of integers.
+    @return: A list of permutations.
+             we will sort your return value in output
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # write your code here
+        permutations = []
+        visited = set()
+        self.dfs(nums, visited, [], permutations)
+        return permutations
+
+
+    def dfs(self, nums, visited, permutation, permutations):
+        if len(permutation) == len(nums):
+            print(permutation)
+            permutations.append(permutation[:])
+            return
+
+        for i in range(len(nums)):
+            if nums[i] in visited:
+                continue
+            permutation.append(nums[i])
+            visited.add(nums[i])
+            self.dfs(nums, visited, permutation, permutations)
+            visited.remove(nums[i])
+            permutation.pop()
+            
+
