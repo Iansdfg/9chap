225eeac9d71c3d14fe3070cd2e4296feacb30621
Tian Feng <feng.txy@outlook.com>
2023-09-28 09:22:49 -0700

    Update 120. Word Ladder.py

--- 2BFS/120. Word Ladder.py
+++ 2BFS/120. Word Ladder.py
@@ -1,9 +1,12 @@
+####注意放进visited的时间点：pop out vs create （选择create的时候放入， 这样不用在loop里判断是否在queue里）
+
+
 from typing import (
     Set,
 )
 
-APLHA = "abcdefghijklmnopqrstuvwxyz"
 from collections import deque
+APLHA = "abcdefghijklmnopqrstuvwxyz"
 
 class Solution:
     """
@@ -15,23 +18,23 @@ class Solution:
     def ladder_length(self, start: str, end: str, dict: Set[str]) -> int:
         # write your code here
         queue = deque([start])
-        level = 1
+        res = 1
         visited = set([start])
-
         while queue:
             for _ in range(len(queue)):
                 curr_word = queue.popleft()
-
                 for i in range(len(curr_word)):
                     for alp in APLHA:
                         new_word = curr_word[:i] + alp + curr_word[i+1:]
                         if new_word == end:
-                            return level+1
+                            return res+1
                         if new_word in visited:
                             continue
                         if new_word in dict:
                             queue.append(new_word)
                             visited.add(new_word)
-            level += 1 
+            res += 1 
+        return 0
+                        
 
-       return -1
+
