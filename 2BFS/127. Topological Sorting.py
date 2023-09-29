65295a1ea53f458050c98ebe6e343414686d8e81
Tian Feng <feng.txy@outlook.com>
2023-09-29 12:04:47 -0700

    Update 127. Topological Sorting.py

--- 2BFS/127. Topological Sorting.py
+++ 2BFS/127. Topological Sorting.py
@@ -10,75 +10,38 @@ class Solution:
     @param graph: A list of Directed graph node
     @return: Any topological order for the given graph.
     """
+    
     def topSort(self, graph):
         # write your code here
-        if not graph:
-            return []
-        node_indegree = self.get_indegree(graph)
-        starts = [key for key in node_indegree if node_indegree[key] == 0]
-        queue = deque(starts)
-        order = []
-        while queue:
-            curr = queue.popleft()
-            order.append(curr)
-            for neighbor in curr.neighbors:
-                node_indegree[neighbor] -= 1
-                if node_indegree[neighbor] == 0:
-                    queue.append(neighbor)
-        return order
-                
-     
+        node_indegree = {}
 
-    def get_indegree(self, graph):
-        node_indegree = {x:0 for x in graph }
         for node in graph:
+            if node not in node_indegree:
+                node_indegree[node] = 0
+
             for neighbor in node.neighbors:
-                node_indegree[neighbor] += 1 
-        return node_indegree
-
-
-########################10/4/2022########################
-"""
-class DirectedGraphNode:
-     def __init__(self, x):
-         self.label = x
-         self.neighbors = []
-"""
-from collections import deque
-class Solution:
-    """
-    @param graph: A list of Directed graph node
-    @return: Any topological order for the given graph.
-    """
-    def topSort(self, graph):
-        # write your code here
-
-        node_to_indegree = dict()
-        for node in graph:
-            node_to_indegree[node] = 0
-
-        for node in graph:
-            for neighbor in node.neighbors:
-                node_to_indegree[neighbor] += 1 
-
+                if neighbor in node_indegree:
+                    node_indegree[neighbor]+=1
+                else:
+                    node_indegree[neighbor] = 1
+        
         queue = deque([])
-        for node in node_to_indegree:
-            # print(key.label, node_to_indegree[key])
-            if node_to_indegree[node] == 0:
+        for node in node_indegree:
+            if node_indegree[node] == 0:
                 queue.append(node)
 
-        topOrde = []
+        res = []
         while queue:
             curr = queue.popleft()
-            topOrde.append(curr)
-
+            res.append(curr)
             for neighbor in curr.neighbors:
-                node_to_indegree[neighbor] -= 1 
-                if node_to_indegree[neighbor] == 0:
+                node_indegree[neighbor] -= 1 
+                if node_indegree[neighbor] == 0:
                     queue.append(neighbor)
+        return res
 
-        return topOrde
+
 
 
         
-
+        
