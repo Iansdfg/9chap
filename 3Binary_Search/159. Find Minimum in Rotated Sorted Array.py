7d183e112806c85ed50069fe8bf9b2896946af5f
Tian Feng <feng.txy@outlook.com>
2023-10-01 15:04:08 -0700

    Update 159. Find Minimum in Rotated Sorted Array.py

--- 3Binary_Search/159. Find Minimum in Rotated Sorted Array.py
+++ 3Binary_Search/159. Find Minimum in Rotated Sorted Array.py
@@ -1,28 +1,3 @@
-class Solution:
-    """
-    @param nums: a rotated sorted array
-    @return: the minimum number in the array
-    """
-    def find_min(self, nums):
-        # write your code here
-        start, end = 0, len(nums) - 1 
-
-        while start + 1 < end:
-            #Compare with end b/c its consistent
-            #if array is not rotated. 
-            #If compare with start, the answer is 
-            #wrong if not rotated.
-            mid = (start + end)//2
-            if nums[mid] < nums[end]:
-                end = mid 
-            elif nums[mid] > nums[end]:
-                start = mid 
-
-        return min(nums[start], nums[end])
-    
-    
-##################10/17/2022##################
-                
 from typing import (
     List,
 )
@@ -34,14 +9,16 @@ class Solution:
     """
     def find_min(self, nums: List[int]) -> int:
         # write your code here
-        start, end = 0, len(nums) - 1
-        while start + 1 < end: 
-            #Compare with end b/c its consistent
-            #if array is not rotated. 
-            mid = (start + end) // 2
-            if nums[mid] < nums[-1]:
-                end = mid 
-            elif nums[mid] > nums[-1]:
-                start = mid 
+        if not nums:
+            return -1
+        start, end = 0, len(nums)-1
 
+        while start + 1 < end:
+            mid = (start + end) //2 
+            if nums[mid] <= nums[end]:
+                end = mid 
+            elif nums[mid] > nums[end]:
+                start = mid 
+                
         return min(nums[start], nums[end])
+
