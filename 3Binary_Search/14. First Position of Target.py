abb7cadbec13df7d82d98a9f00443f1132a0190b
Tian Feng <feng.txy@outlook.com>
2023-10-01 12:59:26 -0700

    Update 14. First Position of Target.py

--- 3Binary_Search/14. First Position of Target.py
+++ 3Binary_Search/14. First Position of Target.py
@@ -1,26 +1,33 @@
+from typing import (
+    List,
+)
+
 class Solution:
     """
     @param nums: The integer array.
     @param target: Target to find.
     @return: The first position of target. Position starts from 0.
     """
-    def binary_search(self, nums, target):
+    def binary_search(self, nums: List[int], target: int) -> int:
         # write your code here
-        if not nums:
-            return - 1 
-        start, end = 0, len(nums) - 1 
-
+        if not nums: return -1
+        start, end = 0, len(nums) - 1
+        
         while start + 1 < end:
-            mid = (start + end) // 2
-            if nums[mid] > target:
-                end = mid 
-            elif nums[mid] < target:
+            mid = (start + end)//2
+
+            if target > nums[mid]:
                 start = mid 
-            else:
+            if target <= nums[mid]:
                 end = mid 
 
         if nums[start] == target:
             return start
-        if nums[end] == target:
-            return end
-        return -1 
+        elif nums[end] == target:
+            return end 
+        return -1
+            
+            
+
+
+
