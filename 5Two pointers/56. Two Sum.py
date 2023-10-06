9c79aedb13d0b92c4fff93a63635259c959acc92
Tian Feng <feng.txy@outlook.com>
2023-10-05 23:53:12 -0700

    Update 56. Two Sum.py

--- 5Two pointers/56. Two Sum.py
+++ 5Two pointers/56. Two Sum.py
@@ -16,3 +16,32 @@ class Solution:
                 return [num_pos[num], pos]
             num_pos[target - num] = pos
             
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param numbers: An array of Integer
+    @param target: target = numbers[index1] + numbers[index2]
+    @return: [index1, index2] (index1 < index2)
+    """
+    def two_sum(self, numbers: List[int], target: int) -> List[int]:
+        # write your code here
+        new_nums = []
+        for pos, num in enumerate(numbers):
+            new_nums.append((num, pos))
+        new_nums.sort()
+        print(new_nums)
+        left, right = 0, len(numbers) - 1 
+        while left < right:
+            if new_nums[left][0] + new_nums[right][0] < target:
+                left += 1 
+            elif new_nums[left][0] + new_nums[right][0] > target:
+                right -= 1
+            else:
+                big = max(new_nums[left][1], new_nums[right][1])
+                small = min(new_nums[left][1], new_nums[right][1])
+                
+                return [small, big]
+        return [-1, -1]
