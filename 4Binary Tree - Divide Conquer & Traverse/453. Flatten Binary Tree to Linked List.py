977f1069d6028eb16f2f7ef5d998af691b871994
Tian Feng <feng.txy@outlook.com>
2023-10-06 22:00:22 -0700

    Update 453. Flatten Binary Tree to Linked List.py

--- 4Binary Tree - Divide Conquer & Traverse/453. Flatten Binary Tree to Linked List.py
+++ 4Binary Tree - Divide Conquer & Traverse/453. Flatten Binary Tree to Linked List.py
@@ -81,6 +81,61 @@ class Solution:
 
         return root
 
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root: TreeNode):
+        # write your code here
+        return self.dfs(root)
+
+
+
+    def dfs(self, node):
+        if not node:
+            return None 
+
+        left = self.dfs(node.left)
+        right = self.dfs(node.right)
+
+        if not left and not right:
+            return node 
+        if not left:
+            return node 
+
+        if not right:
+            node.right = left
+            node.left = None 
+            return node
+
+        left_end = left
+        while left_end.right:
+            left_end = left_end.right
+
+        right_end = right 
+        while right_end.right:
+            right_end = right_end.right
+
+        left_end.right = right
+        node.right = left
+        node.left = None
+
+        return node 
+
+
 
 
 
