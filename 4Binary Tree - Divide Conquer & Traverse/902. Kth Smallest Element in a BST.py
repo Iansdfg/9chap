ee9abf5a36bcff6ab773a0b4d6c1bd3d64b473f8
Tian Feng <feng.txy@outlook.com>
2023-10-08 13:04:37 -0700

    Update 902. Kth Smallest Element in a BST.py

--- 4Binary Tree - Divide Conquer & Traverse/902. Kth Smallest Element in a BST.py
+++ 4Binary Tree - Divide Conquer & Traverse/902. Kth Smallest Element in a BST.py
@@ -16,58 +16,25 @@ class Solution:
     @param k: the given k
     @return: the kth smallest element in BST
     """
-    def kth_smallest(self, root, k):
-        # write your code here
-        dummy = TreeNode(0)
-        dummy.right = root 
-        stack = [dummy]
-
-        for _ in range(k + 1):
-            curr = stack.pop()
-            res = curr.val
-            curr = curr.right
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-        return res
-    
-    
-from lintcode import (
-    TreeNode,
-)
-
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
-class Solution:
-    """
-    @param root: the given BST
-    @param k: the given k
-    @return: the kth smallest element in BST
-    """
     def kth_smallest(self, root: TreeNode, k: int) -> int:
         # write your code here
-        dummy = TreeNode(0)
+        if not root:
+            return None 
+        dummy = TreeNode(-1)
         dummy.right = root
         stack = [dummy]
+        res = None 
 
-        for i in range(k):
+        for _ in range(k+1):
             curr = stack.pop()
+            res = curr.val 
+
             if curr.right:
                 curr = curr.right
                 stack.append(curr)
+                # while 在 if loop 中 因为 left child is already put into stack
                 while curr.left:
                     curr = curr.left
                     stack.append(curr)
-            
-            res = stack[-1].val
 
-        return res 
-
-
-            
+        return res
