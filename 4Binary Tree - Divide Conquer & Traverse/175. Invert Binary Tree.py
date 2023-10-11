ede4b8a9025ebf8f3e22ffa9cb45416b3825736d
Tian Feng <feng.txy@outlook.com>
2023-10-11 12:45:05 -0700

    Update 175. Invert Binary Tree.py

--- 4Binary Tree - Divide Conquer & Traverse/175. Invert Binary Tree.py
+++ 4Binary Tree - Divide Conquer & Traverse/175. Invert Binary Tree.py
@@ -32,3 +32,38 @@ class Solution:
         node.left, node.right = right, left
 
         return node 
+
+
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+from collections import deque
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def invert_binary_tree(self, root: TreeNode):
+        # write your code here
+        queue = deque([root])
+
+        while queue:
+            curr = queue.popleft()
+            curr.left, curr.right = curr.right, curr.left
+
+            
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+
+                
+
