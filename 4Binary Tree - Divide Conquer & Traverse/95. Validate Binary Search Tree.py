e493440ea4b7fe2cfb68f87b7684d72f73234108
Tian Feng <feng.txy@outlook.com>
2023-10-18 22:02:19 -0700

    Update 95. Validate Binary Search Tree.py

--- 4Binary Tree - Divide Conquer & Traverse/95. Validate Binary Search Tree.py
+++ 4Binary Tree - Divide Conquer & Traverse/95. Validate Binary Search Tree.py
@@ -31,5 +31,43 @@ class Solution:
         min_num = min(root.val, left_min, right_min)
 
         return max_num, min_num, is_bst
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    def is_valid_b_s_t(self, root: TreeNode) -> bool:
+        # write your code here
+        valid, min_val, max_val  = self.dfs(root)
+        return valid
+
+
+    def dfs(self, node):
+        if not node:
+            return True, float('inf'), -float('inf') 
+
+        left_valid, left_min, left_max= self.dfs(node.left)
+        right_valid, right_min, right_max = self.dfs(node.right)
+
+        valid = False
+        if left_max < node.val and right_min > node.val:
+            valid = left_valid and right_valid
+        
+        min_val = min(node.val, left_min)
+        max_val = max(node.val, right_max)
+
+        return valid, min_val, max_val 
 
 
