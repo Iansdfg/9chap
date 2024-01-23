7ee9da2b2df9c782d1de9df6dc420b803578c275
Tian Feng <feng.txy@outlook.com>
2024-01-22 20:52:57 -0800

    Update 31. Partition Array.py

--- 5Two pointers/31. Partition Array.py
+++ 5Two pointers/31. Partition Array.py
@@ -1,22 +1,24 @@
+from typing import (
+    List,
+)
+
 class Solution:
     """
     @param nums: The integer array you should partition
     @param k: An integer
     @return: The index after partition
     """
-    def partition_array(self, nums, k):
+    def partition_array(self, nums: List[int], k: int) -> int:
         # write your code here
-
-        left, right = 0, len(nums) - 1 
-
+        left, right = 0, len(nums) - 1
         while left <= right:
-            while left <= right and nums[left] < k:
-                left += 1
-            while left <= right and nums[right] >= k:
+            while left <= right and nums[left] < k :
+                left += 1 
+            while left <= right and nums[right] >= k :
                 right -= 1 
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left] 
                 left += 1 
-                right -= 1 
-        return left
+                right -= 1
+        return left 
 
